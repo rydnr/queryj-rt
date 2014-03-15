08fe99127a3fd06f8f4ae9c06b26c01282f5482b
chous <chous@acm-sl.org>
2014-03-15 08:42:28 +0100

    201403150842

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandler.java
@@ -111,7 +111,7 @@ public class BindQueryParametersHandler
         {
             @NotNull final String t_strSql = t_strValue.trim();
 
-            @Nullable t_ExceptionToThrow = null;
+            @Nullable QueryJBuildException t_ExceptionToThrow = null;
             @Nullable SQLException t_ExceptionToWrap = null;
 
             @Nullable PreparedStatement t_PreparedStatement = null;
