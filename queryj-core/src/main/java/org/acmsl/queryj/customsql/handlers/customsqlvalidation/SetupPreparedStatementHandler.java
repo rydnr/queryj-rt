31b76fa8eff7c345c9fb1eef4946e884f57b2188
chous <chous@acm-sl.org>
2014-03-15 10:21:41 +0100

    201403151021

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/SetupPreparedStatementHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/SetupPreparedStatementHandler.java
@@ -146,7 +146,7 @@ public class SetupPreparedStatementHandler
 
         if (result == null)
         {
-            throw new PreparedStatementNotAvailable
+            throw new PreparedStatementNotAvailableFor
         }
     }
 
