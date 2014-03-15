7dd7afd9d7788fe9565c5070361f98ed3b687a7f
chous <chous@acm-sl.org>
2014-03-15 09:05:01 +0100

    201403150905

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
@@ -102,7 +102,7 @@ public class BindQueryParametersHandlerTest
 
         @NotNull final List<ParameterRef> t_lParameterRefs = new ArrayList<>(1);
         t_lParameterRefs.add(new ParameterRefElement("id"));
-        sql.add();
+        sql.add(new ParameterRefElement("id"));
         @NotNull final Parameter parameter =
             new ParameterElement("id", 1, "name", "String", "1");
 
