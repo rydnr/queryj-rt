843b4bbd555b5d4b96b6053072af5031fa3da145
chous <chous@acm-sl.org>
2014-03-15 09:00:13 +0100

    201403150900

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
@@ -107,7 +107,7 @@ public class BindQueryParametersHandlerTest
 
         EasyMock.expect(t_CustomSqlProvider.getSqlParameterDAO()).andReturn(t_SqlParameterDAO);
         EasyMock.expect(t_SqlParameterDAO.findByPrimaryKey("" + parameter.getName())).andReturn(parameter);
-        EasyMock.expect(t_Connection.prepareStatement())
+        EasyMock.expect(t_Connection.prepareStatement(sql.getValue()))
         new QueryJCommandWrapper<CustomSqlProvider>(parameters).setSetting(
             CustomSqlProviderRetrievalHandler.CUSTOM_SQL_PROVIDER, t_CustomSqlProvider);
         new QueryJCommandWrapper<Connection>(parameters).setSetting(
