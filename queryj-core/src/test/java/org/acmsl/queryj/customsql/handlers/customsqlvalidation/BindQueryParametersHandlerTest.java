57bb6c7d65383dd8c4d7a92dda7e38a72bb96abc
chous <chous@acm-sl.org>
2014-03-15 08:08:32 +0100

    201403150808

---
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
@@ -0,0 +1,87 @@
+/*
+                        queryj
+
+    Copyright (C) 2002-today  Jose San Leandro Armendariz
+                              chous@acm-sl.org
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: jose.sanleandro@acm-sl.com
+
+ ******************************************************************************
+ *
+ * Filename: BindQueryParametersHandlerTest.java
+ *
+ * Author: Jose San Leandro Armendariz
+ *
+ * Description: 
+ *
+ * Date: 2014/03/15
+ * Time: 08:03
+ *
+ */
+package org.acmsl.queryj.customsql.handlers.customsqlvalidation;
+
+/*
+ * Importing JetBrains annotations.
+ */
+import org.acmsl.queryj.ConfigurationQueryJCommandImpl;
+import org.acmsl.queryj.QueryJCommand;
+import org.acmsl.queryj.QueryJCommandWrapper;
+import org.acmsl.queryj.api.exceptions.QueryJBuildException;
+import org.acmsl.queryj.customsql.Sql;
+import org.acmsl.queryj.customsql.Sql.Cardinality;
+import org.acmsl.queryj.customsql.SqlElement;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * Importing checkthread.org annotations.
+ */
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ *
+ * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
+ * @since 3.0
+ * Created: 2014/03/15 08:03
+ */
+@RunWith(JUnit4.class)
+public class BindQueryParametersHandlerTest
+{
+    @Test
+    public void binds_the_parameters_to_the_query()
+        throws QueryJBuildException
+    {
+        @NotNull final BindQueryParametersHandler instance = new BindQueryParametersHandler();
+
+        @NotNull final QueryJCommand parameters = new ConfigurationQueryJCommandImpl(new PropertiesConfiguration());
+
+        @NotNull final Sql<String> sql =
+            new SqlElement<>(
+                "id", "dao", "name", "String", Cardinality.SINGLE, "all", false /* validation */, false, "description");
+
+        new QueryJCommandWrapper<Sql<String>>(parameters).setSetting(RetrieveQueryHandler.CURRENT_SQL, sql);
+
+        // Add parameters to the command
+
+        Assert.assertTrue(instance.handle(parameters));
+
+    }
+}
