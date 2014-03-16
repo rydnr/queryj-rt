198b06d5df8ee9ced28a411dc5253ff14597b8f9
chous <chous@acm-sl.org>
2014-03-16 18:40:18 +0100

    201403161840

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/CustomSqlValidationChainTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/CustomSqlValidationChainTest.java
@@ -85,7 +85,7 @@ public class CustomSqlValidationChainTest
 
         instance.buildChain(t_Chain);
 
-        Assert.assertTrue(t_Chain.contains(new BindQueryParametersHandler.class));
+        Assert.assertTrue(t_Chain.contains(new BindQueryParametersHandler()));
         Assert.assertTrue(t_Chain.contains(CacheValidationOutcomeHandler.class));
         Assert.assertTrue(t_Chain.contains(CheckResultSetGettersWorkForDefinedPropertiesHandler.class));
         Assert.assertTrue(t_Chain.contains(ExecuteQueryHandler.class));
