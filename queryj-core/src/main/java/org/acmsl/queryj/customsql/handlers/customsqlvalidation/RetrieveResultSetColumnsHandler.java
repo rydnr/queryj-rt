d393479b16eb24bf1ca92a21b2e3f4c0ebfe543c
chous <chous@acm-sl.org>
2014-03-16 11:16:19 +0100

    201403161116

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultSetColumnsHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultSetColumnsHandler.java
@@ -82,7 +82,7 @@ public class RetrieveResultSetColumnsHandler
     {
         @NotNull final ResultSet t_ResultSet = new ExecuteQueryHandler().retrieveCurrentResultSet(command);
 
-        @NotNull final List<Property<String>> t_lColumns =
+        @NotNull final List<Property<String>> t_lColumns = retrieveColumns(t_ResultSet, )
         return true;
     }
 
