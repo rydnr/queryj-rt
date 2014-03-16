dd105c4eb3cbdfaea519e30b1d58d7202ec38ac3
chous <chous@acm-sl.org>
2014-03-16 07:05:04 +0100

    201403160705

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandlerTest.java
@@ -130,7 +130,7 @@ public class RetrieveResultPropertiesHandlerTest
         EasyMock.expect(t_Statement.executeQuery()).andReturn(t_ResultSet);
 
         new QueryJCommandWrapper<MetadataManager>(parameters)
-            .setSetting(CustomSqlProviderRetrievalHandler.CUSTOM_SQL_PROVIDER, t_MetadataManager);
+            .setSetting(, t_MetadataManager);
         new QueryJCommandWrapper<CustomSqlProvider>(parameters).setSetting(
             CustomSqlProviderRetrievalHandler.CUSTOM_SQL_PROVIDER, t_CustomSqlProvider);
 
