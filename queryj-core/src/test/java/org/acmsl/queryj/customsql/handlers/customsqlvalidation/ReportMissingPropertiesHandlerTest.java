104ad9e0af7cdbb999b4a8e5b42d4cc69bf99cd1
chous <chous@acm-sl.org>
2014-03-16 11:57:09 +0100

    201403161157

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandlerTest.java
@@ -79,7 +79,7 @@ public class ReportMissingPropertiesHandlerTest
         t_lProperties.add(new PropertyElement<>("name", "name", 1, String.class.getSimpleName(), false));
         t_lProperties.add(new PropertyElement<>("tmst", "tmst", 1, "Date", false));
 
-        @NotNull final List<Property<String>> t_lColumns = new ArrayList<>(2);
+        @NotNull final List<Property<String>> t_lColumns = new ArrayList<>(3);
         t_lProperties.add(new PropertyElement<>("name", "name", 1, String.class.getSimpleName(), false));
         t_lProperties.add(new PropertyElement<>("tmst", "tmst", 1, "Date", false));
 
