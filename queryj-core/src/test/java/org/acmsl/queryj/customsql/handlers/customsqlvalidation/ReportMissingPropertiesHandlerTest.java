5d9ca08093ea9765303c981775adc40f0e81bbef
chous <chous@acm-sl.org>
2014-03-16 12:51:07 +0100

    201403161251

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandlerTest.java
@@ -120,6 +120,6 @@ public class ReportMissingPropertiesHandlerTest
         Assert.assertFalse(instance.handle(t_Parameters));
 
         EasyMock.verify(t_Log);
-        PowerMockito.verifyStatic(UniqueLogFactory.class);
+        PowerMockito.verifyStatic(UniqueLogFactory.class, Mockito.never());
     }
 }
