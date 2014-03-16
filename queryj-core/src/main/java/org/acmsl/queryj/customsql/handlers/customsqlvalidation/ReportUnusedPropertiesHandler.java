7d7f22c4b57baa2bcd21f8cf23fa073959bc5697
chous <chous@acm-sl.org>
2014-03-16 13:14:08 +0100

    201403161314

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportUnusedPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportUnusedPropertiesHandler.java
@@ -53,6 +53,9 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 import org.jetbrains.annotations.Nullable;
 
+/*
+ * Importing JDK classes.
+ */
 import java.util.List;
 
 /**
