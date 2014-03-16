079ae201e62fae226d358cd795eace71526bc02c
chous <chous@acm-sl.org>
2014-03-16 08:11:35 +0100

    201403160811

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandlerTest.java
@@ -115,7 +115,7 @@ public class RetrieveResultPropertiesHandlerTest
             new PropertyElement<>("tmst", "tmst", 1, "Date", false);
         @NotNull final List<Property<String>> t_lProperties = new ArrayList<>(2);
         t_lProperties.add(new PropertyElement<>("name", "name", 1, String.class.getSimpleName(), false));
-        t_lProperties.add(t_Property2);
+        t_lProperties.add(new PropertyElement<>("tmst", "tmst", 1, "Date", false));
         @NotNull final Result<String> t_Result = new ResultElement<>("r1", "Vo");
         t_Result.add(new PropertyRefElement("name"));
         t_Result.add(new PropertyRefElement("tmst"));
