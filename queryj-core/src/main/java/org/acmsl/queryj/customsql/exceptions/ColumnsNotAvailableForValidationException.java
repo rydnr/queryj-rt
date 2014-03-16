14715c09e39b53cbb4778452889a6e99452ba26f
chous <chous@acm-sl.org>
2014-03-16 11:25:34 +0100

    201403161125

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/exceptions/ColumnsNotAvailableForValidationException.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/exceptions/ColumnsNotAvailableForValidationException.java
@@ -50,6 +50,7 @@ import org.checkthread.annotations.ThreadSafe;
 /**
  * Represents the error when trying to access the list of columns (represented as
  * {@link org.acmsl.queryj.customsql.Property properties}) extracted from the {@link java.sql.ResultSet}, and
+ * no
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/16 11:22
