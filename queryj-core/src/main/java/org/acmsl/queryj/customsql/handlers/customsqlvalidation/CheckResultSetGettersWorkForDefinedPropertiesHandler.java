74c29722670619749036e4120c0940b8ad398204
chous <chous@acm-sl.org>
2014-03-16 09:10:00 +0100

    201403160910

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CheckResultSetGettersWorkForDefinedPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CheckResultSetGettersWorkForDefinedPropertiesHandler.java
@@ -74,7 +74,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Performs some checks
+ * Performs some checks o
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/16 08:44
