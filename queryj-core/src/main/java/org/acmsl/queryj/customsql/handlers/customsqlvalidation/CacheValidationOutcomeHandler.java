ca34256a1c2c54a7a11fb97865ff9039b6857e16
chous <chous@acm-sl.org>
2014-03-16 16:01:03 +0100

    201403161601

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandler.java
@@ -69,7 +69,8 @@ public class CacheValidationOutcomeHandler
      *         or maybe because it's not desirable to continue the chain.
      */
     @Override
-    public boolean handle(@NotNull final QueryJCommand command) throws QueryJBuildException
+    public boolean handle(@NotNull final QueryJCommand command)
+        throws QueryJBuildException
     {
         final boolean t_bResultSetGettersCheck =
             new CheckResultSetGettersWorkForDefinedPropertiesHandler().getValidationOutcome(t_Sql);
