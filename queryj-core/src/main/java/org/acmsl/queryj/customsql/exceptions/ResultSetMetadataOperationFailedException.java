1c51411867696064a94c2a24011ea54b8428c328
chous <chous@acm-sl.org>
2014-03-16 06:58:38 +0100

    201403160658

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/exceptions/ResultSetMetadataOperationFailedException.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/exceptions/ResultSetMetadataOperationFailedException.java
@@ -36,7 +36,7 @@
 package org.acmsl.queryj.customsql.exceptions;
 
 /*
- * Importing JetBrains annotations.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.api.exceptions.QueryJNonCheckedException;
 import org.acmsl.queryj.customsql.ResultRef;
