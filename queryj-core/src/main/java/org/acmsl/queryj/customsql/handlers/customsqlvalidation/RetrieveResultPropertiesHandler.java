7f1052e734f54b2b01a1aa6862119373e828de3b
chous <chous@acm-sl.org>
2014-03-16 06:31:27 +0100

    201403160631

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
@@ -71,6 +71,6 @@ public class RetrieveResultPropertiesHandler
     @Override
     public boolean handle(@NotNull final QueryJCommand command) throws QueryJBuildException
     {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
+        return true;
     }
 }
