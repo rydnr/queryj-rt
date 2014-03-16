06698da3c02129a01c2932d9d08468bd1ea4a038
chous <chous@acm-sl.org>
2014-03-16 11:14:16 +0100

    201403161114

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultSetColumnsHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultSetColumnsHandler.java
@@ -95,7 +95,7 @@ public class RetrieveResultSetColumnsHandler
 
         for  (int t_iIndex = 1; t_iIndex <= t_iColumnCount; t_iIndex++)
         {
-            result.add(createPropertyFrom(t_Metadata, t_iIndex));
+            result.add(t_Handler.createPropertyFrom(t_Metadata, t_iIndex));
         }
 
         return result;
