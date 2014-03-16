aa04c450943772a55bf8684fac0d205f3a0b017d
chous <chous@acm-sl.org>
2014-03-16 16:07:40 +0100

    201403161607

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandler.java
@@ -70,7 +70,7 @@ import java.nio.charset.Charset;
 
 /**
  * If all validations succeed for the current {@link Sql}, the
- * outcome is cached to avoid unnecessary
+ * outcome is cached to avoid unnecessary validation
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/16 15:09
