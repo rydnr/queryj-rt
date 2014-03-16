a6b80929e53e16240e23b3941bcc17cf1ad927b6
chous <chous@acm-sl.org>
2014-03-16 07:32:49 +0100

    201403160732

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
@@ -348,6 +348,7 @@ public class RetrieveResultPropertiesHandler
     @NotNull
     protected List<Property<String>> retrieveImplicitProperties(
         @NotNull final Result<String> sqlResult,
+        @Nullable final String table,
         @NotNull final CustomSqlProvider customSqlProvider,
         @NotNull final MetadataManager metadataManager,
         @NotNull final TypeManager typeManager,
