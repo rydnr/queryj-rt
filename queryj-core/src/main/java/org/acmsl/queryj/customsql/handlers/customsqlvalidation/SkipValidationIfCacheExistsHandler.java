235fa579d4f3cd018fd48cb3e75937583a10752b
chous <chous@acm-sl.org>
2014-03-16 16:45:23 +0100

    201403161645

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/SkipValidationIfCacheExistsHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/SkipValidationIfCacheExistsHandler.java
@@ -77,7 +77,8 @@ public class SkipValidationIfCacheExistsHandler
         return validationCacheFound(t_Sql, command);
     }
 
-    private boolean validationCacheFound(final Sql<String> t_sql, final QueryJCommand command)
+    private boolean validationCacheFound(final Sql<String> sql, final QueryJCommand command)
     {
+        return false;  //To change body of created methods use File | Settings | File Templates.
     }
 }
