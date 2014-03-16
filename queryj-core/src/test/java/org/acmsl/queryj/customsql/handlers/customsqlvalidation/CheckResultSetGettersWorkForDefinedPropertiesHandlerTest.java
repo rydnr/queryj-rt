128f9b1a9647fe472fbfc039a253d374fce73dc2
chous <chous@acm-sl.org>
2014-03-16 08:47:13 +0100

    201403160847

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CheckResultSetGettersWorkForDefinedPropertiesHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CheckResultSetGettersWorkForDefinedPropertiesHandlerTest.java
@@ -76,6 +76,7 @@ import org.jetbrains.annotations.NotNull;
 /*
  * Importing JUnit/PowerMock/EasyMock classes.
  */
+import org.easymock.EasyMock;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
