66bd7735a9adf99379e775f67eed1b5985a89fc8
chous <chous@acm-sl.org>
2014-03-16 11:30:38 +0100

    201403161130

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CannotAnalyzeResultSetForValidationException.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CannotAnalyzeResultSetForValidationException.java
@@ -48,7 +48,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Represents any problem dealing with {@link java.sql.ResultSetMetaData} to
+ * Represents any problem dealing with {@link java.sql.ResultSetMetaData} when validating
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/16 11:28
