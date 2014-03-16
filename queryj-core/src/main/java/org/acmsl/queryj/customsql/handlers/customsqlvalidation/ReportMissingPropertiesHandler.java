e60bb15fc04ff87c3941ae57d2c2551800b0cd7f
chous <chous@acm-sl.org>
2014-03-16 13:03:46 +0100

    201403161303

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandler.java
@@ -58,7 +58,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Detects and reports missing properties
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/16 10:34
