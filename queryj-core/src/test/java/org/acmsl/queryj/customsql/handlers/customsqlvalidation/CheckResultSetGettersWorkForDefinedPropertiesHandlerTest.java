4a969f11cdcbdff413b72d7d3f26cf1dd0bc766f
chous <chous@acm-sl.org>
2014-03-16 10:57:06 +0100

    201403161057

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CheckResultSetGettersWorkForDefinedPropertiesHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CheckResultSetGettersWorkForDefinedPropertiesHandlerTest.java
@@ -146,7 +146,6 @@ public class CheckResultSetGettersWorkForDefinedPropertiesHandlerTest
         EasyMock.expect(t_Table.getName()).andReturn("dao");
         EasyMock.expect(t_ResultDAO.findByPrimaryKey(t_Result.getId())).andReturn(t_Result).anyTimes();
         EasyMock.expect(t_Statement.executeQuery()).andReturn(t_ResultSet);
-        EasyMock.expect(t_ResultSet.getMetaData()).andReturn(t_Metadata);
         EasyMock.expect(t_ResultSet.next()).andReturn(true);
         EasyMock.expect(t_Metadata.getColumnCount()).andReturn(t_lProperties.size());
 
