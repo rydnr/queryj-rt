d4901e387b5a9420ca91f16b8e46691b9a2ab765
chous <chous@acm-sl.org>
2014-03-16 16:24:54 +0100

    201403161624

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandlerTest.java
@@ -119,6 +119,9 @@ public class CacheValidationOutcomeHandlerTest
                 statementFlagsDAO,
                 resultSetFlagsDAO);
 
+        EasyMock.expect(resultDAO.findBySqlId(t_Sql.getId())).andReturn(t_Result);
+        EasyMock.replay(resultDAO);
+
         @NotNull final QueryJCommand t_Command = new ConfigurationQueryJCommandImpl(new PropertiesConfiguration());
 
         new QueryJCommandWrapper<File>(t_Command).setSetting(
