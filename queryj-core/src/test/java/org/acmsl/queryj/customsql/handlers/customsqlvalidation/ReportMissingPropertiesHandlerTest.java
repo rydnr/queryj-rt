c49b47d09fca7f5787d7d35430629fd413cc3e5d
chous <chous@acm-sl.org>
2014-03-16 12:57:29 +0100

    201403161257

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandlerTest.java
@@ -109,7 +109,7 @@ public class ReportMissingPropertiesHandlerTest
 
         @NotNull final Log t_Log = EasyMock.createNiceMock(Log.class);
 
-        Mockito.mockStatic(UniqueLogFactory.class);
+        PowerMock.mockStatic(UniqueLogFactory.class);
         Mockito.when(UniqueLogFactory.getLog(ReportMissingPropertiesHandler.class)).thenReturn(t_Log);
 
         t_Log.warn(EasyMock.anyObject());
