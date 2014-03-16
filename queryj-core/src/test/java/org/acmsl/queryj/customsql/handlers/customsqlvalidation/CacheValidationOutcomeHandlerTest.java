41cda779c3333b6b480b0cc5b40e57a442f47d62
chous <chous@acm-sl.org>
2014-03-16 16:23:45 +0100

    201403161623

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandlerTest.java
@@ -209,7 +209,7 @@ public class CacheValidationOutcomeHandlerTest
 
         @NotNull final Charset t_Charset = Charset.defaultCharset();
 
-        @NotNull final String hash = t_CustomSqlProvider.getHash(t_Sql, t_Charset.displayName());
+        @NotNull final String hash = "bzS4lagreKYbqR9tX8G2d5CCYGA%3D";
 
         @NotNull final String path = instance.hashPath(tempFolder.getRoot().getAbsolutePath(), hash);
 
