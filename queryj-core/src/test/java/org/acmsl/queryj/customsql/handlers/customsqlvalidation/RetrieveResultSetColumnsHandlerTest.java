4a59dae028e65736fc9993be0209aa03d730f159
chous <chous@acm-sl.org>
2014-03-16 11:46:29 +0100

    201403161146

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultSetColumnsHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultSetColumnsHandlerTest.java
@@ -116,7 +116,7 @@ public class RetrieveResultSetColumnsHandlerTest
 
         for (@NotNull final Property<String> t_Property : t_lProperties)
         {
-            EasyMock.expect(t_PropertyDAO.findByPrimaryKey(t_Property.getId())).andReturn(t_Property);
+            EasyMock.expect(t_ResultSetMetaData.getColumnCount()
             if (t_Property.getType().equals(String.class.getSimpleName()))
             {
                 EasyMock.expect(t_ResultSet.getString(t_Property.getColumnName())).andReturn("1");
