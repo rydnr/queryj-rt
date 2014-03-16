3873923e0829d98ec7f8e7ea8e597b4844b41f8e
chous <chous@acm-sl.org>
2014-03-16 15:22:04 +0100

    201403161522

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/exceptions/ResultSetGettersValidationNotAvailableException.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/exceptions/ResultSetGettersValidationNotAvailableException.java
@@ -49,7 +49,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Represents the
+ * Represents the error when trying to
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/16 15:20
