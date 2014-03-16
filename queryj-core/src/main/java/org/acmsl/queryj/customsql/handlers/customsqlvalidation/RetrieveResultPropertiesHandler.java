cb67eaca2d275afd3f9147aa9b016d9b51b52641
chous <chous@acm-sl.org>
2014-03-16 06:38:09 +0100

    201403160638

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
@@ -118,6 +118,8 @@ public class RetrieveResultPropertiesHandler
     {
         @NotNull final ResultSet t_ResultSet = new ExecuteQueryHandler().retrieveCurrentResultSet(command);
         @NotNull final Result<String> t_Result;
+        @NotNull final Sql<String> t_Sql = new RetrieveQueryHandler().retrieveCurrentSql(command);
+        @NotNull final CustomSqlProvider t_CustomSqlProvider = retrieveCustomSqlProvider()
         validateResultSet(t_ResultSet, t_Sql, t_Result, t_CustomSqlProvider, t_MetadataManager, new JdbcTypeManager());
         return false;
     }
