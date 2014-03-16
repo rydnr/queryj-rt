0e3c995a18e6e639d769c29243ee9b65e7ea5ef1
chous <chous@acm-sl.org>
2014-03-16 18:40:06 +0100

    201403161840

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/CustomSqlValidationChainTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/CustomSqlValidationChainTest.java
@@ -74,6 +74,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class CustomSqlValidationChainTest
 {
+    @SuppressWarnings("unchecked")
     @Test
     public void includes_required_handlers()
         throws QueryJBuildException
