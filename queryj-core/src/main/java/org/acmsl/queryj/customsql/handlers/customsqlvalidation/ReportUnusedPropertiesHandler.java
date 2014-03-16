a9af274e9f44dc5b73af95b0564ad901277d2253
chous <chous@acm-sl.org>
2014-03-16 13:13:34 +0100

    201403161313

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportUnusedPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportUnusedPropertiesHandler.java
@@ -152,6 +152,6 @@ public class ReportUnusedPropertiesHandler
     protected List<Property<String>> detectExtraProperties(
         @NotNull final List<Property<String>> properties, @NotNull final List<Property<String>> resultSetProperties)
     {
-        return new ReportMissingPropertiesHandler(resultSetProperties, properties);
+        return new ReportMissingPropertiesHandler().detectMissingProperties(resultSetProperties, properties);
     }
 }
