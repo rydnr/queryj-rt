61cf7c3bca25bb5a7182e2d294ec55b9acd942ac
chous <chous@acm-sl.org>
2014-03-16 13:01:17 +0100

    201403161301

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandlerTest.java
@@ -55,7 +55,6 @@ import org.jetbrains.annotations.NotNull;
 /*
  * Importing checkthread.org annotations.
  */
-import org.checkthread.annotations.ThreadSafe;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Assert;
 import org.junit.Test;
