8ddc24e835b8f48fb252c28b7efe35bb5137971f
chous <chous@acm-sl.org>
2014-03-16 07:37:12 +0100

    201403160737

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
@@ -210,7 +210,6 @@ public class RetrieveResultPropertiesHandler
             {
                 throw new CustomSqlWithNoPropertiesException(sql);
             }
-
         }
         else
         {
