89a9fefd27a84e7790c807da9af98bdfd56aacf7
chous <chous@acm-sl.org>
2014-03-16 13:02:57 +0100

    201403161302

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/ReportMissingPropertiesHandlerTest.java
@@ -80,7 +80,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Tests for {@link RetrieveResultSetColumnsHandler}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/16 10:32
