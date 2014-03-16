2af4109afcccab006338e5b24de05c0cc4cfc8d6
chous <chous@acm-sl.org>
2014-03-16 16:11:39 +0100

    201403161611

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandlerTest.java
@@ -102,7 +102,17 @@ public class CacheValidationOutcomeHandlerTest
         @NotNull final Sql<String> t_Sql =
             new SqlElement<>("sql-id", "dao", "sql-name", "select", Cardinality.SINGLE, "all", true, false, "fake sql");
 
-\        @NotNull final CustomSqlProvider t_CustomSqlProvider =
+        @NotNull final Result<String> t_Result1 = new ResultElement<>("p1", "class1");
+
+        @NotNull final SqlDAO sqlDAO = EasyMock.createNiceMock(SqlDAO.class);
+        @NotNull final SqlParameterDAO parameterDAO = EasyMock.createNiceMock(SqlParameterDAO.class);
+        @NotNull final SqlPropertyDAO propertyDAO = EasyMock.createNiceMock(SqlPropertyDAO.class);
+        @NotNull final SqlResultDAO resultDAO = EasyMock.createNiceMock(SqlResultDAO.class);
+        @NotNull final SqlConnectionFlagsDAO connectionFlagsDAO = EasyMock.createNiceMock(SqlConnectionFlagsDAO.class);
+        @NotNull final SqlStatementFlagsDAO statementFlagsDAO = EasyMock.createNiceMock(SqlStatementFlagsDAO.class);
+        @NotNull final SqlResultSetFlagsDAO resultSetFlagsDAO = EasyMock.createNiceMock(SqlResultSetFlagsDAO.class);
+
+        @NotNull final CustomSqlProvider t_CustomSqlProvider =
             new SemiMockedAbstractCustomSqlProvider(
                 sqlDAO,
                 parameterDAO,
