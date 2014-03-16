d759aa722d700f414aa7065797ce6be837c8dc3a
chous <chous@acm-sl.org>
2014-03-16 18:50:17 +0100

    201403161850

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/CustomSqlValidationChainTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/CustomSqlValidationChainTest.java
@@ -84,7 +84,7 @@ public class CustomSqlValidationChainTest
         @NotNull final CustomSqlValidationChain instance = new CustomSqlValidationChain();
 
         @NotNull final Chain<QueryJCommand, QueryJBuildException, QueryJCommandHandler<QueryJCommand>> t_Chain =
-            EasyMock.createNiceMock(Chain.class);
+            new
         EasyMock.expect(t_Chain.add((QueryJCommandHandler<QueryJCommand>) EasyMock.anyObject())).a
                                     EasyMock.replay(t_Chain);
 
