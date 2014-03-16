ef1a922c16534c9f92e33db265d68a6f29167c9d
chous <chous@acm-sl.org>
2014-03-16 18:41:04 +0100

    201403161841

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/CustomSqlValidationChainTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/CustomSqlValidationChainTest.java
@@ -100,7 +100,7 @@ public class CustomSqlValidationChainTest
         Assert.assertTrue(t_Chain.contains(SkipValidationIfCacheExistsHandler.class));
     }
 
-    private boolean contains(final BindQueryParametersHandler handler, final Chain chain)
+    protected boolean contains(final BindQueryParametersHandler handler, final Chain chain)
     {
         return false;  //To change body of created methods use File | Settings | File Templates.
     }
