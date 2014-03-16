162d494e31c9b1c805b95835b6cc9c25f3d4b514
chous <chous@acm-sl.org>
2014-03-16 18:53:05 +0100

    201403161853

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/CustomSqlValidationChainTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/CustomSqlValidationChainTest.java
@@ -106,7 +106,7 @@ public class CustomSqlValidationChainTest
 
     protected boolean contains(
         @NotNull final Class<?> handlerClass,
-        @NotNull final QueryJChain<QueryJCommandHandler<QueryJCommand>> chain)
+        @NotNull final Chain<QueryJCommand, QueryJCommandHandler<QueryJCommand>> chain)
     {
         boolean result = false;
 
