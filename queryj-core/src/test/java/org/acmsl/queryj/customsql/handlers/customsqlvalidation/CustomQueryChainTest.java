144580cf5813a4e570bc6b93241359641099e862
chous <chous@acm-sl.org>
2014-03-17 08:22:17 +0100

    201403170822

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CustomQueryChainTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CustomQueryChainTest.java
@@ -48,7 +48,6 @@ import org.jetbrains.annotations.NotNull;
 /*
  * Importing checkthread.org annotations.
  */
-import org.checkthread.annotations.ThreadSafe;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Assert;
 import org.junit.Test;
