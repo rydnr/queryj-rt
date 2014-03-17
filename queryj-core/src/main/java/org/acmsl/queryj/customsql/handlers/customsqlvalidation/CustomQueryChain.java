fed9ec915c78917661a4292c023ec6ba183d6dec
chous <chous@acm-sl.org>
2014-03-17 08:21:07 +0100

    201403170821

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CustomQueryChain.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CustomQueryChain.java
@@ -27,7 +27,7 @@
  *
  * Author: Jose San Leandro Armendariz
  *
- * Description: Runs a list of steps to validate a custom {@link org.acmsl.queryj.customsql.Sql query}.
+ * Description: Runs a list of steps to validate a custom query.
  *
  * Date: 2014/03/17
  * Time: 08:15
