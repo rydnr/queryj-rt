ed151fea2839ff214fdd0b3173c85e541cb1f1d4
chous <chous@acm-sl.org>
2014-03-17 08:20:09 +0100

    201403170820

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CustomQueryChain.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CustomQueryChain.java
@@ -51,7 +51,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- *
+ * Iterates through the
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/17 08:15
