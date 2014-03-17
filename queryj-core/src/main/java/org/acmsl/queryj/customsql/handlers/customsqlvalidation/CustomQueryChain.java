55a5af97a9b5980d21b0c12c1e5f309dbe5056e3
chous <chous@acm-sl.org>
2014-03-17 08:21:19 +0100

    201403170821

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CustomQueryChain.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CustomQueryChain.java
@@ -39,10 +39,18 @@ package org.acmsl.queryj.customsql.handlers.customsqlvalidation;
  * Importing JetBrains annotations.
  */
 import org.acmsl.commons.patterns.Chain;
+
+/*
+ * Importing JetBrains annotations.
+ */
 import org.acmsl.queryj.AbstractQueryJChain;
 import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.api.exceptions.QueryJBuildException;
 import org.acmsl.queryj.tools.handlers.QueryJCommandHandler;
+
+/*
+ * Importing JetBrains annotations.
+ */
 import org.jetbrains.annotations.NotNull;
 
 /*
