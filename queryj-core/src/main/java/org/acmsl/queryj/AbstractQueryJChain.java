225ebf3aac79ea8f84339f839f34d0a2c978781e
chous <chous@acm-sl.org>
2014-03-17 06:59:07 +0100

    201403170659

--- queryj-core/src/main/java/org/acmsl/queryj/AbstractQueryJChain.java
+++ queryj-core/src/main/java/org/acmsl/queryj/AbstractQueryJChain.java
@@ -256,7 +256,7 @@ public abstract class AbstractQueryJChain
     {
         return
               "{ \"class\": \"" + AbstractQueryJChain.class.getSimpleName() + '"'
-        + ", \"package\": \""
+        + ", \"package\": \"org.acmsl.queryj"
             + ", \"chain\": " + getChain() + " }";
     }
 }
