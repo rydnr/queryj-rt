e1b8b864a42086e0ec861edc5e1e1d9393050ece
chous <chous@acm-sl.org>
2014-03-18 11:14:14 +0100

    201403181114

--- queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/InvalidColumnNameInCustomResultExceptionTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/InvalidColumnNameInCustomResultExceptionTest.java
@@ -50,7 +50,7 @@ import org.acmsl.queryj.customsql.SqlElement;
 import org.jetbrains.annotations.NotNull;
 
 /*
- * Importing checkthread.org annotations.
+ * Importing JUnit classes.
  */
 import org.junit.Test;
 import org.junit.runner.RunWith;
