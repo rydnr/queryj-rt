4ea338278c1aaac29834ae78ab0489bd0a04f343
chous <chous@acm-sl.org>
2014-03-18 11:49:55 +0100

    201403181149

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveQueryHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveQueryHandler.java
@@ -120,7 +120,7 @@ public class RetrieveQueryHandler
 
         if (t_Log != null)
         {
-            t_Log.info("Validating " + t_iTotalQueries + " queries. It c";
+            t_Log.info("Validating " + t_iTotalQueries + " queries. It can take ";
         }
         while (   (t_iIndex > -1)
                && (t_iIndex < t_lSql.size()))
