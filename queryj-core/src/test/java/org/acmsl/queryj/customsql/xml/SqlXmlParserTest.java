9a401a32e9308fdcd9b99e77fe1da6b9a3e9cff7
chous <chous@acm-sl.org>
2014-03-18 09:34:00 +0100

    201403180934

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/xml/SqlXmlParserTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/xml/SqlXmlParserTest.java
@@ -70,35 +70,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/*
-This file is part of  JUnitDoclet, a project to generate basic
-test cases  from source code and  helping to keep them in sync
-during refactoring.
-
-Copyright (C) 2002-2005  ObjectFab GmbH  (http://www.objectfab.de/)
-
-This library is  free software; you can redistribute it and/or
-modify  it under the  terms of  the  GNU Lesser General Public
-License as published  by the  Free Software Foundation; either
-version 2.1  of the  License, or  (at your option)  any  later
-version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You  should  have  received a  copy of the  GNU Lesser General
-Public License along with this  library; if not, write  to the
-Free  Software  Foundation, Inc.,  59 Temple Place,  Suite 330,
-Boston, MA  02111-1307  USA
-*/
-
-
 /**
-* Tests SqlXmlParserTest class.
-* @see org.acmsl.queryj.customsql.xml.SqlXmlParser
-*/
+ * Tests SqlXmlParserTest class.
+ * @see org.acmsl.queryj.customsql.xml.SqlXmlParser
+ */
 @RunWith(JUnit4.class)
 public class SqlXmlParserTest
 {
