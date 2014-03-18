f1527c0bb30601aa2e5bce761deea43bf5302b3f
chous <chous@acm-sl.org>
2014-03-18 08:25:24 +0100

    201403180825

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/SetupPreparedStatementHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/SetupPreparedStatementHandler.java
@@ -183,7 +183,7 @@ public class SetupPreparedStatementHandler
         }
         else
         {
-            throw new CustomQueryNotSuitable
+            throw new CustomQueryNotSuitableForValidation(sql)
             @Nullable final Log t_Log = UniqueLogFactory.getLog(CustomSqlValidationHandler.class);
 
             if (t_Log != null)
