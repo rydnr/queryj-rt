921467a33bc2505df595d4f19c8934457c5aaabe
chous <chous@acm-sl.org>
2014-03-18 10:35:30 +0100

    201403181035

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
@@ -383,7 +383,7 @@ public class RetrieveResultPropertiesHandler
             if (metadataManager.isInvalidColumnNameException(cannotRetrieveColumnValue))
             {
                 throw
-                    new InvalidCustomResultPropertyTypeException(
+                    new CustomResultWithInvalidNumberOfColumnsException()
                         property, sql, sqlResult, cannotRetrieveColumnValue);
             }
             else if (metadataManager.isInvalidColumnNameException(cannotRetrieveColumnValue))
