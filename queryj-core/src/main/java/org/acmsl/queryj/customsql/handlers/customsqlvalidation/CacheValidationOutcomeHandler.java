fcd8b453b0c8e1f2b733be6da6383c7a78846b56
chous <chous@acm-sl.org>
2014-03-18 08:03:23 +0100

    201403180803

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CacheValidationOutcomeHandler.java
@@ -144,7 +144,7 @@ public class CacheValidationOutcomeHandler
         @NotNull final String path = hashPath(outputFolder.getAbsolutePath(), hash);
 
         if (   (!existsAlready(path))
-            && (!path.equals(outputFolder.getName())))
+            && (!path.equals(outputFolder.getAbsolutePath())))
         {
             new File(outputFolder.getAbsolutePath()).mkdirs();
             FileUtils.getInstance().writeFileIfPossible(path, "", charset);
