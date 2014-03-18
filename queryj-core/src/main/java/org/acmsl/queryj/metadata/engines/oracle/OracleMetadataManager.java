ca42d4a10fa6870fa283a5db0c05df6bf5ee8003
chous <chous@acm-sl.org>
2014-03-18 10:31:00 +0100

    201403181031

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/oracle/OracleMetadataManager.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/oracle/OracleMetadataManager.java
@@ -692,7 +692,7 @@ public class OracleMetadataManager
         return sqlException.getErrorCode() == 17006;
     }
 
-    public boolean isInvalidColumnTypeException(final SQLException sqlException)
+    public boolean isInvalidColumnTypeException(@NotNull final SQLException sqlException)
     {
         return false;  //To change body of created methods use File | Settings | File Templates.
     }
