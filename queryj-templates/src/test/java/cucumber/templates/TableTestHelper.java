f480b8df2311ad70c5f23b6faa900de36c530589
chous <chous@acm-sl.org>
2014-03-21 19:35:25 +0100

    201403211935

--- queryj-templates/src/test/java/cucumber/templates/TableTestHelper.java
+++ queryj-templates/src/test/java/cucumber/templates/TableTestHelper.java
@@ -188,8 +188,7 @@ public class TableTestHelper
                     tableEntry.get(PARENT_TABLE),
                     tableEntry.get(STATIC),
                     isNullOrBlank(tableEntry.get(DECORATED)),
-                    isNullOrBlank(tableEntry.get(RELATIOSHIP))
-                );
+                    isNullOrBlank(tableEntry.get(RELATIOSHIP)));
         }
 
         return result;
