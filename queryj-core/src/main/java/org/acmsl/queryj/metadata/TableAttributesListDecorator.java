ce71c3002bde4a84599eb11acacdf4e991277e99
chous <chous@acm-sl.org>
2014-03-21 19:04:05 +0100

    201403211904

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableAttributesListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableAttributesListDecorator.java
@@ -125,6 +125,9 @@ public class TableAttributesListDecorator
         throw new RuntimeException(INVALID_OPERATION);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @NotNull
     @Override
     public List<Row<DecoratedString>> getStaticContent()
