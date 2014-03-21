52bdf889ca784d35eb8df4a978a486cd31c107bd
chous <chous@acm-sl.org>
2014-03-21 19:17:40 +0100

    201403211917

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableAttributesPartialListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableAttributesPartialListDecorator.java
@@ -210,6 +210,9 @@ public class TableAttributesPartialListDecorator
         throw new RuntimeException(AbstractTableAttributesListDecorator.INVALID_OPERATION);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @NotNull
     public PartialListDecorator plus()
     {
