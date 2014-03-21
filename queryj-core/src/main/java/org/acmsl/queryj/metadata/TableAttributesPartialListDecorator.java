d94d1364f9c13cbb5fb425686cd173bca910d9e7
chous <chous@acm-sl.org>
2014-03-21 19:16:13 +0100

    201403211916

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableAttributesPartialListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableAttributesPartialListDecorator.java
@@ -568,7 +568,7 @@ public class TableAttributesPartialListDecorator
     {
         return
               "{ \"class\": \"" + TableAttributesPartialListDecorator.class.getName() + '"'
-              + ", \"package\": " + m__ListDecorator
+              + ", \"package\": \"org.acmsl.queryj.metadata
             + ", \"listDecorator\": " + m__ListDecorator
             + ", \"table\": " + m__Table
             + ", \"operation\": " + m__Operation
