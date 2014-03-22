8920c57e18d58708f8e0555881ec32467eb6052f
chous <chous@acm-sl.org>
2014-03-22 17:50:39 +0100

    201403221750

--- queryj-templates/src/test/java/cucumber/templates/TableTestHelper.java
+++ queryj-templates/src/test/java/cucumber/templates/TableTestHelper.java
@@ -580,7 +580,6 @@ public class TableTestHelper
             @NotNull final String dao = sqlRow.get("dao");
             @NotNull final String type = sqlRow.get("type");
             @Nullable final String matches = sqlRow.get("matches");
-
             @NotNull final String validate = sqlRow.get("validate");
             @NotNull final Cardinality cardinality = Cardinality.fromString(matches.toLowerCase(Locale.US));
             @NotNull final String value = sqlRow.get(VALUE);
