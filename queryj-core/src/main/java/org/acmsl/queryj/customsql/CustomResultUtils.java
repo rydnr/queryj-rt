6bb89b14a0884aebc57e9f116ab446a608ad00ab
chous <chous@acm-sl.org>
2014-03-23 12:11:00 +0100

    201403231211

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/CustomResultUtils.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/CustomResultUtils.java
@@ -377,7 +377,7 @@ public class CustomResultUtils
         if (t_strDao != null)
         {
             @Nullable Table<String, Attribute<String>, List<Attribute<String>>> t_Table =
-                tableDAO().findByDAO("" + t_strDao);
+                tableDAO.findByDAO("" + t_strDao);
 
             if  (t_Table == null)
             {
