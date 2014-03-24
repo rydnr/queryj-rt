fec3454ef8019fa1c464fe84438f614b5f2742ff
chous <chous@acm-sl.org>
2014-03-24 19:34:21 +0100

    201403241934

--- queryj-placeholders/src/test/java/org/acmsl/queryj/placeholders/DecoratedStringTest.java
+++ queryj-placeholders/src/test/java/org/acmsl/queryj/placeholders/DecoratedStringTest.java
@@ -125,7 +125,7 @@ public class DecoratedStringTest
     }
 
     /**
-     * Tests {@link DecoratedString#getSingular().getCamelCase()}
+     * Tests {@code DecoratedString#getSingular().getCamelCase()}
      */
     @Test
     public void singleLine_works()
