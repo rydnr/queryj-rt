91d60468a4b405fb5056db34f51c195d36811094
chous <chous@acm-sl.org>
2014-03-24 07:40:36 +0100

    201403240740

--- queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/ExternallyManagedFieldsRetrievalHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/ExternallyManagedFieldsRetrievalHandler.java
@@ -84,7 +84,6 @@ public class ExternallyManagedFieldsRetrievalHandler
      *
      * @param parameters the parameters.
      * @return <code>true</code> if the chain should be stopped.
-     * @throws QueryJBuildException if the build process cannot be performed.
      */
     @Override
     public boolean handle(@NotNull final QueryJCommand parameters)
