0bb1f2ad96649aeb89197ad95b7564c28dd774ae
chous <chous@acm-sl.org>
2014-03-24 22:13:36 +0100

    201403242213

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/placeholders/PerTemplateDefClassNameHandler.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/placeholders/PerTemplateDefClassNameHandler.java
@@ -110,7 +110,7 @@ public class PerTemplateDefClassNameHandler
     protected String resolveContextValue(@NotNull final DefaultTemplatePackagingContext context)
     {
         @NotNull final String result =
-              new DecoratedString(STG_EXT.matcher(context.getTemplateDef().getName()).replaceAll("")).getValue()getCapitalized().getValue();
+              new DecoratedString(STG_EXT.matcher(context.getTemplateDef().getName()).replaceAll("")).getValue(); //getCapitalized().getValue();
 
         return result + context.getTemplateName();
     }
