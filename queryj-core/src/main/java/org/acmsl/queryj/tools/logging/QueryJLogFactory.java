56b8a79074f84ff218381025558c289d52bdb2f1
chous <chous@acm-sl.org>
2014-03-24 08:00:56 +0100

    201403240800

--- queryj-core/src/main/java/org/acmsl/queryj/tools/logging/QueryJLogFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/logging/QueryJLogFactory.java
@@ -1,6 +1,6 @@
 //;-*- mode: java -*-
 /*
-                        QueryJ-Core
+                        QueryJ Core
 
     Copyright (C) 2002-today  Jose San Leandro Armendariz
                               chous@acm-sl.org
