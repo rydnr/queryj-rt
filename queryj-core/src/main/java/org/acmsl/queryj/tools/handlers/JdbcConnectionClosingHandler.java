8e4e8dce2a22a59bb113164d71286c07b6d900e3
chous <chous@acm-sl.org>
2014-03-24 07:30:34 +0100

    201403240730

--- queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/JdbcConnectionClosingHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/JdbcConnectionClosingHandler.java
@@ -101,7 +101,6 @@ public class JdbcConnectionClosingHandler
      * Handles given information.
      * @param parameters the parameters.
      * @return <code>true</code> if the chain should be stopped.
-     * @throws QueryJBuildException the build process cannot be performed.
      */
     @Override
     public boolean handle(@NotNull final QueryJCommand parameters)
