44aa9870231055ab02772b2ee187dcb998d6cbb2
chous <chous@acm-sl.org>
2014-03-24 07:36:56 +0100

    201403240736

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/xml/ResultRefElementFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/xml/ResultRefElementFactory.java
@@ -78,10 +78,6 @@ public class ResultRefElementFactory
      * @param digester the Digester instance.
      * @param conversionUtils the ConversionUtils instance.
      * @return the &lt;result-ref&gt; information.
-     * @throws SAXException if the attributes are not valid.
-     * @precondition attributes != null
-     * @precondition digester != null
-     * @precondition conversionUtils != null
      */
     @Nullable
     public Object createObject(
