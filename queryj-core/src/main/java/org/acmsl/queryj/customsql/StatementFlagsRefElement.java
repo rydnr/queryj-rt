2ba5068708fce0e0295685617d50fd5aff43e879
chous <chous@acm-sl.org>
2014-03-24 07:42:26 +0100

    201403240742

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/StatementFlagsRefElement.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/StatementFlagsRefElement.java
@@ -56,6 +56,9 @@ public class StatementFlagsRefElement
     extends  AbstractIdElement<String>
     implements StatementFlagsRef
 {
+    /**
+     * The serial version id.
+     */
     private static final long serialVersionUID = 4902641145472579846L;
 
     /**
