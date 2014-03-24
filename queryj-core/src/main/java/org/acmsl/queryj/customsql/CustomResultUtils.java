e8a66cbec6969e4f19a9192b4e122fa5c8bd820f
chous <chous@acm-sl.org>
2014-03-24 19:02:03 +0100

    201403241902

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/CustomResultUtils.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/CustomResultUtils.java
@@ -365,7 +365,7 @@ public class CustomResultUtils
     /**
      * Retrieves the table associated to the {@link Sql}.
      * @param sql the {@link Sql}.
-     * @param tableDAO the database metadata manager.
+     * @param tableDAO the {@link TableDAO}.
      * @return the table name.
      */
     @Nullable
