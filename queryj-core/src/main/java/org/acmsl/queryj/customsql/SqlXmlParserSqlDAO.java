2946008f1458eeaa9b153edead13049fdd00eb90
chous <chous@acm-sl.org>
2014-03-24 18:58:32 +0100

    201403241858

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/SqlXmlParserSqlDAO.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/SqlXmlParserSqlDAO.java
@@ -83,7 +83,7 @@ public class SqlXmlParserSqlDAO
     private static final Map<String, Boolean> PRIMARY_KEY_MISS_CACHE = new HashMap<>();
 
     /**
-     * The cache of resultId -&gt; List<Sql<String>>
+     * The cache of resultId -&gt; List<Sql<String&gt;&gt;
      */
     private static final Map<String, List<Sql<String>>> FIND_BY_RESULTID_CACHE = new HashMap<>();
 
