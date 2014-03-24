57eb7703ec58eb255249fe31f676c33584de25ae
chous <chous@acm-sl.org>
2014-03-24 19:12:52 +0100

    201403241912

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerTableTemplateBuildHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerTableTemplateBuildHandler.java
@@ -33,7 +33,7 @@
 package org.acmsl.queryj.api.handlers;
 
 /*
- * Importing some project classes.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.QueryJCommandWrapper;
