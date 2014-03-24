01a2de17e125413df04f3e1e0bf09f4cdaa0ef7d
chous <chous@acm-sl.org>
2014-03-24 19:29:20 +0100

    201403241929

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/AntProjectAdapter.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/AntProjectAdapter.java
@@ -1474,7 +1474,6 @@ public class AntProjectAdapter
      * @param length the buffer length.
      * @param project the {@link Project} instance.
      * @return the number of bytes processed.
-     * @throws IOException in some cases.
      */
     protected int demuxInput(
         @NotNull final byte[] buffer, final int offset, final int length, @NotNull final Project project)
