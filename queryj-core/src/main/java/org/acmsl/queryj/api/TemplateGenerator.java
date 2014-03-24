83829873f2e04675d612cb49afd2de986eaafb1f
chous <chous@acm-sl.org>
2014-03-24 08:01:20 +0100

    201403240801

--- queryj-core/src/main/java/org/acmsl/queryj/api/TemplateGenerator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/TemplateGenerator.java
@@ -59,8 +59,6 @@ public interface TemplateGenerator<T extends Template<C>, C extends TemplateCont
      * @param rootFolder the root folder.
      * @param charset the file encoding.
      * @return <code>true</code> if it actually generated the file.
-     * @throws IOException if the file cannot be created.
-     * @throws QueryJBuildException if the generation process fails.
      */
     boolean write(
         @NotNull final T template,
