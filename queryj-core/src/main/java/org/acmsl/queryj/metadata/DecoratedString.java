d3462b6f1074061e410f4dd49d240beb2e76863f
chous <chous@acm-sl.org>
2014-03-24 22:21:23 +0100

    201403242221

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/DecoratedString.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/DecoratedString.java
@@ -86,7 +86,11 @@ public class DecoratedString
      * A regex for replacing new lines.
      */
     protected static final Pattern NEWLINE_PATTERN = Pattern.compile("\\s*\\n\\s*");
-    private static final Pattern EXT_REGEX = Pattern.compile(".*(\\.*)$");
+
+    /**
+     * A regex for removing the extension.
+     */
+    protected static final Pattern EXT_REGEX = Pattern.compile(".*(\\.*)$");
 
     /**
      * The actual value.
