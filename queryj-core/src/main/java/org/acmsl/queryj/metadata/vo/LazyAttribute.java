e5192753524fb8e24b98412e3f7b7d2f73ab05e0
chous <chous@acm-sl.org>
2014-03-24 18:42:53 +0100

    201403241842

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/vo/LazyAttribute.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/vo/LazyAttribute.java
@@ -1045,7 +1045,6 @@ public class LazyAttribute
      * @param o the object to be compared.
      * @return a negative integer, zero, or a positive integer as this object
      *         is less than, equal to, or greater than the specified object.
-     * @throws ClassCastException if the specified object's type prevents it
      *                            from being compared to this object.
      */
     @Override
