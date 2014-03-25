ecb0fc2d1dda3f542a2f536335a34cde2be5691e
chous <chous@acm-sl.org>
2014-03-25 22:35:36 +0100

    201403252235

--- queryj-core/src/main/java/org/acmsl/queryj/api/dao/DAOTemplateUtils.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/dao/DAOTemplateUtils.java
@@ -419,7 +419,7 @@ public class DAOTemplateUtils
 
         if (t_Table != null)
         {
-            @Nullable final Attribute<String> t_strStaticAttribute = t_Table.getStaticAttribute();
+            @Nullable final Attribute<String> t_strStaticAttribute = t_Table
             @Nullable final Attribute<String> t_strStaticAttribute = t_Table.getStaticAttribute();
                 metaLanguageUtils.retrieveStaticAttribute(t_Table.getComment());
 
