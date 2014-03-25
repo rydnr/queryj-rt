adbe20ea6870beda953873c33a369c244e258294
chous <chous@acm-sl.org>
2014-03-25 22:28:20 +0100

    201403252228

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/vo/Table.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/vo/Table.java
@@ -52,7 +52,8 @@ import java.util.List;
 /**
  * Represents <i>table</i> entities in the metadata model.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
- * @param <V> the
+ * @param <V> the type of the String attributes.
+ * @param <A> the type of the attributes.
  */
 public interface Table<V, A extends Attribute<V>, L extends List<A>>
     extends Serializable,
