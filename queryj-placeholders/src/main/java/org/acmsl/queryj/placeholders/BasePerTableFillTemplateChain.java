439e6635d363ac2dbed5f68192572511e16ce1b6
chous <chous@acm-sl.org>
2014-03-27 07:43:56 +0100

    201403270743

--- queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/BasePerTableFillTemplateChain.java
+++ queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/BasePerTableFillTemplateChain.java
@@ -156,7 +156,7 @@ public class BasePerTableFillTemplateChain
 
         result.add(
             new TemplateContextFillHandler<>(
-                new StaticValuesHandler(context))
+                new StaticValuesHandler(context)));
         )
         return result;
     }
