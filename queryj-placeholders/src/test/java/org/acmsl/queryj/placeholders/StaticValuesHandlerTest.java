d49da0aa43cebc651da66a4d01a3e55d5aa3a369
chous <chous@acm-sl.org>
2014-03-27 07:42:21 +0100

    201403270742

--- queryj-placeholders/src/test/java/org/acmsl/queryj/placeholders/StaticValuesHandlerTest.java
+++ queryj-placeholders/src/test/java/org/acmsl/queryj/placeholders/StaticValuesHandlerTest.java
@@ -46,6 +46,10 @@ import org.acmsl.queryj.api.PerTableTemplateContext;
 import org.acmsl.queryj.api.exceptions.QueryJBuildException;
 import org.acmsl.queryj.metadata.vo.Row;
 import org.acmsl.queryj.metadata.vo.RowValueObject;
+
+/*
+ * Importing JetBrains annotations.
+ */
 import org.jetbrains.annotations.NotNull;
 
 /*
