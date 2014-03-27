898e9acfab6fa39bdaf15b5ff4f13cb7451840e3
chous <chous@acm-sl.org>
2014-03-27 07:29:46 +0100

    201403270729

---
+++ queryj-placeholders/src/test/java/org/acmsl/queryj/placeholders/StaticValuesHandlerTest.java
@@ -0,0 +1,78 @@
+/*
+                        QueryJ Placeholders
+
+    Copyright (C) 2002-today  Jose San Leandro Armendariz
+                              chous@acm-sl.org
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: jose.sanleandro@acm-sl.com
+
+ ******************************************************************************
+ *
+ * Filename: StaticValuesHandlerTest.java
+ *
+ * Author: Jose San Leandro Armendariz
+ *
+ * Description: Tests for StaticValuesHandler.
+ *
+ * Date: 2014/03/27
+ * Time: 07:18
+ *
+ */
+package org.acmsl.queryj.placeholders;
+
+/*
+ * Importing QueryJ Core classes.
+ */
+import org.acmsl.queryj.api.PerTableTemplateContext;
+
+/*
+ * Importing JetBrains annotations.
+ */
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * Importing JUnit/PowerMock/EasyMock classes.
+ */
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * Tests for {@link StaticValuesHandler}.
+ * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
+ * @since 3.0
+ * Created: 2014/03/27 07:18
+ */
+@RunWith(PowerMockRunner.class)
+public class StaticValuesHandlerTest
+{
+    @Test
+    public void returns_the_expected_placeholder()
+    {
+        @NotNull final PerTableTemplateContext t_Context = EasyMock.createNiceMock(PerTableTemplateContext.class);
+
+        @NotNull final StaticValuesHandler instance =
+            new StaticValuesHandler(t_Context);
+
+        Assert.assertEquals("static_values", instance.getPlaceHolder());
+    }
+
+
+}
