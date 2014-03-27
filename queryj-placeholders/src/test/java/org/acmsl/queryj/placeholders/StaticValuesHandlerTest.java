6040144bf59aca6e38cad54466743689fc92e0fb
chous <chous@acm-sl.org>
2014-03-27 07:42:30 +0100

    201403270742

--- queryj-placeholders/src/test/java/org/acmsl/queryj/placeholders/StaticValuesHandlerTest.java
+++ queryj-placeholders/src/test/java/org/acmsl/queryj/placeholders/StaticValuesHandlerTest.java
@@ -39,10 +39,6 @@ package org.acmsl.queryj.placeholders;
  * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.api.PerTableTemplateContext;
-
-/*
- * Importing QueryJ Core classes.
- */
 import org.acmsl.queryj.api.exceptions.QueryJBuildException;
 import org.acmsl.queryj.metadata.vo.Row;
 import org.acmsl.queryj.metadata.vo.RowValueObject;
