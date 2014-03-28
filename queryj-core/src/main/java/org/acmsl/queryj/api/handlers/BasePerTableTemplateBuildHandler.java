2265f8579ddbb62a7c22aa435909bc3e19e571a0
chous <chous@acm-sl.org>
2014-03-28 20:12:42 +0100

    201403282012

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerTableTemplateBuildHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerTableTemplateBuildHandler.java
@@ -125,6 +125,7 @@ public abstract class BasePerTableTemplateBuildHandler
     {
         buildTemplate(
             parameters,
+            buildCommand()
             metadataManager,
             retrieveCustomSqlProvider(parameters),
             retrieveTemplateFactory(),
