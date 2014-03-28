b0370d167084db3d03ab6497fa3f55021d9bb8ad
chous <chous@acm-sl.org>
2014-03-28 20:30:51 +0100

    201403282030

--- queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/TableNameNotAvailableExceptionTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/TableNameNotAvailableExceptionTest.java
@@ -66,7 +66,7 @@ public class TableNameNotAvailableExceptionTest
     @Test
     public void exception_message_is_defined_in_Spanish_and_English()
     {
-        @NotNull final BasePackageNameNotAvailableException instance = new BasePackageNameNotAvailableException();
+        @NotNull final TableNameNotAvailableException instance = new BasePackageNameNotAvailableException();
 
         for (@NotNull final Locale t_Locale : Arrays.asList(new Locale("en"), new Locale("es")))
         {
