977f3d911c17a99682335b09c47ab15e9703216e
chous <chous@acm-sl.org>
2014-03-28 18:32:15 +0100

    201403281832

--- queryj-core/src/main/java/org/acmsl/queryj/api/exceptions/BasePackageNameNotAvailableException.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/exceptions/BasePackageNameNotAvailableException.java
@@ -27,7 +27,9 @@
  *
  * Author: Jose San Leandro Armendariz
  *
- * Description: 
+ * Description: Represents a situation in which the information about the base e
+ * name is required (and expected), but it's not available. A bug or
+ * violation of the preconditions/invariants.
  *
  * Date: 2014/03/28
  * Time: 18:30
