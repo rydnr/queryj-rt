073a936a9ba2bb95e7710f4bc359a14ca83e1504
chous <chous@acm-sl.org>
2014-03-28 07:27:55 +0100

    201403280727

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/exceptions/CustomSqlProviderNotAvailableException.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/exceptions/CustomSqlProviderNotAvailableException.java
@@ -47,6 +47,7 @@ import org.checkthread.annotations.ThreadSafe;
 
 /**
  * Represents the situation in which the {@link org.acmsl.queryj.customsql.CustomSqlProvider}
+ * instance is
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/28 07:21
