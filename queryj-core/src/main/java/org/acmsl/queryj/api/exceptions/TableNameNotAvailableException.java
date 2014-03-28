ad53a6fa4ffe1e60a2a44ccec1cc8ba1a1bdd04d
chous <chous@acm-sl.org>
2014-03-28 20:32:35 +0100

    201403282032

--- queryj-core/src/main/java/org/acmsl/queryj/api/exceptions/TableNameNotAvailableException.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/exceptions/TableNameNotAvailableException.java
@@ -46,7 +46,8 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Represents the situation in which the table name information is
+ * Represents the situation in which the table name information was
+ * available and ready to use
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/28 20:31
