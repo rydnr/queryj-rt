71694963a61cdfeeb80cc90b4d94f360e6c56f87
chous <chous@acm-sl.org>
2014-03-28 07:26:05 +0100

    201403280726

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/exceptions/CustomSqlProviderNotAvailableExceptionTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/exceptions/CustomSqlProviderNotAvailableExceptionTest.java
@@ -41,7 +41,7 @@ package org.acmsl.queryj.customsql.exceptions;
 import org.jetbrains.annotations.NotNull;
 
 /*
- * Importing checkthread.org annotations.
+ * Importing JUnit classes.
  */
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
