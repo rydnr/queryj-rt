ea64893a3d98e253a48622131c03d98c00368f60
chous <chous@acm-sl.org>
2014-03-28 07:44:40 +0100

    201403280744

--- queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/DecoratorFactoryNotAvailableExceptionTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/DecoratorFactoryNotAvailableExceptionTest.java
@@ -55,7 +55,7 @@ import java.util.Arrays;
 import java.util.Locale;
 
 /**
- *
+ * Tests for {@link DecoratorFactoryNotAvailableException}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/28 07:43
