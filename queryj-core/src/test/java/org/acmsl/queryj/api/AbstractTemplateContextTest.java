2bbc7bd35cc6ad22b9bff169aee31e113791d60b
chous <chous@acm-sl.org>
2014-03-28 07:37:15 +0100

    201403280737

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateContextTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateContextTest.java
@@ -252,7 +252,7 @@ public class AbstractTemplateContextTest
         new QueryJCommandWrapper<String>(t_Command).setSetting("header", header);
 
         @NotNull final DecoratorFactory decoratorFactory = EasyMock.createNiceMock(DecoratorFactory.class);
-        new QueryJCommandWrapper<DecoratorFactory>(t_Command).setSetting(DecoratorFactory.class.getSimpleName(), );
+        new QueryJCommandWrapper<DecoratorFactory>(t_Command).setSetting(DecoratorFactory.class.getName(), );
         @NotNull final String packageName = "package.name";
         @NotNull final String basePackageName = "base.package.name";
         @NotNull final String repositoryName = "repository.name";
