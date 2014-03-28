1e1fd2cb7801d0793fc7b018f5735ee9901830ef
chous <chous@acm-sl.org>
2014-03-28 19:58:33 +0100

    201403281958

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
@@ -98,7 +98,7 @@ public abstract class AbstractTemplateContext
      * The base package name key.
      */
     protected static final String BASE_PACKAGE_NAME = "basePackageName";
-    private static final String FILE_NAME = "f";
+    private static final String FILE_NAME = "fileName";
 
     /**
      * The command.
