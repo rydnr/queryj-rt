80c6405fcdc1282eecc4ed4a4cbf7ab1d7bb0871
chous <chous@acm-sl.org>
2014-03-28 07:24:46 +0100

    201403280724

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/exceptions/CustomSqlProviderNotAvailableExceptionTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/exceptions/CustomSqlProviderNotAvailableExceptionTest.java
@@ -58,7 +58,7 @@ public class CustomSqlProviderNotAvailableExceptionTest
 {
     public void exception_message_is_defined_in_Spanish_and_English()
     {
-        @NotNull final
+        @NotNull final CustomSqlProviderNotAvailableException instance = new CustomSqlProviderNotAvailableException();
     }
 
 }
