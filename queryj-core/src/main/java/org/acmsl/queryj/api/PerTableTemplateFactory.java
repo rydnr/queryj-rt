d8829c9e2131d91104447814316bf131306a7c81
chous <chous@acm-sl.org>
2014-03-28 20:17:58 +0100

    201403282017

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerTableTemplateFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerTableTemplateFactory.java
@@ -35,6 +35,7 @@ package org.acmsl.queryj.api;
 /*
  * Importing some project classes.
  */
+import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.customsql.CustomSqlProvider;
 import org.acmsl.queryj.metadata.DecoratorFactory;
 import org.acmsl.queryj.metadata.MetadataManager;
@@ -80,6 +81,7 @@ public interface PerTableTemplateFactory<T extends PerTableTemplate<C>, C extend
      */
     @Nullable
     public T createTemplate(
+        @NotNull final QueryJCommand command,
         @NotNull final DecoratorFactory decoratorFactory,
         @NotNull final String packageName,
         @NotNull final String tableName,
