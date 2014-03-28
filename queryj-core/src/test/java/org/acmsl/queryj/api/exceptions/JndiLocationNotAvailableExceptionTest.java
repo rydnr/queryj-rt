80dcb9fb11d9abbec2885ae595dbc953dd0b272e
chous <chous@acm-sl.org>
2014-03-28 19:54:08 +0100

    201403281954

--- queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/JndiLocationNotAvailableExceptionTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/JndiLocationNotAvailableExceptionTest.java
@@ -52,7 +52,7 @@ import java.util.Arrays;
 import java.util.Locale;
 
 /**
- *
+ * Tests for {@link JndiLocationNotAvailableException}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/28 19:52
