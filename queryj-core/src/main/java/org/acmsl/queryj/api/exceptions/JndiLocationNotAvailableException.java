a1f861da1f3358938bee34393beb3f833ff2cea5
chous <chous@acm-sl.org>
2014-03-28 19:56:01 +0100

    201403281956

--- queryj-core/src/main/java/org/acmsl/queryj/api/exceptions/JndiLocationNotAvailableException.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/exceptions/JndiLocationNotAvailableException.java
@@ -36,11 +36,6 @@
 package org.acmsl.queryj.api.exceptions;
 
 /*
- * Importing JetBrains annotations.
- */
-import org.jetbrains.annotations.NotNull;
-
-/*
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
