5737fabf0a35564781e156c48553833d9a938861
chous <chous@acm-sl.org>
2014-03-29 07:49:19 +0100

    201403290749

--- queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/AbstractQueryJCommandHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/AbstractQueryJCommandHandler.java
@@ -47,6 +47,13 @@ import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.customsql.CustomSqlProvider;
 import org.acmsl.queryj.customsql.handlers.CustomSqlProviderRetrievalHandler;
 import org.acmsl.queryj.metadata.MetadataManager;
+import org.acmsl.queryj.tools.exceptions.MetadataManagerNotAvailableException;
+import org.acmsl.queryj.tools.exceptions.MissingConnectionAtRuntimeException;
+import org.acmsl.queryj.tools.exceptions.MissingCustomSqlProviderAtRuntimeException;
+import org.acmsl.queryj.tools.exceptions.MissingDataSourceJndiPathAtRuntimeException;
+import org.acmsl.queryj.tools.exceptions.MissingOutputDirAtRuntimeException;
+import org.acmsl.queryj.tools.exceptions.MissingProjectPackageAtRuntimeException;
+import org.acmsl.queryj.tools.exceptions.MissingRepositoryNameAtRuntimeException;
 
 /*
  * Importing some ACM-SL classes.
