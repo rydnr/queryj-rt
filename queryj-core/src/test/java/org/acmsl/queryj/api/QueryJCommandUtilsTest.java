d7366f2d719726f62c856db36b76c943384d418c
chous <chous@acm-sl.org>
2014-03-29 07:57:48 +0100

    201403290757

--- queryj-core/src/test/java/org/acmsl/queryj/api/QueryJCommandUtilsTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/QueryJCommandUtilsTest.java
@@ -62,6 +62,6 @@ public class QueryJCommandUtilsTest
     {
         @NotNull final QueryJCommandUtils instance = QueryJCommandUtils.getInstance();
 
-        @NotNull final MetadataManager t_MetadataManager = EasyMock.createNiceMock(MetadataManager.class);
+        @NotNull final MetadataManager t_MetadataManager = EasyMock.createMock(MetadataManager.class);
     }
 }
