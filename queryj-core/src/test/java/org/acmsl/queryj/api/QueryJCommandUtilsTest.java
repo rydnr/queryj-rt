0d25da6a385eef5ea76be12877bcbfd3e54587f1
chous <chous@acm-sl.org>
2014-03-29 07:59:27 +0100

    201403290759

--- queryj-core/src/test/java/org/acmsl/queryj/api/QueryJCommandUtilsTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/QueryJCommandUtilsTest.java
@@ -69,6 +69,7 @@ public class QueryJCommandUtilsTest
 
         @NotNull final MetadataManager t_MetadataManager = EasyMock.createMock(MetadataManager.class);
         @NotNull final QueryJCommand t_Command = new ConfigurationQueryJCommandImpl(new PropertiesConfiguration());
-        new QueryJCommandWrapper<MetadataManager>(t_Command).setSetting(DatabaseMetaDataRetrievalHandler.METADATA_MANAGER, t_MetadataManager);
+        new QueryJCommandWrapper<MetadataManager>(t_Command)
+            .setSetting(DatabaseMetaDataRetrievalHandler.METADATA_MANAGER, t_MetadataManager);
     }
 }
