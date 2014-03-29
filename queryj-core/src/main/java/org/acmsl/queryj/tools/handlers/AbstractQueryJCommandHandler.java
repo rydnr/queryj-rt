1d0a6b8d7ccea4828b28d8c6def303c77c17d8e7
chous <chous@acm-sl.org>
2014-03-29 08:17:34 +0100

    201403290817

--- queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/AbstractQueryJCommandHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/AbstractQueryJCommandHandler.java
@@ -181,6 +181,17 @@ public abstract class AbstractQueryJCommandHandler
     }
 
     /**
+     * Retrieves the database metadata manager from the attribute map.
+     * @param parameters the parameter map.
+     * @return the manager.
+     */
+    @NotNull
+    public MetadataManager retrieveMetadataManager(@NotNull final QueryJCommand parameters)
+    {
+        return QueryJCommandUtils.getInstance().retrieveMetadataManager(parameters);
+    }
+
+    /**
      * Retrieves whether to use sub folders or not.
      * @param command the command.
      * @return such flag.
