52e69a845df583a4221467ae8569897a47caa0bf
chous <chous@acm-sl.org>
2014-03-29 08:37:08 +0100

    201403290837

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateContextTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateContextTest.java
@@ -39,9 +39,6 @@ package org.acmsl.queryj.api;
  * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.ConfigurationQueryJCommandImpl;
-import org.acmsl.queryj.QueryJCommand;
-import org.acmsl.queryj.QueryJCommandWrapper;
-import org.acmsl.queryj.QueryJSettings;
 import org.acmsl.queryj.customsql.CustomSqlProvider;
 import org.acmsl.queryj.customsql.handlers.CustomSqlProviderRetrievalHandler;
 import org.acmsl.queryj.metadata.DecoratorFactory;
