794ffb37d0339318b10912a10f249e628d75bd8e
chous <chous@acm-sl.org>
2014-03-30 23:16:29 +0200

    201403302316

--- queryj-template-packaging-maven-plugin/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/JdbcSettingNotAvailableExceptionTest.java
+++ queryj-template-packaging-maven-plugin/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/JdbcSettingNotAvailableExceptionTest.java
@@ -50,7 +50,7 @@ import java.util.Arrays;
 import java.util.Locale;
 
 /**
- *
+ * Tests for {@link JdbcSettingNotAvailableException}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/30 22:13
