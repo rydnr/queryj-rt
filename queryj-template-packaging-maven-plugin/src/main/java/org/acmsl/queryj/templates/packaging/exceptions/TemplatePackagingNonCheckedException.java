8892e12d72e3557c20ea0a919385b9d830918ee8
chous <chous@acm-sl.org>
2014-03-30 21:08:23 +0200

    201403302108

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/TemplatePackagingNonCheckedException.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/TemplatePackagingNonCheckedException.java
@@ -109,6 +109,6 @@ public abstract class TemplatePackagingNonCheckedException
     @Override
     protected String retrieveExceptionsBundleProperty()
     {
-        return "org.acmsl.queryj.templates.packaging.exceptions" super.retrieveExceptionsBundleProperty();
+        return "org.acmsl.queryj.templates.packaging.exceptions";
     }
 }
