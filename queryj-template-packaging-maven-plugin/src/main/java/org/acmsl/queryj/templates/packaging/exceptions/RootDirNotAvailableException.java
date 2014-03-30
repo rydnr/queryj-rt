d40b0bf30fe64c38fcb6e0085a0ccc81254b6677
chous <chous@acm-sl.org>
2014-03-30 21:24:11 +0200

    201403302124

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/RootDirNotAvailableException.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/RootDirNotAvailableException.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Identifies the bug when the root-dir
+ * Identifies the bug when the root-dir information is lost.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/30 20:22
