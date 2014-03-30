cee7671d623ceee656810e1e5e544b7e68a54a9d
chous <chous@acm-sl.org>
2014-03-30 21:07:25 +0200

    201403302107

--- queryj-template-packaging-maven-plugin/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/TemplatePackagingNonCheckedExceptionTest.java
+++ queryj-template-packaging-maven-plugin/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/TemplatePackagingNonCheckedExceptionTest.java
@@ -78,4 +78,3 @@ public class TemplatePackagingNonCheckedExceptionTest
         Assert.assertEquals("org.acmsl.queryj.exceptions", instance.retrieveExceptionsBundleProperty());
     }
 }
-// org.acmsl.queryj.exceptions
