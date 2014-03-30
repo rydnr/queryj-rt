c6fff280198dc447c268583cd4d9c735047045db
chous <chous@acm-sl.org>
2014-03-30 23:40:12 +0200

    201403302340

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/TemplateDefNotAvailableException.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/TemplateDefNotAvailableException.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Used to represent
+ * Used to represent the required TemplateDef is not available.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/30 22:39
