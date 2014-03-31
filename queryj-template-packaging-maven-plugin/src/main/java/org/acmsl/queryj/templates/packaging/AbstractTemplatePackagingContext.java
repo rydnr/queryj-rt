0d9ce40ea25193b23d640bd978eb893771079f43
chous <chous@acm-sl.org>
2014-03-31 07:59:21 +0200

    201403310759

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
@@ -96,6 +96,7 @@ public class AbstractTemplatePackagingContext
     public AbstractTemplatePackagingContext(@NotNull final QueryJCommand command)
     {
         immutableSetCommand(command);
+        immutableSetTemplateName()
     }
 
     /**
