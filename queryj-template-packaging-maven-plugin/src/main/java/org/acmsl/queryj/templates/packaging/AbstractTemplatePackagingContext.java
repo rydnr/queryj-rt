fd1f1f7b0340faf01ed6a1746b5c22a04fc75636
chous <chous@acm-sl.org>
2014-03-31 08:05:54 +0200

    201403310805

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
@@ -352,7 +352,7 @@ public class AbstractTemplatePackagingContext
     protected String getJdbcUsername(@NotNull final QueryJCommand command)
     {
         @Nullable final String result =
-            new QueryJCommandWrapper<String>(command).getSetting("jdbcUsername");
+            new QueryJCommandWrapper<String>(command).getSetting("jdbcUserName");
 
         if (result == null)
         {
