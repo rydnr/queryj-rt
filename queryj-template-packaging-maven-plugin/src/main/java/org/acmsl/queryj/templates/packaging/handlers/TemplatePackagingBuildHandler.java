9f823cd01393fe84c2bec44cc8bce7d2a0dee756
chous <chous@acm-sl.org>
2014-03-31 07:50:53 +0200

    201403310750

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/handlers/TemplatePackagingBuildHandler.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/handlers/TemplatePackagingBuildHandler.java
@@ -137,6 +137,8 @@ public abstract class TemplatePackagingBuildHandler
             "outputDir",
             new File(rootDir.getAbsolutePath() + File.separator + outputPackage.replaceAll("\\.", File.separator)));
 
+        new QueryJCommandWrapper<String>(parameters).setSetting("fileName", fileName);
+
         return new DefaultTemplatePackagingContext(templateDef, parameters);
     }
 
