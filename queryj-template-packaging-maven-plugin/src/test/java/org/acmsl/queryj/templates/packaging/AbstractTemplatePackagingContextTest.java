04f27b93d806adf759e933eb4ccc355408d3ea8c
chous <chous@acm-sl.org>
2014-04-01 22:50:10 +0200

    201404012250

--- queryj-template-packaging-maven-plugin/src/test/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContextTest.java
+++ queryj-template-packaging-maven-plugin/src/test/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContextTest.java
@@ -194,7 +194,7 @@ public class AbstractTemplatePackagingContextTest
             new AbstractTemplatePackagingContext(t_Command)
             {
                 @Override
-                public <T> void immutableSetValue(@NotNull final String key, @NotNull final T value, @NotNull final QueryJCommand command)
+                public <T> void setValue(@NotNull final String key, @NotNull final T value, @NotNull final QueryJCommand command)
                 {
                     super.immutableSetValue(key, value, command);
                 }
