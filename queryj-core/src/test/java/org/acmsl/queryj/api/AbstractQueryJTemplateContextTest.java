5f76ffc2c4be2dcf9814147c40cd251c4455000b
chous <chous@acm-sl.org>
2014-04-01 22:58:14 +0200

    201404012258

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractQueryJTemplateContextTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractQueryJTemplateContextTest.java
@@ -221,7 +221,7 @@ public class AbstractQueryJTemplateContextTest
     @Test
     public void disableCheckthreadAnnotations_is_available()
     {
-        @NotNull final AbstractTemplateContext instance = createContext();
+        @NotNull final AbstractQueryJTemplateContext instance = createContext();
 
         Assert.assertTrue(instance.getDisableCheckthreadAnnotations());
     }
