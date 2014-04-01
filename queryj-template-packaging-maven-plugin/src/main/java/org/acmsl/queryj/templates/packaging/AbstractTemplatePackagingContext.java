0adc22c8bbdc20ec7e03f977e232103dc64c93a1
chous <chous@acm-sl.org>
2014-04-01 12:58:20 +0200

    201404011258

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
@@ -217,7 +217,7 @@ public abstract class AbstractTemplatePackagingContext
     protected <T> T getValue(
         @NotNull final String key,
         @NotNull final QueryJCommand command,
-        @NotNull final Class<? extends QueryJNonCheckedException> exceptionClass)
+        @NotNull final QueryJNonCheckedException exceptionClass)
     {
         @Nullable final T result =
             new QueryJCommandWrapper<T>(command).getSetting(key);
