b06175d876ad55e0781886cd940dead99fd02fde
chous <chous@acm-sl.org>
2014-04-01 22:58:05 +0200

    201404012258

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractQueryJTemplateContextTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractQueryJTemplateContextTest.java
@@ -199,7 +199,7 @@ public class AbstractQueryJTemplateContextTest
     @Test
     public void disableGenerationTimestamps_is_available()
     {
-        @NotNull final AbstractTemplateContext instance = createContext();
+        @NotNull final AbstractQueryJTemplateContext instance = createContext();
 
         Assert.assertTrue(instance.getDisableGenerationTimestamps());
     }
