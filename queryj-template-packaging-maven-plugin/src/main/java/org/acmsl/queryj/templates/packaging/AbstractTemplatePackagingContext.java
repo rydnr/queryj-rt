ffbfe742c281112c07b7046704d29ab9710a0700
chous <chous@acm-sl.org>
2014-04-01 12:55:38 +0200

    201404011255

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
@@ -222,7 +222,7 @@ public abstract class AbstractTemplatePackagingContext
 
         if (result == null)
         {
-            @Nullable RuntimeException t_ExceptionToThrow = null;
+            @Nullable Throwable t_ExceptionToThrow = null;
 
             try
             {
