2676673f26d9612b85e2525cf220aee9f0a84e95
chous <chous@acm-sl.org>
2014-04-01 13:05:25 +0200

    201404011305

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/DefaultTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/DefaultTemplatePackagingContext.java
@@ -104,7 +104,7 @@ public class DefaultTemplatePackagingContext
     @NotNull
     public TemplateDef<String> getTemplateDef()
     {
-        return getTemplateDef(getCommand());
+        return getValue(getCommand());
     }
 
     /**
