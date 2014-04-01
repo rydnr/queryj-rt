02ae4a5eb3578eea4647129edfa8690ecaf94231
chous <chous@acm-sl.org>
2014-04-01 16:46:28 +0200

    201404011646

--- queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/FillTemplateChainWrapper.java
+++ queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/FillTemplateChainWrapper.java
@@ -122,7 +122,7 @@ public class FillTemplateChainWrapper<C extends QueryJTemplateContext>
     {
         return
               "{ \"class\": \"" + FillTemplateChainWrapper.class.getSimpleName() + '"'
-              + ", \"package\": \"org.acmsl.queryj."
+              + ", \"package\": \"org.acmsl.queryj.placeholders\""
             + ", \"parent\": \"" + super.toString() +  "\" }";
     }
 }
