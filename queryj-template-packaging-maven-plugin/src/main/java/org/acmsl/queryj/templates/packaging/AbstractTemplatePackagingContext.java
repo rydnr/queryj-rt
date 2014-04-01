2bf313d3954ae1d26145f9f3bf45e90f4b36efc3
chous <chous@acm-sl.org>
2014-04-01 12:57:20 +0200

    201404011257

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
@@ -314,7 +314,9 @@ public abstract class AbstractTemplatePackagingContext
     @NotNull
     public String getJdbcDriver()
     {
-        return getJdbcDriver(buildJdbcDriverKey(), getCommand(), );
+        return getJdbcDriver(buildJdbcDriverKey(), getCommand(),                 new JdbcSettingNotAvailableException(
+            JdbcSettingNotAvailableException.JdbcSetting.DRIVER);
+        );
     }
 
     /**
