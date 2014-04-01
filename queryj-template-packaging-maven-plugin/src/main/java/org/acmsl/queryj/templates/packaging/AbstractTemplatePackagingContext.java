9f9301d20759f0a0fdaaf470f26f81f4e33c1ecb
chous <chous@acm-sl.org>
2014-04-01 22:30:25 +0200

    201404012230

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
@@ -82,7 +82,7 @@ import java.io.Serializable;
  */
 @ThreadSafe
 public abstract class AbstractTemplatePackagingContext
-    extends AbstractCommandTemplateContext
+    extends AbstractTemplateContext
     implements Serializable
 {
     /**
