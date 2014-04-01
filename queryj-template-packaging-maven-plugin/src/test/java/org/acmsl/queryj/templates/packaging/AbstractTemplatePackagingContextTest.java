7f045d9fd1eb057b72da886c3453969e7a71927c
chous <chous@acm-sl.org>
2014-04-01 22:51:06 +0200

    201404012251

--- queryj-template-packaging-maven-plugin/src/test/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContextTest.java
+++ queryj-template-packaging-maven-plugin/src/test/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContextTest.java
@@ -194,11 +194,7 @@ public class AbstractTemplatePackagingContextTest
             new AbstractTemplatePackagingContext(t_Command)
             {{
                 result.immutableSetValue(result.buildTemplateNameKey(), templateName, t_Command);
-                public <T> void setValue(@NotNull final String key, @NotNull final T value, @NotNull final QueryJCommand command)
-                {
-                    super.immutableSetValue(key, value, command);
-                }
-            };
+            }};
 
 
         @Nullable final String templateName = "template";
