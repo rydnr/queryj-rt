e9d47a0b80e600dee561f0d628224a1c7340dbcd
chous <chous@acm-sl.org>
2014-04-01 12:56:18 +0200

    201404011256

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
@@ -294,7 +294,7 @@ public abstract class AbstractTemplatePackagingContext
     @NotNull
     public File getOutputDir()
     {
-        return getValue(buildOutputDirKey(), getCommand());
+        return getValue(buildOutputDirKey(), getCommand(), OutputDirNotAvailableException.class);
     }
 
     /**
