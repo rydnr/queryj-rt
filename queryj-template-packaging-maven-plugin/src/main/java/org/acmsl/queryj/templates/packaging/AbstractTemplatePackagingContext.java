45cda124cb46fbb07953287c86d421a1f7b4144e
chous <chous@acm-sl.org>
2014-04-01 13:01:07 +0200

    201404011301

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
@@ -348,7 +348,7 @@ public abstract class AbstractTemplatePackagingContext
     {
         return
             getValue(
-                buildJdbcUrlKey(),
+                buildJdbcUserNameKey(),
                 getCommand(),
                 new JdbcSettingNotAvailableException(JdbcSetting.USERNAME));
     }
