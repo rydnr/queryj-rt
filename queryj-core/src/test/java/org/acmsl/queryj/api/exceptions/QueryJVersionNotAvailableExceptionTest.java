0434b87dde9eecfcb481e894c790decdb2d24e3e
chous <chous@acm-sl.org>
2014-04-01 16:20:02 +0200

    201404011620

--- queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/QueryJVersionNotAvailableExceptionTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/QueryJVersionNotAvailableExceptionTest.java
@@ -56,4 +56,6 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class QueryJVersionNotAvailableExceptionTest
 {
+    queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/QueryJVersionNotAvailableExceptionTest.java
+
 }
