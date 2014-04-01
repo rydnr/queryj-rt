6252e2a8a2b27d29c296ab22c239d3159dc7f8bc
chous <chous@acm-sl.org>
2014-04-01 22:57:35 +0200

    201404012257

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractQueryJTemplateContextTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractQueryJTemplateContextTest.java
@@ -111,7 +111,7 @@ public class AbstractQueryJTemplateContextTest
     @Test
     public void header_is_available()
     {
-        @NotNull final AbstractTemplateContext instance = createContext();
+        @NotNull final AbstractQueryJTemplateContext instance = createContext();
 
         Assert.assertEquals("header", instance.getHeader());
     }
