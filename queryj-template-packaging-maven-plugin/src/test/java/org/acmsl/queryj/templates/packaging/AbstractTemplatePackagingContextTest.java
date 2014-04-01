cb279f38171e2c577ffdbd7110849c7332b6230e
chous <chous@acm-sl.org>
2014-04-01 22:51:33 +0200

    201404012251

--- queryj-template-packaging-maven-plugin/src/test/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContextTest.java
+++ queryj-template-packaging-maven-plugin/src/test/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContextTest.java
@@ -192,6 +192,7 @@ public class AbstractTemplatePackagingContextTest
 
         @NotNull final String templateName = "template";
         @NotNull final String fileName = "file.name";
+        @NotNull final String packageName = "package.name";
         @NotNull final AbstractTemplatePackagingContext result =
             new AbstractTemplatePackagingContext(t_Command)
             {{
