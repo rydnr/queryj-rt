2cd850d827c64cab239b698c840df66254cbb9db
chous <chous@acm-sl.org>
2014-04-02 19:12:41 +0200

    201404021912

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractQueryJTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractQueryJTemplateContext.java
@@ -71,7 +71,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 /**
- *
+ * QueryJ-specific template context: used
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/01 21:26
