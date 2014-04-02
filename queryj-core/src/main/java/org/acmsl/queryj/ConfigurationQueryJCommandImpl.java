a2d1f9354dc3b5ed489b03ae40f523ca2ca7bf33
chous <chous@acm-sl.org>
2014-04-02 07:09:33 +0200

    201404020709

--- queryj-core/src/main/java/org/acmsl/queryj/ConfigurationQueryJCommandImpl.java
+++ queryj-core/src/main/java/org/acmsl/queryj/ConfigurationQueryJCommandImpl.java
@@ -181,6 +181,7 @@ public class ConfigurationQueryJCommandImpl
     /**
      * Retrieves the setting for given key.
      * @param key the key.
+     * @param <T> the type.
      * @return the value for such key.
      */
     @Override
