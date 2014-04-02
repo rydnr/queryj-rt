4805198a616d540f013c7748b0dcf97cabbbbb73
chous <chous@acm-sl.org>
2014-04-02 19:13:51 +0200

    201404021913

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractQueryJTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractQueryJTemplateContext.java
@@ -248,7 +248,7 @@ public abstract class AbstractQueryJTemplateContext
     @NotNull
     public String getJndiLocation()
     {
-        return getJndiLocation(getCommand());
+        return getValue(buildJndiLocation(getCommand());
     }
 
     /**
