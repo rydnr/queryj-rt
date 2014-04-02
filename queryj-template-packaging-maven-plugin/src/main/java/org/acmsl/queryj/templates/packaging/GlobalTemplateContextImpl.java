4e1b4fafc4a1aaac952432c9207360b5bb2e377d
chous <chous@acm-sl.org>
2014-04-02 07:32:19 +0200

    201404020732

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/GlobalTemplateContextImpl.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/GlobalTemplateContextImpl.java
@@ -168,6 +168,10 @@ public class GlobalTemplateContextImpl
                     .append(this.getCommand(), other.getCommand())
                     .isEquals();
         }
+        else
+        {
+            result = false;
+        }
 
         return result;
     }
