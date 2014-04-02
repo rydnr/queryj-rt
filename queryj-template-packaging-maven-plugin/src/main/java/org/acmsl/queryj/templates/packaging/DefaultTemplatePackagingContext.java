b7534735c26ec3bf29555fd3829fe402f5e2aa5d
chous <chous@acm-sl.org>
2014-04-02 07:24:43 +0200

    201404020724

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/DefaultTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/DefaultTemplatePackagingContext.java
@@ -133,7 +133,7 @@ public  class DefaultTemplatePackagingContext
     public int hashCode()
     {
         return
-            new HashCodeBuilder().append(DefaultTemplatePackagingContext.class.getName()).append(this.getCommand()).toHashCode();
+            new HashCodeBuilder().append(super.hashCode()).append(DefaultTemplatePackagingContext.class.getName()).append(this.getCommand()).toHashCode();
     }
 
     /**
