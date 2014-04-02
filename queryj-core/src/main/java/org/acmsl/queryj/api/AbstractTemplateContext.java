dc6616c626a89ab600f823c193b9511bc777835b
chous <chous@acm-sl.org>
2014-04-02 07:40:53 +0200

    201404020740

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
@@ -362,7 +362,7 @@ public abstract class AbstractTemplateContext
         return
             new HashCodeBuilder()
                 .append(AbstractTemplateContext.class.getName())
-                .append(m__Pk)
+                .append(this.m__Pk)
                 .append(this.m__Command)
                 .toHashCode();
     }
