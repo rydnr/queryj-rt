2b59119712293a6f1720bed5d92f98b1d3ac9713
chous <chous@acm-sl.org>
2014-04-02 20:56:43 +0200

    201404022056

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
@@ -280,7 +280,7 @@ public abstract class AbstractTemplateContext
      * @return such information.
      */
     @NotNull
-    protected <T> T getValue(
+    protected <T> T getListValue(
         @NotNull final String key,
         @NotNull final String pk,
         @NotNull final QueryJCommand command,
