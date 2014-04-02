97db5945c9e2dfc9bfe5621699e4b70e3c296439
chous <chous@acm-sl.org>
2014-04-02 20:43:49 +0200

    201404022043

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/DefaultTemplatePackagingContext.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/DefaultTemplatePackagingContext.java
@@ -100,7 +100,7 @@ public  class DefaultTemplatePackagingContext
         @NotNull final TemplateDef<String> templateDef,
         @NotNull final QueryJCommand command)
     {
-        super(templateName, command);
+        super(fileName, command);
         immutableSetValue(buildTemplateNameKey(), templateName, command);
         immutableSetValue(buildPackageNameKey(), outputPackage, command);
         immutableSetValue(buildFileNameKey(), fileName, command);
