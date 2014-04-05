1621ce9eeaedfbe46d40de1df5b86bed43b3364b
chous <chous@acm-sl.org>
2014-04-05 07:57:16 +0200

    201404050757

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/oracle/JdbcMetadataTypeManagerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/oracle/JdbcMetadataTypeManagerTest.java
@@ -47,7 +47,7 @@ import org.acmsl.queryj.metadata.engines.JdbcMetadataTypeManager;
 import org.jetbrains.annotations.NotNull;
 
 /*
- * Importing checkthread.org annotations.
+ * Importing JUnit classes.
  */
 import org.junit.Test;
 import org.junit.runner.RunWith;
