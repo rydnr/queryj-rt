a75a9062d0e8a66d47a20cbc44fc953707926f75
chous <chous@acm-sl.org>
2014-04-05 16:39:02 +0200

    201404051639

--- queryj-templates/src/test/java/cucumber/templates/AbstractTemplatesTest.java
+++ queryj-templates/src/test/java/cucumber/templates/AbstractTemplatesTest.java
@@ -881,7 +881,8 @@ public abstract class AbstractTemplatesTest<G, F>
         EasyMock.expect(result.getTables()).andReturn(tables).anyTimes();
         EasyMock.expect(result.isCaseSensitive()).andReturn(false).anyTimes();
         EasyMock.expect(result.getEngine()).andReturn(new UndefinedJdbcEngine(engineName, "11")).anyTimes();
-        EasyMock.expect(result.getTableDAO()).andReturn(tableDAO);
+        EasyMock.expect(result.getTableDAO()).andReturn(tableDAO).anyTy
+        ;
 
         for (@NotNull final Table<String, Attribute<String>, List<Attribute<String>>> table : tables)
         {
