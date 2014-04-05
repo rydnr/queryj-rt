a060b7fee894a1c8388e7a518d35ab84cca24f73
chous <chous@acm-sl.org>
2014-04-05 09:02:22 +0200

    201404050902

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractSqlDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractSqlDecoratorTest.java
@@ -74,17 +74,7 @@ public class AbstractSqlDecoratorTest
     public void isResultNullable_is_correct_for_implicit_results_and_single_queries()
     {
         @NotNull final Sql<String> sql =
-            new SqlElement<>("id1", "name1", "select", Cardinality.SINGLE, "all", false, false, "desc1");
-
-        @NotNull final T id,
-        @NotNull final T name,
-        @NotNull final T type,
-        @NotNull final Cardinality cardinality,
-        @Nullable final T implementation,
-        final boolean validate,
-        final boolean dynamic,
-        @NotNull final T repositoryScope,
-        @NotNull final T description)
+            new SqlElement<>("id1", "name1", "select", Cardinality.SINGLE, "all", false, false, null, "desc1");
 
         @NotNull final CustomSqlProvider customSqlProvider = EasyMock.createNiceMock(CustomSqlProvider.class);
         @NotNull final MetadataManager metadataManager = EasyMock.createNiceMock(MetadataManager.class);
