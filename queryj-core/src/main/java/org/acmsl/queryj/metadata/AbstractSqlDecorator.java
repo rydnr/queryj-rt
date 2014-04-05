c4d975cbeba70ad72ec674b7d2d3437a3b886b15
chous <chous@acm-sl.org>
2014-04-05 11:38:43 +0200

    201404051138

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractSqlDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractSqlDecorator.java
@@ -610,7 +610,6 @@ public abstract class AbstractSqlDecorator
     {
         final boolean result;
 
-        if (resultClass )
         @NotNull final JdbcTypeManager typeManager = new JdbcTypeManager();
 
         @NotNull final Class<?> clazz = typeManager.getClass(resultClass);
