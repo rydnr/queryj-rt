20f92c07cfab1fb28158e07130719dd82da59426
chous <chous@acm-sl.org>
2014-04-05 08:20:17 +0200

    201404050820

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/JdbcMetadataTypeManager.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/JdbcMetadataTypeManager.java
@@ -1786,7 +1786,7 @@ public class JdbcMetadataTypeManager
         return
               "{ \"class\": \"" + JdbcMetadataTypeManager.class.getSimpleName() + '"'
             + ", \"native2JavaTypeMapping\": " + m__mNative2JavaTypeMapping
-        + ", \"package\": "
+        + ", \"package\": \"" + JdbcMetadataTypeManager.class.getPackage().getName() + \""
             + " }";
     }
 }
