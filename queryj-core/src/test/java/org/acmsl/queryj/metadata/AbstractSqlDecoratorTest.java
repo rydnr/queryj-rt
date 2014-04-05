02c4f1d5d0bebcd5dd485470689488025b2748bd
chous <chous@acm-sl.org>
2014-04-05 11:53:06 +0200

    201404051153

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractSqlDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractSqlDecoratorTest.java
@@ -134,7 +134,7 @@ public class AbstractSqlDecoratorTest
 
         EasyMock.expect(customSqlProvider.getSqlResultDAO()).andReturn(resultDAO);
         EasyMock.replay(customSqlProvider);
-        EasyMock.expect(resultDAO.findBySqlId())
+        EasyMock.expect(resultDAO.findBySqlId(sql.getId()))
 
         @NotNull final AbstractSqlDecorator instance =
             new AbstractSqlDecorator(sql, customSqlProvider, metadataManager) {};
