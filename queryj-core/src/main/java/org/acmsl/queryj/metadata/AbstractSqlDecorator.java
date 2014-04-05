f7843c2d8481196f36f4b44e83985c8cd5f08106
chous <chous@acm-sl.org>
2014-04-05 11:35:40 +0200

    201404051135

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractSqlDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractSqlDecorator.java
@@ -613,7 +613,7 @@ public abstract class AbstractSqlDecorator
         @NotNull final Class<?> clazz = typeManager.getClass(resultClass);
 
         result =
-            (   (typeManager.toPrimitive(clazz) != null)
+            (   (typeManager.toPrimitive(clazz) == null)
 
     }
 
