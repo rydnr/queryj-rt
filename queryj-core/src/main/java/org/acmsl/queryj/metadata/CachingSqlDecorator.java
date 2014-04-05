42cf97bcb1bc6c8b18e181b0ad940b2f148e25a4
chous <chous@acm-sl.org>
2014-04-05 11:56:51 +0200

    201404051156

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingSqlDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingSqlDecorator.java
@@ -256,7 +256,7 @@ public class CachingSqlDecorator
         return
               "{ \"class\": \"" + CachingSqlDecorator.class.getSimpleName() + '"'
             + ", \"wrappedParametersCheck\": " + m__bWrappedParametersCheck
-            + ", m__cCachedParameters=" + m__cCachedParameters
+            + ", \"m__cCachedParameters=" + m__cCachedParameters
             + ", m__strCachedResultClass='" + m__strCachedResultClass + '\''
             + '}';
     }
