99ae3facaa7b2e886f9cd4dabe609029ef08072d
chous <chous@acm-sl.org>
2014-04-05 07:37:33 +0200

    201404050737

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/JdbcTypeManagerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/JdbcTypeManagerTest.java
@@ -1721,7 +1721,7 @@ public class JdbcTypeManagerTest
     }
 
     /**
-     * Tests whether Double can be used for Numeric columns.
+     * Tests whether TinyInt can be used for Numeric columns.
      */
     @Test
     public void tinyint_can_be_used_for_numeric_columns()
