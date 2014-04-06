deac07f542ec2c0ebd8a6837c55960419726806e
chous <chous@acm-sl.org>
2014-04-06 18:13:49 +0200

    201404061813

--- queryj-templates/src/test/java/cucumber/templates/TableTestHelper.java
+++ queryj-templates/src/test/java/cucumber/templates/TableTestHelper.java
@@ -188,7 +188,7 @@ public class TableTestHelper
     /**
      * The starting quote regex.
      */
-    protected static final Pattern
+    protected static final Pattern STARTING_REGEX
     /**
      * Singleton implementation to avoid double-locking check.
      */
