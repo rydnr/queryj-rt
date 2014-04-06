6d0c4f50c007c20bc869aefc578f78341b8dcbd4
chous <chous@acm-sl.org>
2014-04-06 13:27:52 +0200

    201404061327

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableDAO.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableDAO.java
@@ -107,7 +107,10 @@ public interface TableDAO
     Table<String, Attribute<String>, List<Attribute<String>>> findByDAO(@NotNull final String dao);
 
     /**
-     * Retrieves the actual contents of the table.
+     /**
+     * Retrieves the actual contents of given table.
+     * @param tableName the table name.
+     * @return the retrieved rows.
      */
     @NotNull
     List<Row<String>> queryContents();
