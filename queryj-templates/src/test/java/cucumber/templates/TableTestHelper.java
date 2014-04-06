a1347e6474c9f4557990ae21f9c08c2ceaef7710
chous <chous@acm-sl.org>
2014-04-06 17:48:37 +0200

    201404061748

--- queryj-templates/src/test/java/cucumber/templates/TableTestHelper.java
+++ queryj-templates/src/test/java/cucumber/templates/TableTestHelper.java
@@ -837,7 +837,7 @@ public class TableTestHelper
     protected String retrieveRowName(
         @NotNull final List<Attribute<String>> rowValues, @NotNull final Table<String, Attribute<String>, List<Attribute<String>>> table)
     {
-        @NotNull final String result;
+        @NotNull String result = null;
 
         @Nullable final Attribute<String> staticAttribute = table.getStaticAttribute();
 
