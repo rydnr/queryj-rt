6b84e916798f9dc2f900d2183cbf03b97c2cb612
chous <chous@acm-sl.org>
2014-04-06 13:22:36 +0200

    201404061322

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/MetadataManagerTableDAO.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/MetadataManagerTableDAO.java
@@ -80,7 +80,8 @@ import java.util.Map;
 /**
  * {@link MetadataManager}-backed {@link TableDAO} instance.
  * @author <a href="mailto:chous@acm-sl.org">chous</a>
- * @since 2012/06/06
+ * @since 3.0
+ * Created: 2012/06/06
  */
 public abstract class MetadataManagerTableDAO<M extends MetadataManager>
     implements TableDAO
