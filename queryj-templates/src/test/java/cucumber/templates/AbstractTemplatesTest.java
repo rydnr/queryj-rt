dc3c0bcd0019697fd02b8fb9243398be42a489d0
chous <chous@acm-sl.org>
2014-04-06 13:29:31 +0200

    201404061329

--- queryj-templates/src/test/java/cucumber/templates/AbstractTemplatesTest.java
+++ queryj-templates/src/test/java/cucumber/templates/AbstractTemplatesTest.java
@@ -871,7 +871,7 @@ public abstract class AbstractTemplatesTest<G, F>
         {
             try
             {
-                EasyMock.expect(templateUtils.queryContents(table.getName(), result, decoratorFactory)).andReturn(staticContents);
+                EasyMock.expect(tableDAO.queryContents(table.getName(), result, decoratorFactory)).andReturn(staticContents);
             }
             catch (@NotNull final SQLException sqlException)
             {
