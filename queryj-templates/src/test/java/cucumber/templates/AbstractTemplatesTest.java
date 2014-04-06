12c3dcdf353e9d111d888fd041611bd514774fd3
chous <chous@acm-sl.org>
2014-04-06 17:33:40 +0200

    201404061733

--- queryj-templates/src/test/java/cucumber/templates/AbstractTemplatesTest.java
+++ queryj-templates/src/test/java/cucumber/templates/AbstractTemplatesTest.java
@@ -876,7 +876,7 @@ public abstract class AbstractTemplatesTest<G, F>
                 // Forced to define the catch block.
             }
             EasyMock.expect(tableDAO.findByName(table.getName())).andReturn(table).anyTimes();
-            EasyMock.expect(tableDAO.findByName(table.getName())).andReturn(table).anyTimes();
+            EasyMock.expect(tableDAO.findByDAO(table.getName())).andReturn(table).anyTimes();
         }
 
         EasyMock.replay(result);
