e18388282ab21b7c169601bcce2a7ab4a8022fc5
chous <chous@acm-sl.org>
2014-04-07 15:08:53 +0200

    201404071508

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/JdbcMetadataTypeManager.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/JdbcMetadataTypeManager.java
@@ -1465,7 +1465,7 @@ public class JdbcMetadataTypeManager
      * @param dataType the data type.
      * @return <code>true</code> if such data type can be managed as a
      * Clob.
-     * @param <T> the
+     * @param <T> the type.
      */
     @Override
     public <T> boolean isClob(@NotNull final T dataType)
