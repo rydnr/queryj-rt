3842fb4d67f7166ac08f93ad839ea3f01cb1fb13
chous <chous@acm-sl.org>
2014-04-07 15:16:40 +0200

    201404071516

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/JdbcMetadataTypeManagerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/JdbcMetadataTypeManagerTest.java
@@ -97,7 +97,7 @@ public class JdbcMetadataTypeManagerTest
     {
         @NotNull final JdbcMetadataTypeManager instance = new JdbcMetadataTypeManager();
 
-        Assert.assertTrue(instance.isPrimitiveWrapper(Float.class.getSimpleName()));
+        Assert.assertTrue(instance.isPrimitiveWrapper(Double.class.getSimpleName()));
     }
 
     /**
