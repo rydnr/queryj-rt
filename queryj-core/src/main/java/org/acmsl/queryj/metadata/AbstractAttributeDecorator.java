f7bb42182eba081e6bace6bca7fdc7da8980555f
chous <chous@acm-sl.org>
2014-04-07 15:24:03 +0200

    201404071524

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractAttributeDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractAttributeDecorator.java
@@ -333,7 +333,7 @@ public abstract class AbstractAttributeDecorator
     }
 
     /**
-     * Retrieves whether type of thi attribute is a primitive wrapper or not.
+     * Retrieves whether given type is a primitive wrapper or not.
      * @param type the type.
      * @param metadataTypeManager the {@link MetadataTypeManager}.
      * @return {@code true} in such case.
