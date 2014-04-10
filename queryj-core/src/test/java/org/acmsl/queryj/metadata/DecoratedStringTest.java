ef785c8f80a60ed8c972d7a9ee8926ccb0aedf0e
chous <chous@acm-sl.org>
2014-04-10 11:35:46 +0200

    201404101135

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/DecoratedStringTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/DecoratedStringTest.java
@@ -50,7 +50,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- *
+ * Tests for {@link DecoratedString}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/10 09:20
