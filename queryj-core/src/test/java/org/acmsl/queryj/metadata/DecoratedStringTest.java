17fda28ed2df6d9457e5ac11c6895ace66bf434f
chous <chous@acm-sl.org>
2014-04-10 11:37:01 +0200

    201404101137

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/DecoratedStringTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/DecoratedStringTest.java
@@ -41,7 +41,7 @@ package org.acmsl.queryj.metadata;
 import org.jetbrains.annotations.NotNull;
 
 /*
- * Importing checkthread.org annotations.
+ * Importing JUnit classes.
  */
 import org.junit.Assert;
 import org.junit.Test;
