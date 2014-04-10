9327567f4713298a412b8dfdc6dbec7d493d1bc6
chous <chous@acm-sl.org>
2014-04-10 12:47:10 +0200

    201404101247

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractAttributeDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractAttributeDecoratorTest.java
@@ -153,4 +153,49 @@ public class AbstractAttributeDecoratorTest
 
         Assert.assertTrue(instance.isStrictlyPrimitive());
     }
+
+
+    /**
+     * Tests whether isStrictlyPrimitive works for floats.
+     */
+    @Test
+    public void isStrictlyPrimitive_works_for_floats()
+    {
+        @NotNull final MetadataManager metadataManager =
+            EasyMock.createNiceMock(MetadataManager.class);
+
+        @NotNull final MetadataTypeManager metadataTypeManager =
+            new JdbcMetadataTypeManager();
+
+        EasyMock.expect(metadataManager.getMetadataTypeManager()).andReturn(metadataTypeManager);
+
+        EasyMock.replay(metadataManager);
+
+        @NotNull final Attribute<String> attribute =
+            new AttributeValueObject(
+                "name",
+                Types.FLOAT,
+                "float",
+                "tableName",
+                "comment",
+                1,
+                10, // length
+                1, // precision
+                null, // keyword
+                null, // retrieval query
+                null, // sequence
+                false, // nullable
+                null, // value
+                false, // read-only
+                false, // is-bool
+                null, // boolean-true
+                null, // boolean-false
+                null); // boolean-null
+
+        @NotNull final AbstractAttributeDecorator instance =
+            new AbstractAttributeDecorator(
+                attribute, metadataManager) {};
+
+        Assert.assertTrue(instance.isStrictlyPrimitive());
+    }
 }
