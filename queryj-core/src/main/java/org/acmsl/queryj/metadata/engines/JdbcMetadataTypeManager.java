96a74a6bca349c1f12ba26000a4cae8790f2c806
chous <chous@acm-sl.org>
2014-04-11 18:55:55 +0200

    201404111855

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/JdbcMetadataTypeManager.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/JdbcMetadataTypeManager.java
@@ -1099,6 +1099,10 @@ public class JdbcMetadataTypeManager
                 result = "BOOLEAN";
                 break;
 
+            case Types.CLOB:
+                result = "CLOB";
+                break;
+
             case Types.TINYINT:
                     result = Literals.TINYINT_U;
                     break;
