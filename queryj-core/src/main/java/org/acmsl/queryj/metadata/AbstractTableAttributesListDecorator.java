1f1fffc1c194818738b6195736f87576e26b458a
chous <chous@acm-sl.org>
2014-04-12 07:52:56 +0200

    201404120752

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecorator.java
@@ -358,7 +358,7 @@ public abstract class AbstractTableAttributesListDecorator
     }
 
     /**
-     * Checks whether given attributes include any Clob attribute.
+     * Checks whether given attributes include any of a certain type.
      * @param attributes the attributes.
      * @param typeId the type id.
      * @return {@code true} in such case.
