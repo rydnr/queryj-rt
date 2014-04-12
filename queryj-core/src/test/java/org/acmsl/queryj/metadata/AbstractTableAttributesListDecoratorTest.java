772f6c427c22357275d59481909e8137d2efccef
chous <chous@acm-sl.org>
2014-04-12 08:01:44 +0200

    201404120801

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
@@ -53,6 +53,7 @@ import org.jetbrains.annotations.NotNull;
 /*
  * Importing JUnit classes.
  */
+import org.easymock.EasyMock;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Assert;
 import org.junit.Test;
