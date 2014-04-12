d92a2d4b8b9a153a2baa0d17c4b36b4d55e58fd7
chous <chous@acm-sl.org>
2014-04-12 08:00:32 +0200

    201404120800

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
@@ -71,7 +71,7 @@ public class AbstractTableAttributesListDecoratorTest
 {
     /**
      * Checks whether getContainsClobs() detects any Clob
-     * attribute, when there is none.
+     * attribute, when there is one.
      */
     @Test
     public void getContainsClobs_works_when_there_are_clobs()
