dc47dc27c3ef8d6405240b836320aa3c44d32d17
chous <chous@acm-sl.org>
2014-04-12 07:58:52 +0200

    201404120758

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
@@ -82,7 +82,6 @@ public class AbstractTableAttributesListDecoratorTest
         @NotNull final String name = "name";
         @NotNull final String comment = "comment";
         @NotNull final List<Attribute<String>> primaryKey = new ArrayList<>(0);
-        @NotNull final List<Attribute<String>> tableAttributes = new ArrayList<>();
         @NotNull final List<ForeignKey<String>> foreignKeys = new ArrayList<>(0);
         @Nullable final Table<String, Attribute<String>, List<Attribute<String>>> parentTable = null;
         @Nullable final Attribute<String> staticAttribute = null;
