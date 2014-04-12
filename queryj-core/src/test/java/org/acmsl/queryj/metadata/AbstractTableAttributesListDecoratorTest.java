263bf9540c21c56b430e70802ece7e60b370182f
chous <chous@acm-sl.org>
2014-04-12 08:02:13 +0200

    201404120802

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
@@ -59,6 +59,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+/*
+ * Importing JDK classes.
+ */
 import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
