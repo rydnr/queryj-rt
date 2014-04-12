aafe63a291e32a2f2c57cfd0fe28d50d5c08cef5
chous <chous@acm-sl.org>
2014-04-12 08:07:00 +0200

    201404120807

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
@@ -100,7 +100,7 @@ public class AbstractTableAttributesListDecoratorTest
                 name,
                 comment,
                 primaryKey,
-                attributes,
+                new ArrayList<Attribute<String>>()attributes,
                 foreignKeys,
                 parentTable,
                 staticAttribute,
