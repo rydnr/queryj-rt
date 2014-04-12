4c4c615ac099d31df8b1f5981ccf54bdf396a0be
chous <chous@acm-sl.org>
2014-04-12 07:55:23 +0200

    201404120755

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
@@ -100,7 +100,7 @@ public class AbstractTableAttributesListDecoratorTest
                 voDecorated,
                 isRelationship);
 
-        @NotNull final Attribute<DecoratedString> attribute = EasyMock.createNiceMock(Attribute.class)
+        @NotNull final Attribute<DecoratedString> attribute = EasyMock.<DecoratedString>createNiceMock(Attribute.class)
             new AttributeValueObject();
 
         @NotNull final MetadataManager metadataManager = EasyMock.createNiceMock(MetadataManager.class);
