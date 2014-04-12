e1ef48513a8ed1e463d32778e9f601bb936892bf
chous <chous@acm-sl.org>
2014-04-12 08:37:19 +0200

    201404120837

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecoratorTest.java
@@ -212,7 +212,6 @@ public class AbstractTableAttributesListDecoratorTest
                 isRelationship);
 
         @NotNull final Attribute<DecoratedString> attribute = EasyMock.createNiceMock(Attribute.class);
-        EasyMock.expect(attribute.getTypeId()).andReturn(Types.DATE);
         EasyMock.expect(attribute.getType()).andReturn(new DecoratedString("Date"));
         EasyMock.replay(attribute);
         attributes.add(attribute);
