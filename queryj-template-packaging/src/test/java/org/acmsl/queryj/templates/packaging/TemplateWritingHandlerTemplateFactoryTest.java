2b6034b8049a17ba4ec133a0366996727a3ccb0e
jlean <jose.sanleandro@ventura24.es>
2014-04-14 17:29:35 +0200

    201404141729

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateWritingHandlerTemplateFactoryTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateWritingHandlerTemplateFactoryTest.java
@@ -33,10 +33,11 @@
  *
  */
 package org.acmsl.queryj.templates.packaging;
+
 /*
- * Importing checkthread.org annotations.
+ * Importing JUnit classes.
  */
-import org.checkthread.annotations.ThreadSafe;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -49,4 +50,6 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class TemplateWritingHandlerTemplateFactoryTest
 {
+    @Test
+    public void 
 }
