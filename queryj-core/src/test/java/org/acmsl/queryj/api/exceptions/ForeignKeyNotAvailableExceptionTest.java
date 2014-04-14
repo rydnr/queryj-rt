449603a748676e8bea1258c7a5ed86ffb86c0992
jlean <jose.sanleandro@ventura24.es>
2014-04-14 17:03:12 +0200

    201404141703

---
+++ queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/ForeignKeyNotAvailableExceptionTest.java
@@ -0,0 +1,56 @@
+/*
+                        QueryJ Core
+
+    Copyright (C) 2002-today  Jose San Leandro Armendariz
+                              chous@acm-sl.org
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: jose.sanleandro@acm-sl.com
+
+ ******************************************************************************
+ *
+ * Filename: ForeignKeyNotAvailableExceptionTest.java
+ *
+ * Author: Jose San Leandro
+ *
+ * Description: Thrown whenever a ForeignKey is expected but it's not
+ *              available, which is a bug.
+ *
+ * Created: 2014/04/14 17:01
+ *
+ */
+package org.acmsl.queryj.api.exceptions;
+/*
+ * Importing checkthread.org annotations.
+ */
+import org.checkthread.annotations.ThreadSafe;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Thrown whenever a {@link ForeignKey} is expected but it's not available, which is a bug.
+ * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
+ * @since 3.0
+ * Created 2014/04/14
+ */
+@RunWith(JUnit4.class)
+public class ForeignKeyNotAvailableExceptionTest
+{
+    @Test
+    public void
+}
