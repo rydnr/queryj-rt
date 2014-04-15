df016035596ed96cbbea2b8238129ca0162939fb
chous <chous@acm-sl.org>
2014-04-15 21:54:11 +0200

    201404152154

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateFactoryTemplateFactoryTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateFactoryTemplateFactoryTest.java
@@ -82,7 +82,7 @@ public class TemplateFactoryTemplateFactoryTest
     @Test
     public void createTemplate_returns_the_correct_template_for_a_per_repository_template()
     {
-        testCreateTemplate(TemplateDefType.PER_REPOSITORY, "TemplateFactory");
+        testCreateTemplate(TemplateDefType.PER_REPOSITORY, Literals.TEMPLATE_FACTORY);
     }
 
     /**
