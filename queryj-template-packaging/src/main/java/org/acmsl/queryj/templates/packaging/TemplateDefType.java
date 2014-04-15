cefda6acfc472106adc188782986a5e472fb298e
chous <chous@acm-sl.org>
2014-04-15 08:44:28 +0200

    201404150844

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefType.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefType.java
@@ -135,6 +135,10 @@ public enum TemplateDefType
         return PER_REPOSITORY.getType().equals(getType());
     }
 
+    public boolean isPerForeignKey()
+    {
+        return PER_FOREIGN_KEY.getType().equals(getType());
+    }
     /**
      * Checks whether this type identifies per-sql template defs.
      * @return {@code true} in such case.
