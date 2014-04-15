f233e8fe44a53adc87592b82b70e31adc3b0932c
jlean <jose.sanleandro@ventura24.es>
2014-04-15 18:25:19 +0200

    201404151825

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/MissingTemplatesExceptionTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/MissingTemplatesExceptionTest.java
@@ -68,6 +68,7 @@ public class MissingTemplatesExceptionTest
     @Test
     public void template_chain_provider_message_is_defined_in_Spanish_and_English()
     {
+
         for (@NotNull final String message : Arrays.asList("default-template-chain-provider", "per-foreign-key-templates-test"))
         @NotNull final MissingTemplatesException instance = new MissingTemplatesException();
 
