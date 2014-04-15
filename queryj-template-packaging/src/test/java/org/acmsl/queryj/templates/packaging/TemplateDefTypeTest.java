1211ffa5fba3f45c04e8dc4087dc9f433fcb2986
chous <chous@acm-sl.org>
2014-04-15 08:40:47 +0200

    201404150840

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefTypeTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefTypeTest.java
@@ -74,7 +74,7 @@ public class TemplateDefTypeTest
         Assert.assertFalse(TemplateDefType.PER_TABLE.isPerRepository());
         Assert.assertTrue(TemplateDefType.PER_REPOSITORY.isPerRepository());
         Assert.assertFalse(TemplateDefType.PER_SQL.isPerRepository());
-        Assert.assertFalse(TemplateDefType.PER_FOREIGN_KEY.isPerTable());
-        Assert.assertFalse(TemplateDefType.PER_CUSTOM_RESULT.isPerTable());
+        Assert.assertFalse(TemplateDefType.PER_FOREIGN_KEY.isPerRepository());
+        Assert.assertFalse(TemplateDefType.PER_CUSTOM_RESULT.isPerRepository());
     }
 }
