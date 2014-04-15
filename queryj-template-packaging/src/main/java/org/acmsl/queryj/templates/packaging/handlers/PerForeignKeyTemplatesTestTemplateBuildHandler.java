23e69bfa90a0aafaa2e47e30405e1aa8634cb946
jlean <jose.sanleandro@ventura24.es>
2014-04-15 18:15:20 +0200

    201404151815

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/PerForeignKeyTemplatesTestTemplateBuildHandler.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/PerForeignKeyTemplatesTestTemplateBuildHandler.java
@@ -155,7 +155,7 @@ public class PerForeignKeyTemplatesTestTemplateBuildHandler
     @Override
     protected String retrieveTemplateName(@NotNull final QueryJCommand parameters)
     {
-        return Literals.PER_FOREIGN_TEMPLATES_TEST;
+        return Literals.PER_FOREIGN_KEY_TEMPLATES_TEST;
     }
 
     /**
