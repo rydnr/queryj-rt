842098add4757dbab35a05d3e54915bf6cb6e802
jlean <jose.sanleandro@ventura24.es>
2014-04-15 18:22:16 +0200

    201404151822

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/MissingTemplatesExceptionTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/MissingTemplatesExceptionTest.java
@@ -52,7 +52,7 @@ import java.util.Arrays;
 import java.util.Locale;
 
 /**
- * Tests for {}
+ * Tests for {@link MissingTemplatesException}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/15 18:21
