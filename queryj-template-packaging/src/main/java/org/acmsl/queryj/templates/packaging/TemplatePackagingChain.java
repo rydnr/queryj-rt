35ae0c53fda73f55ac25ce3591182611a9af1509
jlean <jose.sanleandro@ventura24.es>
2014-04-15 17:08:41 +0200

    201404151708

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplatePackagingChain.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplatePackagingChain.java
@@ -128,6 +128,8 @@ public class TemplatePackagingChain<CH extends QueryJCommandHandler<QueryJComman
 
         chain.add((CH) new PerForeignKeyTemplatesTestTemplateHandlerBundle());
 
+//        chain.add((CH) new PerForeignKeyTemplatesFeatureTemplateHandlerBundle());
+
         chain.add((CH) new DefaultTemplateChainProviderTemplateHandlerBundle());
 
         return chain;
