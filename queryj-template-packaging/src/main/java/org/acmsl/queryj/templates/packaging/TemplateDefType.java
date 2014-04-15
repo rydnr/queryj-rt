4e8f6c1d3095db6848d1ccb3631fd19461a20eed
chous <chous@acm-sl.org>
2014-04-15 08:46:12 +0200

    201404150846

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefType.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefType.java
@@ -179,6 +179,6 @@ public enum TemplateDefType
 
     public boolean isPerCustomResult()
     {
-        return false;  //To change body of created methods use File | Settings | File Templates.
+        return PER_CUSTOM_RESULT.getType().equals(getType());
     }
 }
