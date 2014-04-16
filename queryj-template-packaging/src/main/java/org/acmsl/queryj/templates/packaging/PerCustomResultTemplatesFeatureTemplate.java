3c2f7c4b65d2cf08edbc1801a3d20e6aaf1d9c90
chous <chous@acm-sl.org>
2014-04-16 08:32:48 +0200

    201404160832

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomResultTemplatesFeatureTemplate.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomResultTemplatesFeatureTemplate.java
@@ -27,7 +27,7 @@
  *
  * Author: Jose San Leandro Armendariz
  *
- * Description: 
+ * Description: Used to generate sources using PerForeignKeyTemplatesFeature.stg.
  *
  * Date: 2014/04/16
  * Time: 07:31
