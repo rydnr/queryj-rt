95ac0533c0fd9b885a47edc6f1f88b24793237bf
chous <chous@acm-sl.org>
2014-04-16 22:52:32 +0200

    201404162252

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateBuildHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateBuildHandlerTest.java
@@ -38,7 +38,6 @@ package org.acmsl.queryj.templates.packaging.handlers;
 /*
  * Importing JetBrains annotations.
  */
-import junit.framework.Assert;
 import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.templates.packaging.Literals;
 import org.easymock.EasyMock;
