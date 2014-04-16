8a20eed33827d8c563e0426f40f7b7cd34f507d5
chous <chous@acm-sl.org>
2014-04-16 08:34:45 +0200

    201404160834

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomResultTemplatesFeatureTemplate.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomResultTemplatesFeatureTemplate.java
@@ -74,6 +74,6 @@ public class PerCustomResultTemplatesFeatureTemplate
     @Override
     public String getTemplateName()
     {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return null;
     }
 }
