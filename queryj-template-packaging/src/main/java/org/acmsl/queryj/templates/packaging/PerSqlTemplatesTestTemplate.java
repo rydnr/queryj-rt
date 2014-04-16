4c59799a7cf665ece0ed42d0476fd423ec30e7a7
chous <chous@acm-sl.org>
2014-04-16 22:04:41 +0200

    201404162204

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerSqlTemplatesTestTemplate.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerSqlTemplatesTestTemplate.java
@@ -66,7 +66,7 @@ public class PerSqlTemplatesTestTemplate
 
     /**
      * Retrieves the template name.
-     * @return "PerCustomResultTemplatesTest";
+     * @return "PerSqlTemplatesTest";
      */
     @NotNull
     @Override
