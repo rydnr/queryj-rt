10ecc15ef09d779f881fdc77cb5c953390ad45f9
chous <chous@acm-sl.org>
2014-04-16 23:30:27 +0200

    201404162330

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateBuildHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateBuildHandlerTest.java
@@ -69,6 +69,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+/*
+ * Importing JDK classes.
+ */
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
