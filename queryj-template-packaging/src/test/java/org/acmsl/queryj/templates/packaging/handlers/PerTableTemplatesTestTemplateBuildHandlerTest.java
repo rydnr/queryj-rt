e430761d64a587929b31bdd029c3dc40e8cdd48a
chous <chous@acm-sl.org>
2014-04-16 23:07:29 +0200

    201404162307

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateBuildHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateBuildHandlerTest.java
@@ -129,6 +129,6 @@ public class PerTableTemplatesTestTemplateBuildHandlerTest
         @NotNull final PerTableTemplatesTestTemplateBuildHandler instance =
             new PerTableTemplatesTestTemplateBuildHandler();
 
-        Assert.assertEquals(Literals.CUCUMBER_TEMPLATES);
+        Assert.assertEquals(Literals.CUCUMBER_);
     }
 }
