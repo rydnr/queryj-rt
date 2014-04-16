35f1d6e0c3dbe5fb49651f3db5f8401a79037cdf
chous <chous@acm-sl.org>
2014-04-16 22:01:00 +0200

    201404162201

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerSqlTemplatesTestTemplateTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerSqlTemplatesTestTemplateTest.java
@@ -38,12 +38,15 @@ package org.acmsl.queryj.templates.packaging;
 /*
  * Importing JetBrains annotations.
  */
+import org.easymock.EasyMock;
 import org.jetbrains.annotations.NotNull;
 
 /*
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -56,4 +59,18 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class PerSqlTemplatesTestTemplateTest
 {
+    /**
+     * Checks the template name is correct.
+     */
+    @Test
+    public void templateName_is_correct()
+    {
+        @NotNull final GlobalTemplateContext context = EasyMock.createNiceMock(GlobalTemplateContext.class);
+
+        @NotNull final PerCustomResultTemplatesTestTemplate instance =
+            new PerCustomResultTemplatesTestTemplate(context);
+
+        Assert.assertEquals(Literals.PER_CUSTOM_RESULT_TEMPLATES_TEST, instance.getTemplateName());
+    }
+
 }
