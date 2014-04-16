5aa8cdfd18624a0042fcd14d3fece0b1ac6b2bf5
chous <chous@acm-sl.org>
2014-04-16 07:13:48 +0200

    201404160713

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerRepositoryTemplatesFeatureTemplateTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerRepositoryTemplatesFeatureTemplateTest.java
@@ -69,6 +69,6 @@ public class PerRepositoryTemplatesFeatureTemplateTest
         @NotNull final PerRepositoryTemplatesFeatureTemplate instance =
             new PerRepositoryTemplatesFeatureTemplate(context);
 
-        Assert.assertEquals(Literals.PER_REPOSITORY_TEMPLATES_FEATURE"PerRepositoryTemplatesFeature", instance.getTemplateName());
+        Assert.assertEquals(Literals.PER_REPOSITORY_TEMPLATES_FEATURE, instance.getTemplateName());
     }
 }
