41f44a34ab22a119d03791cc8bde87cf4679cdab
chous <chous@acm-sl.org>
2014-04-16 22:20:26 +0200

    201404162220

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerRepositoryTemplatesTestTemplateFactory.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerRepositoryTemplatesTestTemplateFactory.java
@@ -48,7 +48,7 @@ import org.checkthread.annotations.ThreadSafe;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Factory for {@link PerTableTemplatesTestTemplate}s.
+ * Factory for {@link PerRepositoryTemplatesTestTemplate}s.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/16 21:18
