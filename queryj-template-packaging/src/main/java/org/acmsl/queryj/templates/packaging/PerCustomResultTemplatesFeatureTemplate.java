804496f0f3f588007af0c29bf052cbac46e93198
chous <chous@acm-sl.org>
2014-04-16 08:32:35 +0200

    201404160832

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomResultTemplatesFeatureTemplate.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomResultTemplatesFeatureTemplate.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- *
+ * Used to generate sources using PerRepositoryTemplatesFeature.stg.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/16 07:31
