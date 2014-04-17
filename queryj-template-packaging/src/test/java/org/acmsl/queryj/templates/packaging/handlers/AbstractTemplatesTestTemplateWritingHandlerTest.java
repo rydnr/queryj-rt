6c15a3f5b186549bdbfc96d74f5b1a6b592d9ae1
chous <chous@acm-sl.org>
2014-04-17 09:58:21 +0200

    201404170958

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/AbstractTemplatesTestTemplateWritingHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/AbstractTemplatesTestTemplateWritingHandlerTest.java
@@ -70,8 +70,7 @@ import org.junit.Test;
 public abstract class AbstractTemplatesTestTemplateWritingHandlerTest<
     WH extends TemplatePackagingTestWritingHandler
         <T, GlobalTemplateContext, TemplatePackagingTemplateGenerator<T, GlobalTemplateContext>>,
-    BH extends TemplatePackagingTestBuildHandler
-        <T, TF, GlobalTemplateContext>,
+    BH extends TemplatePackagingTestBuildHandler<T, TF, GlobalTemplateContext>,
     T extends TemplatePackagingTemplate,
     TF extends TemplatePackagingTemplateFactory<T, GlobalTemplateContext>>
 {
