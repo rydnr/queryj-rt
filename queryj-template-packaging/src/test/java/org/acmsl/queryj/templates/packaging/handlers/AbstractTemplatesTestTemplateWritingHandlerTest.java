2039ca53654d98603d6b183d8c998015b65e4b2e
chous <chous@acm-sl.org>
2014-04-17 09:51:43 +0200

    201404170951

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/AbstractTemplatesTestTemplateWritingHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/AbstractTemplatesTestTemplateWritingHandlerTest.java
@@ -64,7 +64,7 @@ import org.junit.Test;
 public abstract class AbstractTemplatesTestTemplateWritingHandlerTest<
     WH extends TemplatePackagingTestWritingHandler
         <T, GlobalTemplateContext, TemplatePackagingTemplateGenerator<T, GlobalTemplateContext>>,
-        T extends PerTableTemplatesTestTemplate>
+    T extends PerTableTemplatesTestTemplate>
 {
     /**
      * Creates a new writing handler instance.
