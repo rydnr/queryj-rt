c451cdd938786701727c17a8d26693d1933250fd
chous <chous@acm-sl.org>
2014-04-17 10:02:03 +0200

    201404171002

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateWritingHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateWritingHandlerTest.java
@@ -61,8 +61,6 @@ import org.jetbrains.annotations.NotNull;
  * Importing JUnit classes.
  */
 import org.easymock.EasyMock;
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
