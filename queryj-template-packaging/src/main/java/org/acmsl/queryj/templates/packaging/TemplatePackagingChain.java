d1df027effa2a97df02974289eb271cfec8b9d8e
chous <chous@acm-sl.org>
2014-04-17 13:01:24 +0200

    201404171301

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplatePackagingChain.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplatePackagingChain.java
@@ -129,6 +129,10 @@ public class TemplatePackagingChain<CH extends QueryJCommandHandler<QueryJComman
 
 //        chain.add((CH) new PerForeignKeyTemplatesFeatureTemplateHandlerBundle());
 
+        chain.add((CH) new PerForeignKeyTemplatesTestTemplateHandlerBundle());
+
+//        chain.add((CH) new PerForeignKeyTemplatesFeatureTemplateHandlerBundle());
+
         chain.add((CH) new DefaultTemplateChainProviderTemplateHandlerBundle());
 
         return chain;
