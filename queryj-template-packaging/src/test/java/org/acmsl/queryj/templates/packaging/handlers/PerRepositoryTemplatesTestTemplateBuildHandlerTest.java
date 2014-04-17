3be9ea24d1165c85d7efbae075de3db45e18faca
chous <chous@acm-sl.org>
2014-04-17 08:28:21 +0200

    201404170828

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateBuildHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateBuildHandlerTest.java
@@ -27,7 +27,7 @@
  *
  * Author: Jose San Leandro Armendariz
  *
- * Description: 
+ * Description: Tests for PerRepositoryTemplatesTestTemplateBuildHandler.
  *
  * Date: 2014/04/17
  * Time: 07:27
