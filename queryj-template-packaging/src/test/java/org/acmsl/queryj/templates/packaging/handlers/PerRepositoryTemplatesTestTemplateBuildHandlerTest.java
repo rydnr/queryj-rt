fa06d23f879711fedb38fe31e52c2a03569bb0a7
chous <chous@acm-sl.org>
2014-04-17 08:45:47 +0200

    201404170845

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateBuildHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateBuildHandlerTest.java
@@ -137,7 +137,7 @@ public class PerRepositoryTemplatesTestTemplateBuildHandlerTest
     @Test
     public void retrieveTemplateFactory_retrieves_the_correct_factory()
     {
-        retrieveTemplateName_works(PerRepositoryTemplatesTestTemplateFactory.getInstance());
+        retrieveTemplateFactory_retrieves_the_correct_factory(PerRepositoryTemplatesTestTemplateFactory.getInstance());
     }
 
     /**
