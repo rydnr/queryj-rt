2db20debdbb4f432fd9b5f8661113600cd9f3efb
chous <chous@acm-sl.org>
2014-04-17 08:31:03 +0200

    201404170831

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateBuildHandler.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateBuildHandler.java
@@ -38,12 +38,21 @@ package org.acmsl.queryj.templates.packaging.handlers;
 /*
  * Importing JetBrains annotations.
  */
+import org.acmsl.queryj.QueryJCommand;
+import org.acmsl.queryj.QueryJCommandWrapper;
+import org.acmsl.queryj.templates.packaging.GlobalTemplateContext;
+import org.acmsl.queryj.templates.packaging.Literals;
+import org.acmsl.queryj.templates.packaging.PerForeignKeyTemplatesTestTemplate;
+import org.acmsl.queryj.templates.packaging.PerForeignKeyTemplatesTestTemplateFactory;
+import org.acmsl.queryj.templates.packaging.TemplateDef;
 import org.jetbrains.annotations.NotNull;
 
 /*
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
+
+import java.util.List;
 
 /**
  *
@@ -53,5 +62,80 @@ import org.checkthread.annotations.ThreadSafe;
  */
 @ThreadSafe
 public class PerRepositoryTemplatesTestTemplateBuildHandler
+    extends TemplatePackagingTestBuildHandler
+                <PerForeignKeyTemplatesTestTemplate,
+                    PerForeignKeyTemplatesTestTemplateFactory,
+                    GlobalTemplateContext>
+{
+    /**
+     * The key to access the templates in the command.
+     */
+    @NotNull static final String TEMPLATES_KEY = "PerForeignKeyTemplatesTest_templates";
+
+    /**
+     * Creates a {@code PerForeignKeyTemplatesTestTemplateBuildHandler}.
+     */
+    public PerForeignKeyTemplatesTestTemplateBuildHandler() {}
+
+    /**
+     * Retrieves the template factory.
+     * @return the {@link PerForeignKeyTemplatesTestTemplateFactory} instance.
+     */
+    @Override
+    @NotNull
+    protected PerForeignKeyTemplatesTestTemplateFactory retrieveTemplateFactory()
+    {
+        return PerForeignKeyTemplatesTestTemplateFactory.getInstance();
+    }
+
+    /**
+     * Builds the context from given parameters.
+     * @param templateDefs the template defs.
+     * @param parameters   the command with the parameters.
+     * @return the template context.
+     */
+    @NotNull
+    @Override
+    protected GlobalTemplateContext buildContext(
+        @NotNull final List<TemplateDef<String>> templateDefs, @NotNull final QueryJCommand parameters)
+    {
+        return buildGlobalContext(templateDefs, parameters);
+    }
+
+    /**
+     * Retrieves the output package for the generated file.
+     * @param parameters the parameters.
+     * @return such package.
+     */
+    @NotNull
+    @Override
+    protected String retrieveOutputPackage(@NotNull final QueryJCommand parameters)
+    {
+        return org.acmsl.queryj.templates.packaging.Literals.CUCUMBER_TEMPLATES;
+    }
+
+    /**
+     * Retrieves the template name, using the parameters if necessary.
+     * @param parameters the parameters.
+     * @return the template name.
+     */
+    @NotNull
+    @Override
+    protected String retrieveTemplateName(@NotNull final QueryJCommand parameters)
+    {
+        return Literals.PER_FOREIGN_KEY_TEMPLATES_TEST;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void storeTemplate(
+        @NotNull final PerForeignKeyTemplatesTestTemplate template,
+        @NotNull final QueryJCommand parameters)
+    {
+        new QueryJCommandWrapper<PerForeignKeyTemplatesTestTemplate>(parameters)
+            .setSetting(TEMPLATES_KEY, template);
+    }
 {
 }
