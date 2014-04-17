77fb73d2412673ecb32ef6f96c57b5749524b2c9
chous <chous@acm-sl.org>
2014-04-17 08:43:50 +0200

    201404170843

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateBuildHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateBuildHandlerTest.java
@@ -90,6 +90,7 @@ public class PerRepositoryTemplatesTestTemplateBuildHandlerTest
                  PerRepositoryTemplatesTestTemplate,
                     PerRepositoryTemplatesTestTemplateFactory>
 {
+
     /**
      * Tests whether retrieveTemplateName() works.
      */
