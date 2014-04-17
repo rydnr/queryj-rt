f5a6eb0664bc1c975462ba75da46e808e3262d4b
chous <chous@acm-sl.org>
2014-04-17 09:11:46 +0200

    201404170911

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerCustomResultTemplatesTestTemplateBuildHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerCustomResultTemplatesTestTemplateBuildHandlerTest.java
@@ -51,6 +51,7 @@ import org.jetbrains.annotations.NotNull;
 /*
  * Importing checkthread.org annotations.
  */
+import org.easymock.EasyMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
