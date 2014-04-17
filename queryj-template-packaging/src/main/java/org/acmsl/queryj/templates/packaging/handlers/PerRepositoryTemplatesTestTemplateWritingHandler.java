c79164ed2afa4632a903c3be490389f6e5b03edc
chous <chous@acm-sl.org>
2014-04-17 10:18:29 +0200

    201404171018

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateWritingHandler.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateWritingHandler.java
@@ -42,7 +42,7 @@ import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.QueryJCommandWrapper;
 
 /*
- * Importing JetBrains annotations.
+ * Importing QueryJ Template Packaging.
  */
 import org.acmsl.queryj.templates.packaging.GlobalTemplateContext;
 import org.acmsl.queryj.templates.packaging.PerRepositoryTemplatesTestTemplate;
