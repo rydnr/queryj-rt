0f64caa98126496f2906340ccf02ae9b5f3a543a
chous <chous@acm-sl.org>
2014-04-17 18:57:00 +0200

    201404171857

--- queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
@@ -1130,8 +1130,8 @@ public abstract class AbstractTemplatesTest<G, F>
         result.append(m__mRows);
         result.append(", results=");
         result.append(m__mResults);
-        result.append(", results=");
-        result.append(m__mResults);
+        result.append(", properties=");
+        result.append(m__mProperties);
         result.append(", engineName=");
         result.append(m__strEngineName);
         result.append(", jdbcDriver=");
