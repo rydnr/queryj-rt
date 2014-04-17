22a94c043ea9ec70d1a5d32e6d935a917a84d0eb
chous <chous@acm-sl.org>
2014-04-17 08:46:10 +0200

    201404170846

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateBuildHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerRepositoryTemplatesTestTemplateBuildHandlerTest.java
@@ -64,7 +64,6 @@ import org.jetbrains.annotations.NotNull;
  * Importing JUnit/EasyMock classes.
  */
 import org.easymock.EasyMock;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
