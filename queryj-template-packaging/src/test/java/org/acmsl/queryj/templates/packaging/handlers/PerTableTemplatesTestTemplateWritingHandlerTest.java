91d5ed0f705158e413866d4ab0ff8f386990cb9f
chous <chous@acm-sl.org>
2014-04-17 10:00:27 +0200

    201404171000

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateWritingHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateWritingHandlerTest.java
@@ -73,6 +73,7 @@ import org.junit.runners.JUnit4;
  */
 @RunWith(JUnit4.class)
 public class PerTableTemplatesTestTemplateWritingHandlerTest
+    extends AbstractTemplatesTestTemplateWritingHandlerTest
 {
     /**
      * Checks retrieveTemplateGenerator retrieves a not-null instance.
