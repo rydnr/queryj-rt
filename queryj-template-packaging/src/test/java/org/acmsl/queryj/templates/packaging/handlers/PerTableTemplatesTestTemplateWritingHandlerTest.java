16351e0f168b87355e08e0f9f3c2386ab152b70e
chous <chous@acm-sl.org>
2014-04-17 09:21:54 +0200

    201404170921

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateWritingHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerTableTemplatesTestTemplateWritingHandlerTest.java
@@ -48,7 +48,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- * Tests for {@link PerForeignKeyTemplatesTestTemplateWritingHandler}.
+ * Tests for {@link PerTableTemplatesTestTemplateWritingHandler}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/17 08:21
