04bd0e0363be6ae611dddaadb8f8f8c7430bf96c
chous <chous@acm-sl.org>
2014-04-17 13:16:53 +0200

    201404171316

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerCustomResultTemplateFactoryTemplateTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerCustomResultTemplateFactoryTemplateTest.java
@@ -38,7 +38,6 @@ package org.acmsl.queryj.templates.packaging;
 /*
  * Importing JetBrains annotations.
  */
-import org.checkthread.annotations.ThreadSafe;
 import org.jetbrains.annotations.NotNull;
 
 /*
