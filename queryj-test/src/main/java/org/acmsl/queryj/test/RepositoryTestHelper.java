05e5161ac7b8f16de7491c0fa9f7d28cae1771be
chous <chous@acm-sl.org>
2014-04-17 12:36:43 +0200

    201404171236

--- queryj-test/src/main/java/org/acmsl/queryj/test/RepositoryTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/RepositoryTestHelper.java
@@ -39,17 +39,23 @@ package org.acmsl.queryj.test;
  * Importing JetBrains annotations.
  */
 import cucumber.api.DataTable;
+import org.acmsl.queryj.Literals;
+import org.acmsl.queryj.metadata.vo.Attribute;
+import org.acmsl.queryj.metadata.vo.Table;
+import org.acmsl.queryj.tools.ant.AntTablesElement;
 import org.jetbrains.annotations.NotNull;
 
 /*
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
+import org.jetbrains.annotations.Nullable;
 
 /*
  * Importing JDK classes.
  */
 import java.util.List;
+import java.util.Map;
 
 /**
  * Helper class for per-foreign key Cucumber tests.
@@ -79,6 +85,59 @@ public class RepositoryTestHelper
     public static RepositoryTestHelper getInstance()
     {
         return RepositoryTestHelperSingletonContainer.SINGLETON;
+    }
+
+    /**
+     * Defines the input tables based on the information provided by the
+     * feature.
+     * @param tableInfo the information about the tables.
+     * @param tables the table collection.
+     */
+    public void defineInputTables(
+        @NotNull final DataTable tableInfo,
+        @NotNull final Map<String, Table<String, Attribute<String>, List<Attribute<String>>>> tables)
+    {
+        @NotNull final List<Map<String, String>> tableEntries = tableInfo.asMaps();
+
+        @Nullable Table<String, Attribute<String>, List<Attribute<String>>> table;
+
+        for (@NotNull final Map<String, String> tableEntry: tableEntries)
+        {
+            table = convertToTable(tableEntry);
+
+            if (table != null)
+            {
+                tables.put(table.getName(), table);
+            }
+        }
+    }
+
+    /**
+     * Converts given table information to a {@link Table}.
+     * @param tableEntry the table information.
+     * @return the {@link Table} instance.
+     */
+    @Nullable
+    protected Table<String, Attribute<String>, List<Attribute<String>>> convertToTable(
+        @NotNull final Map<String, String> tableEntry)
+    {
+        @Nullable Table<String, Attribute<String>, List<Attribute<String>>> result = null;
+
+        @Nullable final String table =  tableEntry.get(AntTablesElement.TABLE);
+
+        if (table != null)
+        {
+            result =
+                convertToTable(
+                    table,
+                    tableEntry.get(Literals.COMMENT),
+                    tableEntry.get(PARENT_TABLE),
+                    tableEntry.get(STATIC),
+                    !isNullOrBlank(tableEntry.get(DECORATED)) && Boolean.TRUE.equals(tableEntry.get(DECORATED)),
+                    !isNullOrBlank(tableEntry.get(RELATIONSHIP)) && Boolean.TRUE.equals(tableEntry.get(RELATIONSHIP)));
+        }
+
+        return result;
     }
 
     /**
