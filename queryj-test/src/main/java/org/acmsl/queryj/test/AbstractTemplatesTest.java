b828c266877af5ffcff30cc3a6d2df718d8d0212
chous <chous@acm-sl.org>
2014-04-17 18:59:07 +0200

    201404171859

--- queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
@@ -464,7 +464,7 @@ public abstract class AbstractTemplatesTest<G, F>
      * @param properties the properties.
      */
     @SuppressWarnings("unused")
-    protected void setProperties(@NotNull final Map<String, Property<String>> properties)
+    protected void setProperties(@NotNull final Map<String, List<Property<String>>> properties)
     {
         immutableSetProperties(properties);
     }
