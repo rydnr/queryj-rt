62b73d9b77bcb957c7386e4c711605e450c95cca
chous <chous@acm-sl.org>
2014-04-17 12:50:41 +0200

    201404171250

--- queryj-test/src/main/java/org/acmsl/queryj/test/CustomResultTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/CustomResultTestHelper.java
@@ -202,7 +202,7 @@ public class CustomResultTestHelper
 
     /**
      * Creates a new {@link Property}.
-     * @param id the id.
+     * @param name the name.
      * @param type the type.
      * @return the {@link Property} instance.
      */
