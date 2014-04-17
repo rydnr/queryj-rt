982bc2133afe5ede17cc850375e395cab1ada868
chous <chous@acm-sl.org>
2014-04-17 13:22:38 +0200

    201404171322

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerSqlTemplateFactoryTemplateTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerSqlTemplateFactoryTemplateTest.java
@@ -23,11 +23,11 @@
 
  ******************************************************************************
  *
- * Filename: PerRepositoryTemplateFactoryTemplateTest.java
+ * Filename: PerSqlTemplateFactoryTemplateTest.java
  *
  * Author: Jose San Leandro Armendariz
  *
- * Description: Tests for PerRepositoryTemplateFactoryTemplate.
+ * Description: Tests for PerSqlTemplateFactoryTemplate.
  *
  * Date: 2014/04/21
  * Time: 12:07
@@ -51,13 +51,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- * Tests for {@link PerRepositoryTemplateFactoryTemplate}.
+ * Tests for {@link PerSqlTemplateFactoryTemplate}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/17 12:21
  */
 @RunWith(JUnit4.class)
-public class PerRepositoryTemplateFactoryTemplateTest
+public class PerSqlTemplateFactoryTemplateTest
 {
     /**
      * Checks whether getTemplateName() returns the correct template name.
@@ -68,8 +68,8 @@ public class PerRepositoryTemplateFactoryTemplateTest
         @NotNull final DefaultTemplatePackagingContext context =
             EasyMock.createNiceMock(DefaultTemplatePackagingContext.class);
 
-        @NotNull final PerRepositoryTemplateFactoryTemplate instance =
-            new PerRepositoryTemplateFactoryTemplate<>(context);
+        @NotNull final PerSqlTemplateFactoryTemplate instance =
+            new PerSqlTemplateFactoryTemplate<>(context);
 
         Assert.assertEquals(Literals.PER_REPOSITORY_TEMPLATE_FACTORY, instance.getTemplateName());
     }
