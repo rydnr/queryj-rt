a00922d96d84833b5976c889277939d7547c6296
chous <chous@acm-sl.org>
2014-04-17 09:03:46 +0200

    201404170903

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerForeignKeyTemplatesTestTemplateBuildHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerForeignKeyTemplatesTestTemplateBuildHandlerTest.java
@@ -139,39 +139,4 @@ public class PerForeignKeyTemplatesTestTemplateBuildHandlerTest
     {
         retrieveTemplateFactory_retrieves_the_correct_factory(PerForeignKeyTemplatesTestTemplateFactory.getInstance());
     }
-
-    /**
-     * Checks whether the output package is the Cucumber's.
-     */
-    @Test
-    public void retrieveOutputPackage_returns_the_cucumber_package()
-    {
-        @NotNull final PerForeignKeyTemplatesTestTemplateBuildHandler instance =
-            new PerForeignKeyTemplatesTestTemplateBuildHandler();
-
-        @NotNull final QueryJCommand command = EasyMock.createNiceMock(QueryJCommand.class);
-
-        Assert.assertEquals(Literals.CUCUMBER_TEMPLATES, instance.retrieveOutputPackage(command));
-    }
-
-    /**
-     * Checks whether buildContext() builds a global context.
-     */
-    @Test
-    public void buildContext_builds_a_global_context()
-        throws IOException
-    {
-        @NotNull final PerForeignKeyTemplatesTestTemplateBuildHandler instance =
-            new PerForeignKeyTemplatesTestTemplateBuildHandler();
-
-        @NotNull final QueryJCommand command =
-            new ConfigurationQueryJCommandImpl(new PropertiesConfiguration());
-
-        new QueryJCommandWrapper<File>(command).setSetting(
-            PerForeignKeyTemplatesTestTemplate.OUTPUT_DIR_FOR_TESTS, new File("/"));
-
-        @NotNull final List<TemplateDef<String>> templateDefs = new ArrayList<>(0);
-
-        Assert.assertNotNull(instance.buildContext(templateDefs, command));
-    }
 }
