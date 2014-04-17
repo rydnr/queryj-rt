0f66b7e2298d6acfec759615fcc814f8f1ca8a89
chous <chous@acm-sl.org>
2014-04-17 13:23:58 +0200

    201404171323

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerSqlTemplateFactoryTemplate.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerSqlTemplateFactoryTemplate.java
@@ -23,7 +23,7 @@
 
  ******************************************************************************
  *
- * Filename: PerRepositoryTemplateFactoryTemplate.java
+ * Filename: PerSqlTemplateFactoryTemplate.java
  *
  * Author: Jose San Leandro
  *
@@ -53,7 +53,7 @@ import org.checkthread.annotations.ThreadSafe;
  * Created: 2014/04/17 12:23
  */
 @ThreadSafe
-public class PerRepositoryTemplateFactoryTemplate<C extends TemplatePackagingContext>
+public class PerSqlTemplateFactoryTemplate<C extends TemplatePackagingContext>
     extends TemplateFactoryTemplate<C>
 {
     /**
@@ -65,14 +65,14 @@ public class PerRepositoryTemplateFactoryTemplate<C extends TemplatePackagingCon
      * Creates a new instance.
      * @param context the template context.
      */
-    public PerRepositoryTemplateFactoryTemplate(@NotNull final C context)
+    public PerSqlTemplateFactoryTemplate(@NotNull final C context)
     {
         super(context);
     }
 
     /**
      * Retrieves the template name.
-     * @return "PerRepositoryTemplateFactory".
+     * @return "PerSqlTemplateFactory".
      */
     @NotNull
     @Override
