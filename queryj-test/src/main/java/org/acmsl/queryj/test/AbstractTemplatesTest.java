a97f89512e95875c871d4a290974fb35086928b8
chous <chous@acm-sl.org>
2014-04-17 18:51:21 +0200

    201404171851

--- queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
@@ -415,8 +415,8 @@ public abstract class AbstractTemplatesTest<G, F>
     }
 
     /**
-     * Specifies the tables.
-     * @param tables the tables.
+     * Specifies the results.
+     * @param results the results.
      */
     protected final void immutableSetResults(@NotNull final Map<String, Result<String>> results)
     {
