b41538214437699ceadaf8f93b4cf57ae25cc034
chous <chous@acm-sl.org>
2014-04-17 10:10:22 +0200

    201404171010

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerForeignKeyTemplatesTestTemplateWritingHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/handlers/PerForeignKeyTemplatesTestTemplateWritingHandlerTest.java
@@ -50,7 +50,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Tests for {@link PerForeignKeyTemplatesTestTemplateBuildHandler}
+ * Tests for {@link PerForeignKeyTemplatesTestTemplateWritingHandlerTest}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/17 09:07
