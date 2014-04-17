12bd256b7555f3ef0baa08f6b8aa3213d0136079
chous <chous@acm-sl.org>
2014-04-17 12:54:01 +0200

    201404171254

--- queryj-test/src/main/java/org/acmsl/queryj/test/CustomResultTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/CustomResultTestHelper.java
@@ -225,7 +225,7 @@ public class CustomResultTestHelper
     {
         @Nullable final Property<String> result;
 
-
+        @Nullable final String
         return new PropertyElement<>(id, columnName, index, type, nullable);
     }
 }
