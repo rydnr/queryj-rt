5381067a13fc93b4a6bcf71ad5435bc6fbc77a74
chous <chous@acm-sl.org>
2014-04-18 18:35:18 +0200

    201404181835

--- queryj-core/src/test/java/org/acmsl/queryj/api/PerCustomResultTemplateContextTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/PerCustomResultTemplateContextTest.java
@@ -44,6 +44,9 @@ import org.jetbrains.annotations.NotNull;
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
  *
@@ -51,7 +54,9 @@ import org.checkthread.annotations.ThreadSafe;
  * @since 3.0
  * Created: 2014/04/18 17:34
  */
-@ThreadSafe
+@RunWith(JUnit4.class)
 public class PerCustomResultTemplateContextTest
 {
+    @Test
+    public void
 }
