774365bf115969d54dc15556f3a9f4ffddfdd88c
chous <chous@acm-sl.org>
2014-04-18 20:48:59 +0200

    201404182048

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerRepositoryTemplateFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerRepositoryTemplateFactory.java
@@ -82,8 +82,5 @@ public interface PerRepositoryTemplateFactory
     public T createTemplate(
         @NotNull final String packageName,
         @NotNull final List<String> tableNames,
-        @NotNull final String jndiLocation,
-        final boolean disableGenerationTimestamps,
-        final boolean disableNotNullAnnotations,
         final boolean disableCheckthreadAnnotations);
 }
