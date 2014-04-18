957a7291dad8bb740f1e1c9ac5a5026981968448
chous <chous@acm-sl.org>
2014-04-18 20:54:11 +0200

    201404182054

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerRepositoryTemplateFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerRepositoryTemplateFactory.java
@@ -38,9 +38,7 @@ package org.acmsl.queryj.api;
  */
 import org.acmsl.commons.patterns.Factory;
 import org.acmsl.queryj.QueryJCommand;
-import org.acmsl.queryj.customsql.CustomSqlProvider;
 import org.acmsl.queryj.metadata.DecoratorFactory;
-import org.acmsl.queryj.metadata.MetadataManager;
 
 /*
  * Importing some JetBrains annotations.
