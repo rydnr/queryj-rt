207554949c491332a2094002c354f103b9524e87
chous <chous@acm-sl.org>
2014-04-18 20:48:51 +0200

    201404182048

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerRepositoryTemplateFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerRepositoryTemplateFactory.java
@@ -80,9 +80,6 @@ public interface PerRepositoryTemplateFactory
      */
     @Nullable
     public T createTemplate(
-        @NotNull final MetadataManager metadataManager,
-        @NotNull final CustomSqlProvider customSqlProvider,
-        @NotNull final DecoratorFactory decoratorFactory,
         @NotNull final String packageName,
         @NotNull final String projectPackage,
         @NotNull final String repository,
