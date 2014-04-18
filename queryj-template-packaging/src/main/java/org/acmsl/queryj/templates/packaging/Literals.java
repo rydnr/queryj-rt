c06970c1309b7829485fd738880ca95281992823
chous <chous@acm-sl.org>
2014-04-18 20:14:08 +0200

    201404182014

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/Literals.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/Literals.java
@@ -136,7 +136,7 @@ public interface Literals
     /**
      * String literal: "PerSqlTemplatesTest".
      */
-    String PER_SQL_TEMPLATES_TEST = "PerSqlTemplatesTest";
+    String PER_SQL_TEMPLATES_TEST = "PerCustomSqlTemplatesTest";
     /**
      * String literal: "PerRepositoryTemplateFactory".
      */
