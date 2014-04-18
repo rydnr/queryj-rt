599cc32e2c60410ab59eb59c4f3a98fdce838399
chous <chous@acm-sl.org>
2014-04-18 14:57:01 +0200

    201404181457

--- queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/AbstractExceptionTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/exceptions/AbstractExceptionTest.java
@@ -64,7 +64,7 @@ public class AbstractExceptionTest<E extends QueryJNonCheckedException>
     @Test
     public void exception_message_is_defined_in_Spanish_and_English()
     {
-        @NotNull final E instance = new E();
+        @NotNull final E instance = createInstance();
 
         for (@NotNull final Locale t_Locale : Arrays.asList(new Locale("en"), new Locale("es")))
         {
