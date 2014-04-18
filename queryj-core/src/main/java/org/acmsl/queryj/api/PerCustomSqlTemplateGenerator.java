47d74d1ab0a7db8ed973a345ee76a3424bbbf17c
chous <chous@acm-sl.org>
2014-04-18 20:42:51 +0200

    201404182042

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomSqlTemplateGenerator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomSqlTemplateGenerator.java
@@ -34,7 +34,8 @@ package org.acmsl.queryj.api;
 
 /**
  * Represents entities able to write per-sql templates.
- * @param <N> the template
+ * @param <N> the template type.
+ * @param <C> the template context type.
  * @author <a href="mailto:chous@acm-sl.org">Jose San Leandro Armendariz</a>
  * @since 2.0
  */
