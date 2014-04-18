06965f394418eecd005e1a3b19b6bc12dd3f52d8
chous <chous@acm-sl.org>
2014-04-18 14:51:24 +0200

    201404181451

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomResultTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomResultTemplateContext.java
@@ -117,7 +117,7 @@ public class PerCustomResultTemplateContext
     @NotNull
     public String buildResultKey()
     {
-        return org.acmsl.queryj.plaLiterals.RESULT_NAME;
+        return org.acmsl.queryj.plLiterals.RESULT_NAME;
     }
 
     /**
