63f4c2eee40528d56ba64386b5e1efac36300251
chous <chous@acm-sl.org>
2014-04-18 19:37:44 +0200

    201404181937

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/Literals.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/Literals.java
@@ -120,7 +120,7 @@ public interface Literals
     /**
      * String literal: "PerSqlTemplatesFeature".
      */
-    String PER_SQL_TEMPLATES_FEATURE = "PerSqlTemplatesFeature";
+    String PER_SQL_TEMPLATES_FEATURE = "PerCustomSqlTemplatesFeature";
     /**
      * String literal: "PerForeignKeyTemplatesFeature".
      */
