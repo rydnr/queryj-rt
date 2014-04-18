a532023323d40b70f42ea1062d17e7d302f074f3
chous <chous@acm-sl.org>
2014-04-18 15:01:22 +0200

    201404181501

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomResultTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomResultTemplateContext.java
@@ -195,6 +195,9 @@ public class PerCustomResultTemplateContext
         return new EqualsBuilder().appendSuper(super.equals(obj)).append(this.m__Result, other.m__Result).isEquals();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @NotNull
     @Override
     public String toString()
