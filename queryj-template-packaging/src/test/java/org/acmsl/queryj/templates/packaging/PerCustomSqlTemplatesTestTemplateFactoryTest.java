b69302f41773a4d114871a7ce6d060064c586e3b
chous <chous@acm-sl.org>
2014-04-18 18:56:07 +0200

    201404181856

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerCustomSqlTemplatesTestTemplateFactoryTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerCustomSqlTemplatesTestTemplateFactoryTest.java
@@ -27,7 +27,7 @@
  *
  * Author: Jose San Leandro Armendariz
  *
- * Description: Tests for PerSqlTemplatesTestTemplateFactory.
+ * Description: Tests for PerCustomSqlTemplatesTestTemplateFactory.
  *
  * Date: 2014/04/16
  * Time: 21:44
