4711d3f8e988f149374d750db9753c4b46b96bcc
chous <chous@acm-sl.org>
2014-04-18 15:04:16 +0200

    201404181504

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomResultTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomResultTemplateContext.java
@@ -42,11 +42,8 @@ package org.acmsl.queryj.api;
 import org.acmsl.queryj.Literals;
 import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.api.exceptions.MissingResultException;
-import org.acmsl.queryj.customsql.CustomSqlProvider;
 import org.acmsl.queryj.customsql.Property;
 import org.acmsl.queryj.customsql.Result;
-import org.acmsl.queryj.metadata.DecoratorFactory;
-import org.acmsl.queryj.metadata.MetadataManager;
 
 /*
  * Importing some Apache Commons Lang builder classes.
