f0df41b297ec7c54cd9ddf719b6bb0721b9acbde
chous <chous@acm-sl.org>
2014-04-19 08:56:40 +0200

    201404190856

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
@@ -522,7 +522,7 @@ public abstract class AbstractTemplateContext
         return
               "{ \"class\": \"" + AbstractTemplateContext.class.getSimpleName() + '"'
             + ", \"pk\": \"" + m__Pk + '"'
-            + ", \"decoratorFactory\": " + m__Pk + '"'
+            + ", \"decoratorFactory\": " + m__DecoratorFactory
             + ", \"command\": " + m__Command
             + ", \"package\": \"org.acmsl.queryj.api\""
             + " }";
