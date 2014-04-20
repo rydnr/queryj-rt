ee65f6b2aed296660ebd8eeed925a632520e648d
chous <chous@acm-sl.org>
2014-04-20 21:05:16 +0200

    201404202105

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractBasePerCustomResultTemplate.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractBasePerCustomResultTemplate.java
@@ -41,11 +41,11 @@ import org.acmsl.queryj.Literals;
 import org.acmsl.queryj.api.exceptions.InvalidPerCustomResultTemplateException;
 import org.acmsl.queryj.api.exceptions.InvalidTemplateException;
 import org.acmsl.queryj.customsql.Result;
+import org.acmsl.queryj.metadata.DecoratorFactory;
 
 /*
  * Importing StringTemplate classes.
  */
-import org.acmsl.queryj.metadata.DecoratorFactory;
 import org.stringtemplate.v4.ST;
 
 /*
