46c155895eebb2d06e4897e464babb295ae46b2e
chous <chous@acm-sl.org>
2014-04-20 20:55:35 +0200

    201404202055

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomSqlTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomSqlTemplateContext.java
@@ -39,6 +39,7 @@ package org.acmsl.queryj.api;
 /*
  * Importing some project classes.
  */
+import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.customsql.CustomSqlProvider;
 import org.acmsl.queryj.customsql.Sql;
 import org.acmsl.queryj.metadata.DecoratorFactory;
