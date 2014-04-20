f1095db08d37b51e8708e2d449cee2632ce20dbe
chous <chous@acm-sl.org>
2014-04-20 20:52:41 +0200

    201404202052

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerRepositoryTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerRepositoryTemplateContext.java
@@ -39,9 +39,6 @@ package org.acmsl.queryj.api;
  * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.QueryJCommand;
-import org.acmsl.queryj.customsql.CustomSqlProvider;
-import org.acmsl.queryj.metadata.DecoratorFactory;
-import org.acmsl.queryj.metadata.MetadataManager;
 
 /*
  * Importing some Apache Commons-Lang classes.
