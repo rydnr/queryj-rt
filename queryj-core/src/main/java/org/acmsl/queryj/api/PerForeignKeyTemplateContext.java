cd9d0b01a2a98bd6cfab67c9b0beafbd51e1150d
chous <chous@acm-sl.org>
2014-04-21 12:28:40 +0200

    201404211228

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerForeignKeyTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerForeignKeyTemplateContext.java
@@ -80,6 +80,7 @@ public class PerForeignKeyTemplateContext
      * @param command the {@link QueryJCommand} instance.
      */
     public PerForeignKeyTemplateContext(
+        @NotNull final String fileName,
         @NotNull final String packageName,
         @NotNull final ForeignKey<String> foreignKey,
         @NotNull final QueryJCommand command)
