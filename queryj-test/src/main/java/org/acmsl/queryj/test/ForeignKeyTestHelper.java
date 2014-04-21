85c25d761a4f81a32f161dfdf672ddd879ea1478
chous <chous@acm-sl.org>
2014-04-21 17:45:13 +0200

    201404211745

--- queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
@@ -163,6 +163,7 @@ public class ForeignKeyTestHelper
                         sourceTable,
                         "",
                         0,
+                        0,
                         )
                 )
             }
