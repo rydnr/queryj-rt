cbc6707f02f89921afcd29fcb90ea20f6b1b5086
chous <chous@acm-sl.org>
2014-04-21 17:41:50 +0200

    201404211741

--- queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
@@ -143,7 +143,7 @@ public class ForeignKeyTestHelper
     @NotNull
     protected List<Attribute<String>> fromCsv(@Nullable final String sourceColumns)
     {
-        @NotNull final List<Attribute<String>> result = new ArrayList<>
+        @NotNull final List<Attribute<String>> result = new ArrayList<>();
     }
 
     /**
