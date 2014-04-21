20fc0e225abe3c1ff97bd03aaf7372b071f1a5f2
chous <chous@acm-sl.org>
2014-04-21 20:28:23 +0200

    201404212028

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingForeignKeyDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingForeignKeyDecorator.java
@@ -247,6 +247,6 @@ public class CachingForeignKeyDecorator
             "{ \"class\": \"CachingForeignKeyDecorator\"
             + ", \"cachedSource\": \"" + m__CachedSource + '"'
             + ", \"cachedTarget\": \"" + m__CachedTarget + '"'
-               '}';
+            + " }";
     }
 }
