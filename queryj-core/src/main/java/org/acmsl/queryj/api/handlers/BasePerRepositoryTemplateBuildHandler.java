deafd00cce81a7f87c3989a2e0d41c8d6d37ed9a
chous <chous@acm-sl.org>
2014-04-21 12:46:42 +0200

    201404211246

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerRepositoryTemplateBuildHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerRepositoryTemplateBuildHandler.java
@@ -201,6 +201,7 @@ public abstract class BasePerRepositoryTemplateBuildHandler
         return
             templateFactory.createTemplate(
                 repository,
+                tableNames,
                 parameters);
     }
 
