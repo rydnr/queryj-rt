122d056f469cc11cf2c2be5702dfcacb889a4868
chous <chous@acm-sl.org>
2014-04-21 12:38:17 +0200

    201404211238

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerForeignKeyTemplateBuildHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerForeignKeyTemplateBuildHandler.java
@@ -42,7 +42,6 @@ import org.acmsl.queryj.api.PerForeignKeyTemplate;
 import org.acmsl.queryj.api.PerForeignKeyTemplateContext;
 import org.acmsl.queryj.api.PerForeignKeyTemplateFactory;
 import org.acmsl.queryj.api.exceptions.QueryJBuildException;
-import org.acmsl.queryj.customsql.CustomSqlProvider;
 import org.acmsl.queryj.metadata.CachingDecoratorFactory;
 import org.acmsl.queryj.metadata.DecoratorFactory;
 import org.acmsl.queryj.metadata.MetadataManager;
