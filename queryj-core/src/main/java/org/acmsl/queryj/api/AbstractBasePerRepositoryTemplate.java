38f5a1232ff02d504fb756f9b6b7d3cbfc11219e
chous <chous@acm-sl.org>
2014-04-21 09:27:20 +0200

    201404210927

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractBasePerRepositoryTemplate.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractBasePerRepositoryTemplate.java
@@ -38,7 +38,6 @@ package org.acmsl.queryj.api;
  */
 import org.acmsl.queryj.api.exceptions.InvalidPerRepositoryTemplateException;
 import org.acmsl.queryj.api.exceptions.InvalidTemplateException;
-import org.acmsl.queryj.metadata.DecoratorFactory;
 
 /*
  * Importing StringTemplate classes.
