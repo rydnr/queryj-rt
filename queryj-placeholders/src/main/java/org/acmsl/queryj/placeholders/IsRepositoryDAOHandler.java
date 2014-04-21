2d261c5e1a2ada62fe507141ad2f6a5eb2f6b6ee
chous <chous@acm-sl.org>
2014-04-21 20:48:08 +0200

    201404212048

--- queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/IsRepositoryDAOHandler.java
+++ queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/IsRepositoryDAOHandler.java
@@ -57,7 +57,8 @@ import org.checkthread.annotations.ThreadSafe;
 /**
  * Is able to resolve "is_repository_DAO" placeholders in templates.
  * @author <a href="mailto:chous@acm-sl.org">chous</a>
- * @since 2012/05/24
+ * @since 3.0
+ * Created: 2012/05/24
  */
 @SuppressWarnings("unused")
 @ThreadSafe
