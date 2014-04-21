f7c4f42213c4662e96164403c910eb73767b7187
chous <chous@acm-sl.org>
2014-04-21 17:40:46 +0200

    201404211740

--- queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
@@ -137,6 +137,7 @@ public class ForeignKeyTestHelper
 
     private List<Attribute<String>> fromCsv(final String sourceColumns)
     {
+        return null;  //To change body of created methods use File | Settings | File Templates.
     }
 
     /**
