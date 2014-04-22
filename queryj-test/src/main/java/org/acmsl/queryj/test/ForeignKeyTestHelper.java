d581bce8d5a3f25df3dffdd994e79f3f2b6e45b8
chous <chous@acm-sl.org>
2014-04-22 07:39:18 +0200

    201404220739

--- queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
@@ -91,6 +91,7 @@ public class ForeignKeyTestHelper
      * Defines a foreign key based on the Cucumber information given.
      * @param fkInfo the Cucumber table.
      * @param foreignKeys the list to fill with the foreign keys.
+     * @param tables the table information (needed to provide
      */
     public void defineInputForeignKey(
         @NotNull final DataTable fkInfo,
