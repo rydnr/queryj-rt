f71bf99d0fda12aebe11fb2c17149eeb8ecc9cb4
chous <chous@acm-sl.org>
2014-04-22 07:33:01 +0200

    201404220733

--- queryj-test/src/test/java/org/acmsl/queryj/test/ForeignKeyTestHelperTest.java
+++ queryj-test/src/test/java/org/acmsl/queryj/test/ForeignKeyTestHelperTest.java
@@ -103,7 +103,8 @@ public class ForeignKeyTestHelperTest
     }
 
     /**
-     * Tests whether defineInputForeignKey()
+     * Tests whether defineInputForeignKey() builds a foreign key which
+     * uses the given
      */
     @Test
     public void defineInputForeignKeys_takes_into_account_the_table_data()
