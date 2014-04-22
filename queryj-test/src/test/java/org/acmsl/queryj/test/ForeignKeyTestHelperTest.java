b299aa459722e86c3e3060a90b75a4ab54fbf33b
chous <chous@acm-sl.org>
2014-04-22 07:35:18 +0200

    201404220735

--- queryj-test/src/test/java/org/acmsl/queryj/test/ForeignKeyTestHelperTest.java
+++ queryj-test/src/test/java/org/acmsl/queryj/test/ForeignKeyTestHelperTest.java
@@ -127,7 +127,8 @@ public class ForeignKeyTestHelperTest
         @NotNull final List<ForeignKey<String>> foreignKeys = new ArrayList<>();
 
         @NotNull final Table<String, Attribute<String>, List<Attribute<String>>> table =
-            new TableIncompleteValueObject()
+            new TableIncompleteValueObject("G_CYCLE_TYPES", "");
+
         @NotNull final Map<String, Table<String, Attribute<String>, List<Attribute<String>>>> tableMap =
             new HashMap<>(1);
         tableMap.put("G_CYCLE_TYPES", table);
