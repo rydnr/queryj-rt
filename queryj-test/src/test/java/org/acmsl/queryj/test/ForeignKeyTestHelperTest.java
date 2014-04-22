edb84796e85a103da8f55d4cb5081f9b4413fa5f
chous <chous@acm-sl.org>
2014-04-22 07:15:38 +0200

    201404220715

--- queryj-test/src/test/java/org/acmsl/queryj/test/ForeignKeyTestHelperTest.java
+++ queryj-test/src/test/java/org/acmsl/queryj/test/ForeignKeyTestHelperTest.java
@@ -58,7 +58,7 @@ import org.junit.runners.JUnit4;
 public class ForeignKeyTestHelperTest
 {
     /**
-     * Tests whether defineInputForeignKeys() builds
+     * Tests whether defineInputForeignKeys() builds the
      */
     @Test
     public void defineInputForeignKeys_builds_the_foreign_key_list()
