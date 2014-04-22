890f155fe42aa9e77ba14cb657c220ba3329b5d5
jlean <jose.sanleandro@ventura24.es>
2014-04-22 13:18:38 +0200

    201404221318

--- queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
@@ -240,6 +240,8 @@ public class ForeignKeyTestHelper
                     {
                         throw new AttributeNotFoundException(attributeName, sourceTable);
                     }
+
+                    return
                 }
 
                 @NotNull
