adb0e0ac65680f5b0c6783d621fa00d3ac55bc16
chous <chous@acm-sl.org>
2014-04-22 07:31:03 +0200

    201404220731

--- queryj-test/src/test/java/org/acmsl/queryj/test/ForeignKeyTestHelperTest.java
+++ queryj-test/src/test/java/org/acmsl/queryj/test/ForeignKeyTestHelperTest.java
@@ -91,7 +91,7 @@ public class ForeignKeyTestHelperTest
         row.put("source", "G_CYCLE_TYPES");
         row.put("column(s)", "G_FIRST_DRAW_TYPE_ID");
         row.put("target", "G_DRAWS");
-        row.put()
+        row.put("allows null", "false");
         @NotNull final List<?> data =
             Arrays.asList(
                 "G_CYCLE_TYPES", "G_FIRST_DRAW_TYPE_ID", "G_DRAWS", "false");
