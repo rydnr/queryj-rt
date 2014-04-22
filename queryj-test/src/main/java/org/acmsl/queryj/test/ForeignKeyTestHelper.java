6d63b92f1ad196333fa607aa8fb9f23ebbbf8f97
jlean <jose.sanleandro@ventura24.es>
2014-04-22 13:20:03 +0200

    201404221320

--- queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
@@ -215,7 +215,8 @@ public class ForeignKeyTestHelper
         return
             new Attribute<String>()
             {
-                @
+                @Nullable
+                protected
                 @NotNull
                 @Override
                 public String getName()
