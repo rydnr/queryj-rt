fbde956727686c6ce563d78fd03a8b2d6646e261
jlean <jose.sanleandro@ventura24.es>
2014-04-22 13:16:20 +0200

    201404221316

--- queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/ForeignKeyTestHelper.java
@@ -218,7 +218,7 @@ public class ForeignKeyTestHelper
                 @Override
                 public String getName()
                 {
-                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                    @NotNull final String result;
                 }
 
                 @Override
