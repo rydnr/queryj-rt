328097d839f9d9d18c6cf4c8d3704931863e3837
chous <chous@acm-sl.org>
2014-04-22 07:30:03 +0200

    201404220730

--- queryj-test/src/test/java/org/acmsl/queryj/test/ForeignKeyTestHelperTest.java
+++ queryj-test/src/test/java/org/acmsl/queryj/test/ForeignKeyTestHelperTest.java
@@ -88,7 +88,7 @@ public class ForeignKeyTestHelperTest
 
         @NotNull final String[] columnNames = { "source", "column(s)", "target", "allows null" };
         @NotNull final Map<String, String> source = new HashMap<>(1);
-        source.put("source", "G_C")
+        source.put("source", "G_CYCLE_TYPES");
         @NotNull final List<?> data =
             Arrays.asList(
                 "G_CYCLE_TYPES", "G_FIRST_DRAW_TYPE_ID", "G_DRAWS", "false");
