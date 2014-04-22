ffb0af840ce987216937eb62789d0d3ca0524904
jlean <jose.sanleandro@ventura24.es>
2014-04-22 14:06:03 +0200

    201404221406

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingForeignKeyDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingForeignKeyDecorator.java
@@ -220,7 +220,7 @@ public class CachingForeignKeyDecorator
      * @return such information.
      */
     @Override
-    @NotNull
+    @Nullable
     public Table<DecoratedString, Attribute<DecoratedString>, ListDecorator<Attribute<DecoratedString>>> getTarget()
     {
         @Nullable Table<DecoratedString, Attribute<DecoratedString>, ListDecorator<Attribute<DecoratedString>>> result =
