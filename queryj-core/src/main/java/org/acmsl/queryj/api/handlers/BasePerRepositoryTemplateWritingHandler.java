305051ee81bca8f2726093b17023ac70df535af0
chous <chous@acm-sl.org>
2014-04-23 20:27:08 +0200

    201404232027

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerRepositoryTemplateWritingHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerRepositoryTemplateWritingHandler.java
@@ -83,6 +83,7 @@ public abstract class BasePerRepositoryTemplateWritingHandler
             retrieveOutputDir(
                 context,
                 rootDir,
+                retrieveTableRepositoryName()
                 retrieveEngine(parameters, retrieveDatabaseMetaData(parameters)),
                 parameters);
     }
