cc00de4ede1ba827b4914ffbdad2c46a3910d93d
chous <chous@acm-sl.org>
2014-04-23 19:06:54 +0200

    201404231906

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerRepositoryTemplateBuildHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerRepositoryTemplateBuildHandler.java
@@ -195,8 +195,7 @@ public class BasePerRepositoryTemplateBuildHandler
     @NotNull
     protected String retrievePackage(
         @NotNull final Engine<String> engine,
-        @NotNull final String projectPackage,
-        @NotNull final PackageUtils packageUtils);
+        @NotNull final String projectPackage)
 
     /**
      * Stores the template in given attribute map.
