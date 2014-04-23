495ee3551ae08ac9d78dd95959638438da518e55
chous <chous@acm-sl.org>
2014-04-23 19:47:25 +0200

    201404231947

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerRepositoryTemplateBuildHandlerTemplate.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerRepositoryTemplateBuildHandlerTemplate.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Per repository template build handler
+ * Per repository template build handler template.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/23 18:44
