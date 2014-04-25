ead25a52d9172c68b5f1e5b3d367ff00009661b2
chous <chous@acm-sl.org>
2014-04-25 19:41:10 +0200

    201404251941

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomResultTemplateBuildHandlerTemplate.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomResultTemplateBuildHandlerTemplate.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- *
+ * Template for per-custom result template build handlers.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/25 18:39
