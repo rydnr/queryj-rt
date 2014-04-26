d7a4c63b20f299beaee5e4b7d6da1f5641bce325
chous <chous@acm-sl.org>
2014-04-26 09:16:24 +0200

    201404260916

--- queryj-test/src/main/java/org/acmsl/queryj/test/sql/CucumberSqlPropertyDAO.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/sql/CucumberSqlPropertyDAO.java
@@ -139,7 +139,7 @@ public class CucumberSqlPropertyDAO
     }
 
     /**
-     * Retrieves the custom result.
+     * Retrieves the custom results.
      * @return such result.
      */
     @NotNull
