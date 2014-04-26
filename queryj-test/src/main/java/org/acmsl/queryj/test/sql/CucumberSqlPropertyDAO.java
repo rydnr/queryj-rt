fb9135599951322b963b27ec1fcff17437eceda1
chous <chous@acm-sl.org>
2014-04-26 09:30:12 +0200

    201404260930

--- queryj-test/src/main/java/org/acmsl/queryj/test/sql/CucumberSqlPropertyDAO.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/sql/CucumberSqlPropertyDAO.java
@@ -223,7 +223,7 @@ public class CucumberSqlPropertyDAO
     {
         @NotNull final List<Property<String>> result = new ArrayList<>();
 
-        new CucumberSqlResultDAO()
+        new CucumberSqlResultDAO(customResults).findByPrimaryKey(resultId);
 
         for (@Nullable final Result<String> customResult : customResults)
         {
