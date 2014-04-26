7ad08b8180ce3d9838fe2a7731ebf4e0edb08dbc
chous <chous@acm-sl.org>
2014-04-26 07:46:24 +0200

    201404260746

--- queryj-test/src/main/java/org/acmsl/queryj/test/CustomResultTestHelper.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/CustomResultTestHelper.java
@@ -215,7 +215,7 @@ public class CustomResultTestHelper
         if (name != null)
         {
             @Nullable final String columnName = tableEntry.get("columnName");
-            @Nullable final String propertyIndex =
+            @Nullable final String propertyIndex = tableEntry.get(Literals.INDEX);
             result =
                 convertToProperty(
                     name,
