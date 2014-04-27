bc82461375440e52a6521b763ec950972e571963
chous <chous@acm-sl.org>
2014-04-27 21:41:56 +0200

    201404272141

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractQueryJTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractQueryJTemplateContext.java
@@ -163,7 +163,7 @@ public abstract class AbstractQueryJTemplateContext
 
         if (result == null)
         {
-            @NotNull final File file = new QueryJCommandWrapper<>()
+            @NotNull final File file = new QueryJCommandWrapper<File>(command).getSetting(QueryJSettings.HEADER_FILE)
         }
     }
 
