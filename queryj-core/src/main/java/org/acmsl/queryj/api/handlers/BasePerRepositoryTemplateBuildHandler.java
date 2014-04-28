43a4bdf1339566643a71e5d49bb110eb1f9a0666
jlean <jose.sanleandro@ventura24.es>
2014-04-28 11:52:29 +0200

    201404281152

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerRepositoryTemplateBuildHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerRepositoryTemplateBuildHandler.java
@@ -121,7 +121,7 @@ public abstract class BasePerRepositoryTemplateBuildHandler
             new QueryJCommandWrapper<>(command);
 
         @NotNull final String packageName =
-            retrievePackage(retrieveTableRepositoryName(command), retrieve retrieveProjectPackage(command));
+            retrievePackage(retrieveTableRepositoryName(command), retrieveEn retrieveProjectPackage(command));
         buildTemplate(
             command,
             retrieveCustomSqlProvider(command),
