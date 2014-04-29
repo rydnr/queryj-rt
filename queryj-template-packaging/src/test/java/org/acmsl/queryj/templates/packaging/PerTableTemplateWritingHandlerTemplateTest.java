c5d2497f77b6cbefc4628f64dc5a55c22eab98b7
chous <chous@acm-sl.org>
2014-04-29 07:01:11 +0200

    201404290701

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerTableTemplateWritingHandlerTemplateTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/PerTableTemplateWritingHandlerTemplateTest.java
@@ -38,11 +38,10 @@ package org.acmsl.queryj.templates.packaging;
 /*
  * Importing JetBrains annotations.
  */
-import org.easymock.EasyMock;
 import org.jetbrains.annotations.NotNull;
 
 /*
- * Importing checkthread.org annotations.
+ * Importing JUnit/EasyMock classes.
  */
 import org.easymock.EasyMock;
 import org.junit.Assert;
