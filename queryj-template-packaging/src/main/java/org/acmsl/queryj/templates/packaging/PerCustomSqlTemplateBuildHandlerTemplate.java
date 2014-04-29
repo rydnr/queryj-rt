dfa5a1dae8f2c93edb066feb6d37a4bba1110ce3
chous <chous@acm-sl.org>
2014-04-29 06:51:06 +0200

    201404290651

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomSqlTemplateBuildHandlerTemplate.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomSqlTemplateBuildHandlerTemplate.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Template for per-custom result template build handlers.
+ * Template for per-custom SQL template build handlers.
  * @param <C> the template packaging context type.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
