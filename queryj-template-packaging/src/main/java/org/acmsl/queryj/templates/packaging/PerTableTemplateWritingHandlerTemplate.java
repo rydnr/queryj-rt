9e1f651b5b42f034d950905544c7cede2a217555
chous <chous@acm-sl.org>
2014-04-29 07:01:50 +0200

    201404290701

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerTableTemplateWritingHandlerTemplate.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerTableTemplateWritingHandlerTemplate.java
@@ -52,6 +52,31 @@ import org.checkthread.annotations.ThreadSafe;
  * Created: 2014/04/29 06:01
  */
 @ThreadSafe
-public class PerTableTemplateWritingHandlerTemplate
+public class PerTableTemplateWritingHandlerTemplate<C extends TemplatePackagingContext>
+    extends TemplateBuildHandlerTemplate<C>
 {
+    /**
+     * The serial version id.
+     */
+    private static final long serialVersionUID = 9122581776783678705L;
+
+    /**
+     * Creates a new instance using given context.
+     * @param context the context.
+     */
+    public PerTableTemplateBuildHandlerTemplate(@NotNull final C context)
+    {
+        super(context);
+    }
+
+    /**
+     * Retrieves the template name.
+     * @return "PerTableTemplateBuildHandler".
+     */
+    @NotNull
+    @Override
+    public String getTemplateName()
+    {
+        return Literals.PER_TABLE_TEMPLATE_BUILD_HANDLER;
+    }
 }
