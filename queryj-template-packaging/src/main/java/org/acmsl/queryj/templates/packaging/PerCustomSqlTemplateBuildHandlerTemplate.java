eda02798c02fdea9dbaa84827668ba9f58e97c6f
chous <chous@acm-sl.org>
2014-04-29 06:50:47 +0200

    201404290650

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomSqlTemplateBuildHandlerTemplate.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/PerCustomSqlTemplateBuildHandlerTemplate.java
@@ -55,10 +55,10 @@ import org.checkthread.annotations.ThreadSafe;
 public class PerCustomSqlTemplateBuildHandlerTemplate<C extends TemplatePackagingContext>
     extends TemplateBuildHandlerTemplate<C>
 {
-    private static final long serialVersionUID = 7067911093065280009L;
     /**
      * The serial version id.
      */
+    private static final long serialVersionUID = 7067911093065280009L;
 
     /**
      * Creates a new instance using given context.
