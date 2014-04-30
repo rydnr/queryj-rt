ab659e82f5775d6d448fe189e02decf3b364f594
jlean <jose.sanleandro@ventura24.es>
2014-04-30 12:52:44 +0200

    201404301252

--- queryj-core/src/test/java/org/acmsl/queryj/SerializableConfigurationTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/SerializableConfigurationTest.java
@@ -1,5 +1,5 @@
 /*
-                        queryj
+                        QueryJ Core
 
     Copyright (C) 2002-today  Jose San Leandro Armendariz
                               chous@acm-sl.org
@@ -27,7 +27,7 @@
  *
  * Author: Jose San Leandro Armendariz
  *
- * Description: 
+ * Description: Tests for SerializableConfiguration.
  *
  * Date: 2014/04/30
  * Time: 12:51
@@ -48,7 +48,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- *
+ * Tests for {@link SerializableConfiguration}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/04/30 12:51
