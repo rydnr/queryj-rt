a04050dc8f7fe65a70acbf1cc3ce40d20b9c294e
chous <chous@acm-sl.org>
2014-04-30 19:40:56 +0200

    201404301940

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractQueryJTemplateContextTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractQueryJTemplateContextTest.java
@@ -243,7 +243,7 @@ public class AbstractQueryJTemplateContextTest
         @NotNull final QueryJCommand t_Command =
             new ConfigurationQueryJCommandImpl(new SerializablePropertiesConfiguration(), null);
 
-        AbstractQueryJTemplateContext result =
+        @NotNull final AbstractQueryJTemplateContext result =
             new AbstractQueryJTemplateContext("" + pk, t_Command)
             {{
                 immutableSetValue(buildTemplateNameKey(), "", getCommand());
