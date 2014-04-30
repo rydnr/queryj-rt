9cd2dc3f3e240bafe9260d678a0eef9a7c7299b9
chous <chous@acm-sl.org>
2014-04-30 19:18:14 +0200

    201404301918

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateContextTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateContextTest.java
@@ -44,6 +44,7 @@ import org.jetbrains.annotations.NotNull;
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
