294eaf650c7edfd0e600892705dd68a2bb28d1c7
chous <chous@acm-sl.org>
2014-04-30 19:40:36 +0200

    201404301940

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractQueryJTemplateContextTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractQueryJTemplateContextTest.java
@@ -243,6 +243,11 @@ public class AbstractQueryJTemplateContextTest
         @NotNull final QueryJCommand t_Command =
             new ConfigurationQueryJCommandImpl(new SerializablePropertiesConfiguration(), null);
 
+        new AbstractQueryJTemplateContext("" + pk, t_Command)
+        {{
+                immutableSetValue(buildTemplateNameKey(), "", getCommand());
+                immutableSetValue(buildFileNameKey(), "" + pk, getCommand());
+            }};
         @NotNull final MetadataManager metadataManager = EasyMock.createNiceMock(MetadataManager.class);
         new QueryJCommandWrapper<MetadataManager>(t_Command).setSetting(
             DatabaseMetaDataRetrievalHandler.METADATA_MANAGER, metadataManager);
