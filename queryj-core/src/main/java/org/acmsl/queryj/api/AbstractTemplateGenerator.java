450591000d38264defc398371dcdddf5f03c7324
chous <chous@acm-sl.org>
2014-04-30 20:19:27 +0200

    201404302019

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
@@ -191,7 +191,7 @@ public abstract class AbstractTemplateGenerator<N extends Template<C>, C extends
                 outputDir,
                 rootFolder,
                 charset,
-                FileUtils.getInstance();
+                FileUtils.getInstance(),
                 UniqueLogFactory.getLog(AbstractQueryJTemplateGenerator.class));
     }
 
