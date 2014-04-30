26b4610694749c8a20ba4bbed1fbb89284433849
chous <chous@acm-sl.org>
2014-04-30 19:29:15 +0200

    201404301929

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveQueryHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveQueryHandlerTest.java
@@ -83,7 +83,7 @@ public class RetrieveQueryHandlerTest
 {
     /**
      * Checks the first query is retrieved correctly.
-     * @throws QueryJBuildException
+     * Throws QueryJBuildException
      */
     @Test
     public void retrieves_the_first_query()
