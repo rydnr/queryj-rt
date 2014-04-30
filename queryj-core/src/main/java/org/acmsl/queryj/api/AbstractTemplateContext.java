4e898af5edc4826dd510794b39ca37808da8d9b9
chous <chous@acm-sl.org>
2014-04-30 19:14:06 +0200

    201404301914

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
@@ -341,7 +341,7 @@ public abstract class AbstractTemplateContext
     @Override
     public void setFileName(@NotNull final String fileName)
     {
-        new QueryJCommandWrapper<String>(getCommand()).setSetting();
+        new QueryJCommandWrapper<String>(getCommand()).setSetting(buildFileNameKey());
         return getValue(buildFileNameKey(), getCommand(), new FileNameNotAvailableException());
     }
 
