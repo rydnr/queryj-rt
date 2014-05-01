ef086cc69493cdafb6c563deb7b252a48190395f
chous <chous@acm-sl.org>
2014-05-01 19:51:06 +0200

    201405011951

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefImpl.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefImpl.java
@@ -39,6 +39,10 @@ package org.acmsl.queryj.templates.packaging;
  * Importing JetBrains annotations.
  */
 import org.acmsl.queryj.metadata.DecoratedString;
+
+/*
+ * Importing JetBrains annotations.
+ */
 import org.jetbrains.annotations.NotNull;
 
 /*
