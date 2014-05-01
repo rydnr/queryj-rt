af820791c06ff40d42af27250064b7e96a816834
chous <chous@acm-sl.org>
2014-05-01 20:25:19 +0200

    201405012025

--- queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
@@ -1056,7 +1056,7 @@ public abstract class AbstractTemplatesTest<G, F>
             }
             EasyMock.expect(tableDAO.findByName(table.getName())).andReturn(table).anyTimes();
             EasyMock.expect(tableDAO.findByDAO(table.getName())).andReturn(table).anyTimes();
-            EasyMock.expect(columnDAO.findAllColumns(table.getName())).andReturn(table.getAttributes());
+            EasyMock.expect(columnDAO.findAllColumns(table.getName())).andReturn(table.getAttributes().anyTimes());
         }
 
         EasyMock.replay(result);
