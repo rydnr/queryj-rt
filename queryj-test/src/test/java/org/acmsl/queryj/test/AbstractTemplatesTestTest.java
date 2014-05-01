e466f71089ded5ad572391fa661c82f90b3fb0e6
chous <chous@acm-sl.org>
2014-05-01 18:20:52 +0200

    201405011820

--- queryj-test/src/test/java/org/acmsl/queryj/test/AbstractTemplatesTestTest.java
+++ queryj-test/src/test/java/org/acmsl/queryj/test/AbstractTemplatesTestTest.java
@@ -41,7 +41,7 @@ package org.acmsl.queryj.test;
 import org.jetbrains.annotations.NotNull;
 
 /*
- * Importing checkthread.org annotations.
+ * Importing JUnit classes.
  */
 import org.junit.Test;
 import org.junit.runner.RunWith;
