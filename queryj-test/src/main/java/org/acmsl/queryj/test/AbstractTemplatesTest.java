5d2d421372db7aca47dcf4c10e2e6bc71e00d2d9
chous <chous@acm-sl.org>
2014-05-01 20:14:06 +0200

    201405012014

--- queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
@@ -1264,7 +1264,7 @@ public abstract class AbstractTemplatesTest<G, F>
 
         if (stream == null)
         {
-            stream =
+            stream = getClass().getResourceAsStream("/" + template + ".stg.def");
         }
         if (stream == null)
         {
