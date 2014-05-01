5d24efa9151cf02c68e1f1f0af0276fc87f31cfc
chous <chous@acm-sl.org>
2014-05-01 20:13:51 +0200

    201405012013

--- queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
@@ -1260,7 +1260,7 @@ public abstract class AbstractTemplatesTest<G, F>
     {
         @Nullable TemplateDef<String> result = null;
 
-        @Nullable final InputStream stream = getClass().getResourceAsStream("/" + template + ".stg.def");
+        @Nullable InputStream stream = getClass().getResourceAsStream("/" + template + ".stg.def");
 
         if (stream == null)
         {
