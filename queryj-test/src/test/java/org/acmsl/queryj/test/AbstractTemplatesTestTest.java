b8dd64ad3959f46500bf05b2d90133dc1d0f5430
chous <chous@acm-sl.org>
2014-05-01 18:24:03 +0200

    201405011824

--- queryj-test/src/test/java/org/acmsl/queryj/test/AbstractTemplatesTestTest.java
+++ queryj-test/src/test/java/org/acmsl/queryj/test/AbstractTemplatesTestTest.java
@@ -61,7 +61,7 @@ public class AbstractTemplatesTestTest
 {
     /**
      * Checks whether the retrieveTemplateDef() method actually
-     * retrieves a {@link org.acmsl.queryj.templates.TemplateDef}.
+     * retrieves a {@code org.acmsl.queryj.templates.TemplateDef}.
      */
     @Test
     public void retrieveTemplateDef_finds_the_templateDef()
