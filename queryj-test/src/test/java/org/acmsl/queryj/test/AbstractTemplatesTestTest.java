f02696ba8bce855df94c160c9010bc9f2cfaca10
chous <chous@acm-sl.org>
2014-05-01 18:22:14 +0200

    201405011822

--- queryj-test/src/test/java/org/acmsl/queryj/test/AbstractTemplatesTestTest.java
+++ queryj-test/src/test/java/org/acmsl/queryj/test/AbstractTemplatesTestTest.java
@@ -62,7 +62,7 @@ public class AbstractTemplatesTestTest
     {
         @NotNull final AbstractTemplatesTest instance = createInstance();
 
-        Assert.assertNotNull(instance.retrieveTemplateDef());
+        Assert.assertNotNull(instance.retrieveTemplateDef("my"));
     }
 
 }
