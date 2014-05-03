d98e31174fe23fd24ad6e150d2fc01d6940bc356
chous <chous@acm-sl.org>
2014-05-03 08:37:34 +0200

    201405030837

--- queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
+++ queryj-test/src/main/java/org/acmsl/queryj/test/AbstractTemplatesTest.java
@@ -787,7 +787,7 @@ public abstract class AbstractTemplatesTest<G, F>
                 @org.antlr.v4.runtime.misc.NotNull final String msg,
                 @org.antlr.v4.runtime.misc.Nullable final RecognitionException error)
             {
-                @NotNull final InvalidJavaOutputException
+                @NotNull final InvalidJavaOutputException exception;
             }
                 if (error == null)
                 {
