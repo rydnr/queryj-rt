02e217bbce47657ad26317384a3ff640dd29182e
chous <chous@acm-sl.org>
2014-05-03 08:30:13 +0200

    201405030830

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/TemplatePackagingNonCheckedException.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/TemplatePackagingNonCheckedException.java
@@ -47,7 +47,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- *
+ * Base class for all template-packagi
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/03/30 20:03
