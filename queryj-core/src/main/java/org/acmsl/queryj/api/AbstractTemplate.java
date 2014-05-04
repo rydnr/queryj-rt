2ec681c4d5d3835d716e6bb565e393f9c81c190c
chous <chous@acm-sl.org>
2014-05-04 13:17:58 +0200

    201405041317

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplate.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplate.java
@@ -812,7 +812,6 @@ public abstract class AbstractTemplate<C extends TemplateContext>
                         if (   (isInDevMode(t_Group))
                             && (!relevantOnly))
                         {
-
                             synchronized (AbstractTemplate.class)
                             {
                                 try
