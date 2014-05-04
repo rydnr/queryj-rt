2a327b2ab3d5b414f9f535ea86f000e879790b8c
chous <chous@acm-sl.org>
2014-05-04 07:55:10 +0200

    201405040755

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableDecoratorTest.java
@@ -263,7 +263,7 @@ public class AbstractTableDecoratorTest
 
         Assert.assertEquals(2, allAttributes.size());
 
-        Assert.assertTrue(allAttributes.getItems()childAttribute));
+        Assert.assertTrue(allAttributes.contains(childAttribute));
         Assert.assertTrue(allAttributes.contains(parentAttribute));
     }
 }
