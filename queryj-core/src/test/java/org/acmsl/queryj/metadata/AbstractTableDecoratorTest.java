fa97ee15e3b9dcbfc019a8bd78a2a630b3e12c8a
chous <chous@acm-sl.org>
2014-05-04 07:34:05 +0200

    201405040734

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractTableDecoratorTest.java
@@ -174,7 +174,7 @@ public class AbstractTableDecoratorTest
         @NotNull final DecoratorFactory decoratorFactory = CachingDecoratorFactory.getInstance();
         @NotNull final CustomSqlProvider customSqlProvider = EasyMock.createNiceMock(CustomSqlProvider.class);
 
-        @NotNull final AbstractTableDecorator instance = setupTableDecorator(new ArrayList<Attribute<String>>());
+        @NotNull final AbstractTableDecorator instance = setupTableDecorator(new ArrayList<>());
             new AbstractTableDecorator(table, metadataManager, decoratorFactory, customSqlProvider)
             {
                 /**
