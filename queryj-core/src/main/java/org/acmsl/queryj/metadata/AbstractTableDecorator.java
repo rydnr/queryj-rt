d732fd85672b6921c704198e892455c5618c688d
chous <chous@acm-sl.org>
2014-05-04 07:49:22 +0200

    201405040749

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
@@ -2103,7 +2103,7 @@ public abstract class AbstractTableDecorator
 
         if (parent != null)
         {
-            result.add(getAllAttributes())
+            result.add(getAllAttributes(new ArrayList<>(0), parent.getParentTable()))
         }
         return result;
     }
