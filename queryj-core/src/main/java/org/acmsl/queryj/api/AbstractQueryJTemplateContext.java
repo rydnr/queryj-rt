b019e90c72d9ced7689bc99c60bc9dcd29fa9ffe
chous <chous@acm-sl.org>
2014-05-06 10:35:28 +0200

    201405061035

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractQueryJTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractQueryJTemplateContext.java
@@ -385,4 +385,38 @@ public abstract class AbstractQueryJTemplateContext
         return result.toString();
     }
 
+
+    /**
+     * {@inheritDoc}.
+     */
+    @NotNull
+    @Override
+    public String getPackageName()
+    {
+        return getPackageName(getTemplateDef());
+    }
+
+    /**
+     * Retrieves the package name.
+     * @param templateDef the template def.
+     * @return such information.
+     */
+    @NotNull
+    protected String getPackageName(@NotNull final TemplateDef<String> templateDef)
+    {
+        @NotNull final String result;
+
+        switch (templateDef.getOutput())
+        {
+            case JAVA:
+                result = super.getPackageName();
+                break;
+            default:
+                result = "";
+                break;
+        }
+
+        return result;
+    }
+
 }
