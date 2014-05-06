e07de3fe6769bdf8ff8583e23f0470ef54777d01
chous <chous@acm-sl.org>
2014-05-06 16:06:48 +0200

    201405061606

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerCustomResultTemplateBuildHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerCustomResultTemplateBuildHandler.java
@@ -229,7 +229,8 @@ public abstract class BasePerCustomResultTemplateBuildHandler
 
     protected abstract T createTemplate(
         @NotNull final TF templateFactory,
-        @NotNull final Result<String> t_resultElement, final List<Property<String>> byResult, final QueryJCommand parameters);
+        @NotNull final Result<String> t_resultElement,
+        @NotNull final List<Property<String>> byResult, final QueryJCommand parameters);
 
     /**
      * Checks whether the generation is allowed for given result.
