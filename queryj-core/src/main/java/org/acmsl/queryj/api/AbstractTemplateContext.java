fd65077721dd2956cfd5e2b5c2fc42b539f99714
chous <chous@acm-sl.org>
2014-05-06 10:43:07 +0200

    201405061043

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
@@ -304,7 +304,7 @@ public abstract class AbstractTemplateContext
             aux = new QueryJCommandWrapper<T>(command).getSetting(key);
         }
 
-        if ()
+        if (aux ==)
         else
         {
             result = aux;
