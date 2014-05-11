5abb0e8ac529d81f20b4cf40513c083dde61f56e
chous <chous@acm-sl.org>
2014-05-11 08:53:43 +0200

    201405110853

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableDecoratorHelper.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableDecoratorHelper.java
@@ -471,7 +471,7 @@ public class TableDecoratorHelper
         for (@Nullable final Attribute<DecoratedString> attribute : attributes)
         {
             if (   (attribute != null)
-                   && (attribute.isNullable()))
+                && (attribute.isNullable()))
             {
                 result = true;
                 break;
