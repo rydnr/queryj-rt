e93db6729f633edea2cc3bb9a4d03797cacf329e
chous <chous@acm-sl.org>
2014-05-11 09:13:41 +0200

    201405110913

--- queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/TimestampHandler.java
+++ queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/TimestampHandler.java
@@ -89,6 +89,9 @@ public class TimestampHandler
     @NotNull
     public String getValue()
     {
+        @NotNull final String result;
+
+        !context.getDisableGenerationTimestamps()
         return new SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.US).format(new Date());
     }
 }
