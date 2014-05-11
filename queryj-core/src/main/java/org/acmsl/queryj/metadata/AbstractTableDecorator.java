145405bf9e472fe015373ada7e1618926a328846
chous <chous@acm-sl.org>
2014-05-11 08:32:13 +0200

    201405110832

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
@@ -2074,7 +2074,7 @@ public abstract class AbstractTableDecorator
     }
 
     /**
-     * Checks whether some of the attributes are nullable or not.
+     * Checks whether some of the given attributes are nullable or not.
      * @param attributes the {@link Attribute}s.
      * @param metadataTypeManager the {@link MetadataTypeManager} instance.
      * @return {@code true} in such case.
