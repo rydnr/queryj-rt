8e94c72e44f5dc9d0740e5556d0fa70152004996
chous <chous@acm-sl.org>
2014-05-11 09:01:12 +0200

    201405110901

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/TableDecoratorHelperTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/TableDecoratorHelperTest.java
@@ -76,7 +76,7 @@ public class TableDecoratorHelperTest
     @Test
     public void containNullableAttributes_detects_nullable_attributes()
     {
-        @NotNull final List<Attribute<String>> attributes = new ArrayList<>(0);
+        @NotNull final List<Attribute<String>> attributes = new ArrayList<>(4);
 
         @NotNull final Attribute<String> childAttribute1 =
             new AttributeIncompleteValueObject(
