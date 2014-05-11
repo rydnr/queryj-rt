5d7e0d66a7f8018c2c18f199a455d4864faa60b8
chous <chous@acm-sl.org>
2014-05-11 18:46:19 +0200

    201405111846

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerRepositoryTemplateBuildHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerRepositoryTemplateBuildHandler.java
@@ -181,7 +181,7 @@ public abstract class BasePerRepositoryTemplateBuildHandler
 
     /**
      * Retrieves the package name.
-     * @param repository the repositony.
+     * @param repository the repository.
      * @param engine the engine.
      * @param projectPackage the project package.
      * @return the package name.
