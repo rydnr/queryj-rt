be385a2db08ea42e1738d0aeb41f59b3c64263d2
chous <chous@acm-sl.org>
2014-05-11 21:06:45 +0200

    201405112106

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractQueryJTemplate.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractQueryJTemplate.java
@@ -51,9 +51,9 @@ import java.util.Map;
 
 /**
  * Represents generic templates.
+ * @param <C> the template context.
  * @author <a href="mailto:chous@acm-sl.org">Jose San Leandro Armendariz</a>
  * @since 2.0
- * @param <C> the template context.
  */
 public abstract class AbstractQueryJTemplate<C extends QueryJTemplateContext>
     extends AbstractTemplate<C>
