35c20b038daf85d0eb6cf544ca51a9d703edff5a
chous <chous@acm-sl.org>
2014-05-11 21:27:06 +0200

    201405112127

--- queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/HeaderHandler.java
+++ queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/HeaderHandler.java
@@ -36,7 +36,7 @@
 package org.acmsl.queryj.placeholders;
 
 /*
- * Importing some project classes.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.api.QueryJTemplateContext;
 import org.acmsl.queryj.metadata.DecoratedString;
