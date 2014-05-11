39c8742db82fe30890bd5eaa714179f29bb89606
chous <chous@acm-sl.org>
2014-05-11 08:51:51 +0200

    201405110851

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
@@ -2096,7 +2096,7 @@ public abstract class AbstractTableDecorator
     }
 
     /**
-     * Checks whether some of the given attributes are nullable or not.
+     * Checks whether some of the given attributes are no or not.
      * @param attributes the {@link Attribute}s.
      * @param tableDecoratorHelper the {@link TableDecoratorHelper} instance.
      * @return {@code true} in such case.
