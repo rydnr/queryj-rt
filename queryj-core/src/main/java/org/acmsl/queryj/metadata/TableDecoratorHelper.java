b9c01990d067d7268f361e4f085c5059248f1fe0
chous <chous@acm-sl.org>
2014-05-11 08:38:40 +0200

    201405110838

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableDecoratorHelper.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableDecoratorHelper.java
@@ -439,7 +439,6 @@ public class TableDecoratorHelper
     /**
      * Checks whether any attribute is nullable.
      * @param attributes the {@link Attribute}s.
-     * @param metadataTypeManager the {@link MetadataTypeManager} instance.
      * @return {@code true} in such case.
      */
     public boolean containNullableAttributes(
