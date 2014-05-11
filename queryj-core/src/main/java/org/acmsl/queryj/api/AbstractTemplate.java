ae71aa46ec83bb749ed7c21759a219ed55f81b82
chous <chous@acm-sl.org>
2014-05-11 21:05:10 +0200

    201405112105

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplate.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplate.java
@@ -754,8 +754,7 @@ public abstract class AbstractTemplate<C extends TemplateContext>
      */
     @Nullable
     protected String generateOutput(
-        @NotNull final C context,
-        final boolean relevantOnly)
+        @NotNull final C context, final boolean relevantOnly)
       throws InvalidTemplateException,
              DevelopmentModeException
     {
