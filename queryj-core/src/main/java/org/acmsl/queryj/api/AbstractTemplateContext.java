d70a5e2723031ad04183557871d62642bd26f0e2
chous <chous@acm-sl.org>
2014-05-12 06:01:51 +0200

    201405120601

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateContext.java
@@ -109,7 +109,7 @@ public abstract class AbstractTemplateContext
      * @param debug whether debugging is enabled.
      * @param command the {@link org.acmsl.queryj.QueryJCommand} instance.
      */
-    protected AbstractTemplateContext(@NotNull final String pk, @NotNull final QueryJCommand command)
+    protected AbstractTemplateContext(@NotNull final String pk, final boolean debug, @NotNull final QueryJCommand command)
     {
         immutableSetPk(pk);
         immutableSetCommand(command);
