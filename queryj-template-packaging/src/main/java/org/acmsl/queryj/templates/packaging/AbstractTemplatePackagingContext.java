6b93dd0886bba8439bc5d4e4f6e91bb4de1515e3
chous <chous@acm-sl.org>
2014-05-12 06:08:03 +0200

    201405120608

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/AbstractTemplatePackagingContext.java
@@ -81,6 +81,7 @@ public abstract class AbstractTemplatePackagingContext
     /**
      * Creates a new instance.
      * @param pk the pk.
+    final boolean debug,
      * @param command the command.
      */
     public AbstractTemplatePackagingContext(
