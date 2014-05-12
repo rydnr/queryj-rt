f3357403c4db193f0782da51ea3dcb709763217b
chous <chous@acm-sl.org>
2014-05-12 05:54:46 +0200

    201405120554

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplate.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplate.java
@@ -274,7 +274,7 @@ public abstract class AbstractTemplate<C extends TemplateContext>
      * @param placeholderPackage the package of the placeholder classes.
      */
     protected AbstractTemplate(
-        @NotNull final C context, @NotNull final String placeholderPackage, final )
+        @NotNull final C context, @NotNull final String placeholderPackage, final boolean debugEnabled)
     {
         immutableSetTemplateContext(context);
         immutableSetPlaceholderPackage(placeholderPackage);
