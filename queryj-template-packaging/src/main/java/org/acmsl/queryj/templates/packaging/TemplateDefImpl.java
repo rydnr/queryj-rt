9d3c4c434d4984534e177e995a4ac93111603479
chous <chous@acm-sl.org>
2014-05-12 06:10:35 +0200

    201405120610

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefImpl.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefImpl.java
@@ -356,7 +356,7 @@ public class TemplateDefImpl
      * @param disabled whether the template def is disabled.
      */
     @SuppressWarnings("unused")
-    protected void setDisabled(final boolean debug)
+    protected void setDebug(final boolean debug)
     {
         immutableSetDebug(debug);
     }
