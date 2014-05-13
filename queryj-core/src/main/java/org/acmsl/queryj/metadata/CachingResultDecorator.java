9a9299b9469771962e75920ccc05f19c05154c5b
chous <chous@acm-sl.org>
2014-05-13 16:59:57 +0200

    201405131659

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingResultDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingResultDecorator.java
@@ -365,7 +365,7 @@ public class CachingResultDecorator
         result.append("\", \"cachedLobProperties\": \"");
         result.append(this.m__lCachedLobProperties);
         result.append("\", \"cachedImplicit\": \"");
-        result.append(this.m__lCachedImplicitProperties);
+        result.append(this.m__lCachedImplicit);
         result.append("\", \"cachedImplicitProperties\": \"");
         result.append(this.m__lCachedImplicitProperties);
         result.append(", \"package\": \"org.acmsl.queryj.metadata\" }");
