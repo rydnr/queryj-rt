0703fdac52cccfa3b9a0cc0ab440f07bf3c945eb
chous <chous@acm-sl.org>
2014-05-13 08:34:50 +0200

    201405130834

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/ResultDecoratorHelperTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/ResultDecoratorHelperTest.java
@@ -76,7 +76,7 @@ public class ResultDecoratorHelperTest
         @NotNull final Property<String> property2 =
             new PropertyElement<>("prop2", "name", 2, "String", false);
 
-        @NotNull final Property<String> property2 =
+        @NotNull final Property<String> property3 =
             new PropertyElement<>("prop2", "name", 2, "String", false);
 
         @NotNull final List<Property<String>> properties = new ArrayList<>(4);
