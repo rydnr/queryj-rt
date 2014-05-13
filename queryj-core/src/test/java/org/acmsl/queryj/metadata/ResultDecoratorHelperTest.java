c4b59a7fe77074c956fedfced5573575120cb260
chous <chous@acm-sl.org>
2014-05-13 08:36:12 +0200

    201405130836

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/ResultDecoratorHelperTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/ResultDecoratorHelperTest.java
@@ -90,7 +90,7 @@ public class ResultDecoratorHelperTest
         properties.add(property4);
 
         @NotNull final AbstractTableDecorator result =
-            AbstractTableDecoratorTest.setupTableDecorator(attributes1, null);
+            setupTableDecorator(attributes1, null);
 
         Assert.assertTrue(instance.containNullableAttributes(table1.getAttributes()));
 
