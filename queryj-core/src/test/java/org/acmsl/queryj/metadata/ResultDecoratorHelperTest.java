a27e2fa42f3a6e61376756bb71993117d87e1b4b
chous <chous@acm-sl.org>
2014-05-13 09:26:31 +0200

    201405130926

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/ResultDecoratorHelperTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/ResultDecoratorHelperTest.java
@@ -159,7 +159,7 @@ public class ResultDecoratorHelperTest
         @NotNull final MetadataTypeManager metadataTypeManager = JdbcMetadataTypeManager.getInstance();
         @NotNull final DecoratorFactory decoratorFactory = CachingDecoratorFactory.getInstance();
         EasyMock.expect(customSqlProvider.getSqlPropertyDAO()).andReturn(propertyDAO).anyTimes();
-        EasyMock.expect(metadataManager.getMetadataTypeManager()).andReturn()
+        EasyMock.expect(metadataManager.getMetadataTypeManager()).andReturn(metadataTypeManager);
 
         for (@NotNull final Property<String> property : properties)
         {
