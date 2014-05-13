667199b8c0e193f6b95568842e62d27015d16c42
chous <chous@acm-sl.org>
2014-05-13 16:57:18 +0200

    201405131657

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingResultDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingResultDecorator.java
@@ -360,7 +360,7 @@ public class CachingResultDecorator
         result.append(CachingResultDecorator.class.getSimpleName());
         result.append("\", \"super\": ");
         result.append(super.toString());
-        result.append("")
+        result.append(", p")
 
     }
 
