83db1b481eb559657166bcb531f707e3f2f4962d
chous <chous@acm-sl.org>
2014-05-13 16:56:29 +0200

    201405131656

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingResultDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingResultDecorator.java
@@ -356,7 +356,7 @@ public class CachingResultDecorator
     public String toString()
     {
         @NotNull final StringBuilder result =
-            new StringBuilder("{ \"class\": \"" + CachingResultDecorator.class.getSimpleName())
+            new StringBuilder("{ \"class\": \"" + CachingResultDecorator.class.getSimpleName();
 
     }
 
