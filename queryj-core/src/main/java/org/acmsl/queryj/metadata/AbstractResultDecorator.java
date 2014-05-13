5ed9356da8c9b7e87bdb45cc8de5404ba7c2e421
chous <chous@acm-sl.org>
2014-05-13 08:26:57 +0200

    201405130826

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
@@ -679,7 +679,7 @@ public abstract class AbstractResultDecorator
 
     /**
      * Checks whether some of the given attributes cannot be null.
-     * @param properties the {@link Properties}.
+     * @param properties the {@link Property properties}.
      * @param metadataTypeManager the {@link MetadataTypeManager} instance.
      * @param tableDecoratorHelper the {@link TableDecoratorHelper} instance.
      * @return {@code true} in such case.
