c3c7ee75776c1b1c1c9e854c2ca013994e29474e
chous <chous@acm-sl.org>
2014-05-13 08:39:44 +0200

    201405130839

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/ResultDecoratorHelperTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/ResultDecoratorHelperTest.java
@@ -103,5 +103,5 @@ public class ResultDecoratorHelperTest
         Assert.assertTrue(instance.containNullableProperties(result.getProperties()));
     }
 
-    protected AbstractResultDecorator setupResultDecorator()
+    protected AbstractResultDecorator setupResultDecorator(@NotNull final List<Property<String>> )
 }
