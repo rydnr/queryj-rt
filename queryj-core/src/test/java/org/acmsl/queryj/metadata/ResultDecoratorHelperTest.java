ed5f0b5d1e6cb91d8ec8684762f5d972c36cb2f5
chous <chous@acm-sl.org>
2014-05-13 08:41:10 +0200

    201405130841

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/ResultDecoratorHelperTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/ResultDecoratorHelperTest.java
@@ -105,7 +105,7 @@ public class ResultDecoratorHelperTest
     /**
      * Sets up an {@link AbstractResultDecorator} instance, for testing purposes.
      * @param properties the {@link Property properties}.
-     * @return
+     * @return the decorator.
      */
     @NotNull
     protected AbstractResultDecorator setupResultDecorator(@NotNull final List<Property<String>> properties)
