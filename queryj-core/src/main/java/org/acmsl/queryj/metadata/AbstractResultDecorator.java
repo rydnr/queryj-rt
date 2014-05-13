9e127dcb32ff74b02d3c6f18503a16a3169937a7
chous <chous@acm-sl.org>
2014-05-13 08:26:03 +0200

    201405130826

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
@@ -685,7 +685,7 @@ public abstract class AbstractResultDecorator
      * @return {@code true} in such case.
      */
     protected boolean containNotNullAttributes(
-        @NotNull final ListDecorator<Attribute<DecoratedString>> attributes,
+        @NotNull final ListDecorator<Property<DecoratedString>> attributes,
         @NotNull final MetadataTypeManager metadataTypeManager,
         @NotNull final TableDecoratorHelper tableDecoratorHelper)
     {
