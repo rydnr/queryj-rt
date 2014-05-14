a705c2eccb9aa0e09ca6ba86f82e5423dd1293dd
jlean <jose.sanleandro@ventura24.es>
2014-05-14 10:38:24 +0200

    201405141038

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractPropertyDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractPropertyDecoratorTest.java
@@ -46,6 +46,8 @@ import org.jetbrains.annotations.NotNull;
 /*
  * Importing checkthread.org annotations.
  */
+import org.easymock.EasyMock;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
