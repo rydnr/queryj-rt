bb7a33f1b1c83206ed8f64e62d79c01c52658f11
jlean <jose.sanleandro@ventura24.es>
2014-05-14 11:21:35 +0200

    201405141121

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/CustomResultUtils.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/CustomResultUtils.java
@@ -319,6 +319,7 @@ public class CustomResultUtils
                     {
                         result = tableName;
                     }
+
                     if (result != null)
                     {
                         cacheEntry("" + resultElement.getId(), result);
