32474b2cf855957a275f13426d8c51b830d334c7
jlean <jose.sanleandro@ventura24.es>
2014-05-14 10:31:34 +0200

    201405141031

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractPropertyDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractPropertyDecorator.java
@@ -1,5 +1,5 @@
 /*
-                        QueryJ
+                        QueryJ Core
 
     Copyright (C) 2002-today  Jose San Leandro Armendariz
                               chous@acm-sl.org
@@ -33,7 +33,7 @@
 package org.acmsl.queryj.metadata;
 
 /*
- * Importing project-specific classes.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.customsql.IdentifiableElement;
 import org.acmsl.queryj.customsql.Property;
