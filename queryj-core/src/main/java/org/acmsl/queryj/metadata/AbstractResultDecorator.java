c55583e38665444d182fc6a252b75fd05f47e658
chous <chous@acm-sl.org>
2014-05-15 21:24:39 +0200

    201405152124

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
@@ -72,7 +72,6 @@ import org.checkthread.annotations.ThreadSafe;
  * Importing JDK classes.
  */
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
