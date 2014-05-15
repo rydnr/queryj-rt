98935564765e54e82483baba72109073335abc0b
chous <chous@acm-sl.org>
2014-05-15 21:12:41 +0200

    201405152112

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
@@ -744,7 +744,7 @@ public abstract class AbstractResultDecorator
     @NotNull
     protected String getSimpleClassValue(@NotNull final String classValue)
     {
-        return StringUtils.getInstance().retrieveLastWord(classValue, new String[]
+        return StringUtils.getInstance().retrieveLastWord(classValue, new String[] { "." });
     }
 
     /**
