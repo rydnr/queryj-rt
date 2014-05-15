bbe43e8f43f67d50824d6053aca8c2badbad3fcd
chous <chous@acm-sl.org>
2014-05-15 21:24:10 +0200

    201405152124

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractResultDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractResultDecoratorTest.java
@@ -152,7 +152,7 @@ public class AbstractResultDecoratorTest
      * Checks getPackage() retrieves the package name.
      */
     @Test
-    public void getPackage_removes_the_package_name()
+    public void getPackage_retrieves_the_package_name()
     {
         @NotNull final AbstractResultDecorator result =
             AbstractResultDecoratorTest.setupResultDecorator("my.id", "com.foo.bar.MyResult", new ArrayList<>(0));
