ec6f6f5e5b45899fb3b8aae61341bf57ba109a46
chous <chous@acm-sl.org>
2014-05-15 21:23:11 +0200

    201405152123

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
@@ -759,7 +759,7 @@ public abstract class AbstractResultDecorator
         else
         {
             result =
-                new DecoratedString(stringUtils.extractClassName(classValue.getValue(), new String[] { "\\." }));
+                new DecoratedString(stringUtils.extractClassName(classValue.getValue());
         }
 
         return result;
