9580ed65656965c030e257cef81786c51ad7d5c4
chous <chous@acm-sl.org>
2014-05-15 21:23:17 +0200

    201405152123

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractResultDecorator.java
@@ -758,8 +758,7 @@ public abstract class AbstractResultDecorator
         }
         else
         {
-            result =
-                new DecoratedString(stringUtils.extractClassName(classValue.getValue()));
+            result = new DecoratedString(stringUtils.extractClassName(classValue.getValue()));
         }
 
         return result;
