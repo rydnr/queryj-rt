6492b4409d344b0831fe3ab2af72bd75b705f36b
chous <chous@acm-sl.org>
2014-05-16 20:40:28 +0200

    201405162040

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractAttributeDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractAttributeDecoratorTest.java
@@ -368,7 +368,7 @@ public class AbstractAttributeDecoratorTest
     }
 
     /**
-     * Checks whether isFloat() returns false for floats.
+     * Checks whether isDouble() returns false for floats.
      */
     @Test
     public void isFloat_returns_false_for_doubles()
