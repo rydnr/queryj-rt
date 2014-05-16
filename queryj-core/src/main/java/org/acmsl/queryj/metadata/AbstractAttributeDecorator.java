a4e2f817ef519d0fad554db458338f7aeffc8426
chous <chous@acm-sl.org>
2014-05-16 21:00:14 +0200

    201405162100

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractAttributeDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractAttributeDecorator.java
@@ -951,7 +951,7 @@ public abstract class AbstractAttributeDecorator
      */
     public boolean isDouble()
     {
-        return isDouble(getType().getValue());
+        return isDouble(getTypeId(), getMetadataManager());
     }
 
     /**
