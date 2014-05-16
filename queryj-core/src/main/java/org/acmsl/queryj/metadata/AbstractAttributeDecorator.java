1b6a5db6300c0c0af73df7ef6f1a82165144bff8
chous <chous@acm-sl.org>
2014-05-16 20:51:02 +0200

    201405162051

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractAttributeDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractAttributeDecorator.java
@@ -964,6 +964,15 @@ public abstract class AbstractAttributeDecorator
     }
 
     /**
+     * Checks whether this attribute is decimal or not.
+     * @return {@code true in such case.}
+     */
+    public boolean isDecimal()
+    {
+        return false;
+    }
+
+    /**
      * {@inheritDoc}
      */
     @NotNull
