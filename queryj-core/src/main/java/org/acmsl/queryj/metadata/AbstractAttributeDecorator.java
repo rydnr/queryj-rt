5eb342bf906e42291059a17bf799aa4ccfe2cbd7
chous <chous@acm-sl.org>
2014-05-16 20:50:31 +0200

    201405162050

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractAttributeDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractAttributeDecorator.java
@@ -1008,5 +1008,6 @@ public abstract class AbstractAttributeDecorator
 
     public boolean isDecimal()
     {
+        return false;  //To change body of created methods use File | Settings | File Templates.
     }
 }
