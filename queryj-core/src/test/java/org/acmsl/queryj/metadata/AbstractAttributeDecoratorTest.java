e7f7294fa6736f96a9698648acf71ae83f5a3832
chous <chous@acm-sl.org>
2014-05-16 20:49:51 +0200

    201405162049

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractAttributeDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractAttributeDecoratorTest.java
@@ -406,4 +406,46 @@ public class AbstractAttributeDecoratorTest
 
         Assert.assertFalse(instance.isDouble());
     }
+
+    /**
+     * Checks whether isDouble() returns true for doubles.
+     */
+    @Test
+    public void isDouble_returns_true_for_doubles()
+    {
+        @NotNull final MetadataManager metadataManager = EasyMock.createNiceMock(MetadataManager.class);
+
+        @NotNull final MetadataTypeManager metadataTypeManager = new JdbcMetadataTypeManager();
+
+        EasyMock.expect(metadataManager.getMetadataTypeManager()).andReturn(metadataTypeManager).anyTimes();
+
+        EasyMock.replay(metadataManager);
+
+        @NotNull final Attribute<String> attribute =
+            new AttributeValueObject(
+                "name",
+                Types.DOUBLE,
+                "double",
+                "tableName",
+                "comment",
+                1,
+                10, // length
+                1, // precision
+                null, // keyword
+                null, // retrieval query
+                null, // sequence
+                false, // nullable
+                null, // value
+                false, // read-only
+                false, // is-bool
+                null, // boolean-true
+                null, // boolean-false
+                null); // boolean-null
+
+        @NotNull final AbstractAttributeDecorator instance =
+            new AbstractAttributeDecorator(attribute, metadataManager) {};
+
+        Assert.assertTrue(instance.isDouble());
+    }
+
 }
