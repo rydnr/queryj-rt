8dfccbc527fbc71a31e093ebd0ae3169455152e3
chous <chous@acm-sl.org>
2014-05-17 11:11:14 +0200

    201405171111

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractSqlDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/AbstractSqlDecoratorTest.java
@@ -127,7 +127,7 @@ public class AbstractSqlDecoratorTest
             new ResultElement<>("r1", String.class.getSimpleName());
 
         @NotNull final Parameter<String> parameter = new
-            ParameterElement()
+            ParameterElement<>()
         @NotNull final CustomSqlProvider customSqlProvider = EasyMock.createNiceMock(CustomSqlProvider.class);
         @NotNull final SqlResultDAO resultDAO = EasyMock.createNiceMock(SqlResultDAO.class);
         @NotNull final MetadataManager metadataManager = EasyMock.createNiceMock(MetadataManager.class);
