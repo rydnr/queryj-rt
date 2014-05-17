1714baca6c2aa3d56e79a07213f587e59a7d834d
chous <chous@acm-sl.org>
2014-05-17 18:46:27 +0200

    201405171846

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerCustomResultTemplateBuildHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerCustomResultTemplateBuildHandler.java
@@ -358,7 +358,7 @@ public abstract class BasePerCustomResultTemplateBuildHandler
         for (@Nullable final Result<String> customResult : results)
         {
             if (   (customResult != null)
-                && (!resultsByClass.containsKey(customResult.getClassValue()))
+                && (!resultsByClass.containsKey(customResult.getClassValue())))
             {
                 res
             }
