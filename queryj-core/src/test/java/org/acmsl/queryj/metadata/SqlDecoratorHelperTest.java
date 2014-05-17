4add25877fef825a448d00db5f377554ce2f5b04
chous <chous@acm-sl.org>
2014-05-17 11:30:03 +0200

    201405171130

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/SqlDecoratorHelperTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/SqlDecoratorHelperTest.java
@@ -110,7 +110,7 @@ public class SqlDecoratorHelperTest
         (sql, customSqlProvider, metadataManager) {};
 
         @NotNull final List<DecoratedString> parameterTypes =
-            instance.getParameterTypes(sql.getParameters(), metadataTypeManager);
+            instance.getParameterTypes(sqlDecorator.getParameters(), metadataTypeManager);
 
         Assert.assertEquals(1, parameterTypes.size());
 
