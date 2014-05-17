ab8aa0037f5b72c0a44846fc9997b54f84df307a
chous <chous@acm-sl.org>
2014-05-17 13:39:28 +0200

    201405171339

--- queryj-core/src/test/java/org/acmsl/queryj/api/handlers/BasePerCustomResultTemplateBuildHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/handlers/BasePerCustomResultTemplateBuildHandlerTest.java
@@ -78,8 +78,9 @@ public class BasePerCustomResultTemplateBuildHandlerTest
         @NotNull final List<Result<String>> results = new ArrayList<>(4);
 
         results.add(new ResultElement<>("r1", "Class1"));
-        results.add(new ResultElement<>("r1", "Class1"));
-        results.add(new ResultElement<>("r1", "Class1"));
+        results.add(new ResultElement<>("r1", "Class2"));
+        results.add(new ResultElement<>("r1", "Class3"));
+        results.add(new ResultElement<>("r1", "Class4"));
 
         Assert.assertEquals(3, instance.fixDuplicated(results));
     }
