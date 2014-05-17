9e78a202dd8de855588d9c09ee179852e13ff9a8
chous <chous@acm-sl.org>
2014-05-17 12:32:40 +0200

    201405171232

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/SqlDecoratorHelper.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/SqlDecoratorHelper.java
@@ -94,7 +94,7 @@ public class SqlDecoratorHelper
     {
         @NotNull final List<DecoratedString> result = new ArrayList<>(parameters.size());
 
-        for (@Nullable final Attribute<DecoratedString> attr: parameters)
+        for (@Nullable final Parameter<DecoratedString, ?> attr: parameters)
         {
             if (attr != null)
             {
