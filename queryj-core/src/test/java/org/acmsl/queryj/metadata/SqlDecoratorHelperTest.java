19a77f18cfe1e412f24c622acd63ad4cbaf298db
chous <chous@acm-sl.org>
2014-05-17 11:27:46 +0200

    201405171127

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/SqlDecoratorHelperTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/SqlDecoratorHelperTest.java
@@ -38,15 +38,28 @@ package org.acmsl.queryj.metadata;
 /*
  * Importing JetBrains annotations.
  */
+import org.acmsl.queryj.customsql.CustomSqlProvider;
+import org.acmsl.queryj.customsql.Parameter;
+import org.acmsl.queryj.customsql.ParameterElement;
+import org.acmsl.queryj.customsql.ParameterRefElement;
+import org.acmsl.queryj.customsql.Result;
+import org.acmsl.queryj.customsql.ResultElement;
+import org.acmsl.queryj.customsql.Sql;
+import org.acmsl.queryj.customsql.Sql.Cardinality;
+import org.acmsl.queryj.customsql.SqlElement;
+import org.easymock.EasyMock;
 import org.jetbrains.annotations.NotNull;
 
 /*
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.List;
 
 /**
  * Tests for {@link SqlDecoratorHelper}.
@@ -58,5 +71,42 @@ import org.junit.runners.JUnit4;
 public class SqlDecoratorHelperTest
 {
     @Test
-    public void getParameterTypes
+    public void getParameterTypes_returns_no_duplicates()
+    {
+        @NotNull final Sql<String> sql =
+            new SqlElement<>("id1", "name1", "select", Cardinality.SINGLE, "all", false, false, "none", "desc1");
+
+        @NotNull final Result<String> result =
+            new ResultElement<>("r1", String.class.getSimpleName());
+
+        @NotNull final Parameter parameter =
+            new ParameterElement<String, String>("pid", 1, "paramName", "Date", null);
+
+        sql.add(new ParameterRefElement("pid"));
+
+        @NotNull final CustomSqlProvider customSqlProvider = EasyMock.createNiceMock(CustomSqlProvider.class);
+        @NotNull final SqlResultDAO resultDAO = EasyMock.createNiceMock(SqlResultDAO.class);
+        @NotNull final MetadataManager metadataManager = EasyMock.createNiceMock(MetadataManager.class);
+        @NotNull final SqlParameterDAO parameterDAO = EasyMock.createNiceMock(SqlParameterDAO.class);
+
+        EasyMock.expect(customSqlProvider.getSqlResultDAO()).andReturn(resultDAO);
+        EasyMock.expect(customSqlProvider.getSqlParameterDAO()).andReturn(parameterDAO).anyTimes();
+        EasyMock.expect(parameterDAO.findByPrimaryKey("pid")).andReturn(parameter).anyTimes();
+        EasyMock.expect(resultDAO.findBySqlId(sql.getId())).andReturn(result);
+
+        EasyMock.replay(customSqlProvider);
+        EasyMock.replay(parameterDAO);
+        EasyMock.replay(resultDAO);
+
+        @NotNull final AbstractSqlDecorator instance =
+            new AbstractSqlDecorator(sql, customSqlProvider, metadataManager) {};
+
+        @NotNull final List<DecoratedString> parameterTypes = instance.getParameterTypes();
+
+        Assert.assertEquals(1, parameterTypes.size());
+
+        EasyMock.verify(customSqlProvider);
+        EasyMock.verify(parameterDAO);
+
+    }
 }
