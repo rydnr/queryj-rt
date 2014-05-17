4677b46027efa4fce30305668f1ef9ac52393941
chous <chous@acm-sl.org>
2014-05-17 13:34:03 +0200

    201405171334

--- queryj-core/src/test/java/org/acmsl/queryj/api/handlers/BasePerCustomResultTemplateBuildHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/handlers/BasePerCustomResultTemplateBuildHandlerTest.java
@@ -117,6 +117,6 @@ public class BasePerCustomResultTemplateBuildHandlerTest
                     @NotNull final List templates, @NotNull final QueryJCommand parameters)
                 {
                 }
-            }
+            };
     }
 }
