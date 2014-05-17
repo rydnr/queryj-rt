8b77660c9a4053e58c47ba71792590fffd3115fd
chous <chous@acm-sl.org>
2014-05-17 19:05:50 +0200

    201405171905

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractParameterDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractParameterDecorator.java
@@ -33,7 +33,7 @@
 package org.acmsl.queryj.metadata;
 
 /*
- * Importing project-specific classes.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.customsql.Parameter;
 import org.acmsl.queryj.customsql.ParameterElement;
