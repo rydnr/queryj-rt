d7e3a2404f7579f3e14d2d3f13071eb3e96892c1
chous <chous@acm-sl.org>
2014-05-17 11:28:19 +0200

    201405171128

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/SqlDecoratorHelperTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/SqlDecoratorHelperTest.java
@@ -102,7 +102,7 @@ public class SqlDecoratorHelperTest
         EasyMock.replay(parameterDAO);
         EasyMock.replay(resultDAO);
 
-        @NotNull final SqlDecorator instance =
+        @NotNull final SqlDecoratorHelper instance =
             new AbstractSqlDecorator(sql, customSqlProvider, metadataManager) {};
 
         @NotNull final List<DecoratedString> parameterTypes = instance.getParameterTypes();
