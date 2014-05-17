6067f93176651e8d2a3122f853d0e4dc2a3d067d
chous <chous@acm-sl.org>
2014-05-17 18:48:53 +0200

    201405171848

--- queryj-core/src/test/java/org/acmsl/queryj/api/handlers/BasePerCustomResultTemplateBuildHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/handlers/BasePerCustomResultTemplateBuildHandlerTest.java
@@ -103,7 +103,9 @@ public class BasePerCustomResultTemplateBuildHandlerTest
     {
         return
             new BasePerCustomResultTemplateBuildHandler
-                <PerCustomResultTemplate<PerCustomResultTemplateContext>()
+                <PerCustomResultTemplate<PerCustomResultTemplateContext>,
+                    PerCustomResultTemplateContext,
+                ()
             {
                 /**
                  * {@inheritDoc}
