1d08c5bdc9e3131113c103912330b46e86a0fc17
chous <chous@acm-sl.org>
2014-05-17 11:20:54 +0200

    201405171120

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractSqlDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractSqlDecorator.java
@@ -652,7 +652,7 @@ public abstract class AbstractSqlDecorator
         {
             if (parameter != null)
             {
-
+                parameter.getType()
             }
         }
         return result;
