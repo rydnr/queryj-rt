73c429141f35a3d0c3c331a03cddeaac78698c11
chous <chous@acm-sl.org>
2014-05-18 12:42:21 +0200

    201405181242

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingTableDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingTableDecorator.java
@@ -38,7 +38,7 @@
 package org.acmsl.queryj.metadata;
 
 /*
- * Importing project classes.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.customsql.CustomSqlProvider;
 import org.acmsl.queryj.customsql.Result;
@@ -68,8 +68,7 @@ import org.checkthread.annotations.ThreadSafe;
 /**
  * Adds a simple caching mechanism while decorating <code>Table</code>
  * instances.
- * @author <a href="mailto:chous@acm-sl.org"
- *         >Jose San Leandro</a>
+ * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  */
 @ThreadSafe
 public class CachingTableDecorator
