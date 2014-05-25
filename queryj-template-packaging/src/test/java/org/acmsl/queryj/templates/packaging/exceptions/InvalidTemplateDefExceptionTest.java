0a7fb4fde47012c7647eaf2df6132c4314f22dbd
chous <chous@acm-sl.org>
2014-05-25 21:08:12 +0200

    201405252108

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/InvalidTemplateDefExceptionTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/InvalidTemplateDefExceptionTest.java
@@ -85,7 +85,8 @@ public class InvalidTemplateDefExceptionTest
     }
 
     /**
-     * Tests the "per-table-templates-feature" exception is defined for Spanish and English.
+     * Tests the template def parser errors due to invalid "name" values
+     * are defined for Spanish and English.
      */
     @Test
     public void per_table_templates_feature_message_is_defined_in_Spanish_and_English()
