cabfb41e1ef71d35f118c3ad8b73fe4e467b8120
chous <chous@acm-sl.org>
2014-05-25 21:05:40 +0200

    201405252105

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/InvalidTemplateDefExceptionTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/InvalidTemplateDefExceptionTest.java
@@ -130,7 +130,7 @@ public class InvalidTemplateDefExceptionTest
      */
     protected void checkExceptionMessageIsDefinedInSpanishAndEnglish(@NotNull final String type)
     {
-        @NotNull final MissingTemplatesException instance = new MissingTemplatesException(templateName);
+        @NotNull final MissingTemplatesException instance = new MissingTemplatesException(type);
 
         for (@NotNull final Locale t_Locale : Arrays.asList(new Locale("en"), new Locale("es")))
         {
