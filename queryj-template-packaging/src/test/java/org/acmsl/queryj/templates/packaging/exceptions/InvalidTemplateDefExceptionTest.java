66aee838f1cda59cd53ed8808cfdf4e6d1b729ba
chous <chous@acm-sl.org>
2014-05-25 21:07:26 +0200

    201405252107

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/InvalidTemplateDefExceptionTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/InvalidTemplateDefExceptionTest.java
@@ -65,7 +65,7 @@ import java.util.Locale;
 public class InvalidTemplateDefExceptionTest
 {
     /**
-     * Tests the "name" exception is defined for Spanish and English.
+     * Tests the invalid template def "name" exception is defined for Spanish and English.
      */
     @Test
     public void template_chain_provider_message_is_defined_in_Spanish_and_English()
