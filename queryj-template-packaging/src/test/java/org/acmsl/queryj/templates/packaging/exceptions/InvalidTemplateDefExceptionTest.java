be83f08fb300d396ee650d0d6671bac5d63842a4
chous <chous@acm-sl.org>
2014-05-25 21:09:10 +0200

    201405252109

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/InvalidTemplateDefExceptionTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/InvalidTemplateDefExceptionTest.java
@@ -85,7 +85,7 @@ public class InvalidTemplateDefExceptionTest
     }
 
     /**
-     * Tests the template def parser errors due to invalid "name" values
+     * Tests the template def parser errors due to invalid "output" values
      * are defined for Spanish and English.
      */
     @Test
