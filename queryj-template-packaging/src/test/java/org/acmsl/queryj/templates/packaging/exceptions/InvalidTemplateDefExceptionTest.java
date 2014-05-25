2e9952dd95c1cea8dee1473ff57ad29b7a87b0f9
chous <chous@acm-sl.org>
2014-05-25 21:08:39 +0200

    201405252108

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/InvalidTemplateDefExceptionTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/exceptions/InvalidTemplateDefExceptionTest.java
@@ -125,7 +125,7 @@ public class InvalidTemplateDefExceptionTest
     }
 
     /**
-     * Tests the template def parser errors due to invalid "debuc" values
+     * Tests the template def parser errors due to invalid "debug" values
      * are defined for Spanish and English.
      */
     @Test
