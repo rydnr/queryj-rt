a6a8e66779a18d21456704ae8521be261d6c5d8e
chous <chous@acm-sl.org>
2014-05-26 08:36:52 +0200

    201405260836

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefImplTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefImplTest.java
@@ -62,7 +62,7 @@ public class TemplateDefImplTest
     @Test
     public void getDefName_is_based_on_the_filename()
     {
-
+        @NotNull final File
         @NotNull final TemplateDef<String> instance =
             new TemplateDefImpl(
                 "name",
