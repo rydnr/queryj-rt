d032af5091634266ce958fb827f81de08c66d935
chous <chous@acm-sl.org>
2014-05-26 08:43:26 +0200

    201405260843

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefImpl.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefImpl.java
@@ -454,7 +454,7 @@ public class TemplateDefImpl
 
         @NotNull final String absolutePath = file.getAbsolutePath();
 
-        @NotNull final String fileName = fileUtils.getFileName()
+        @NotNull final String fileName = fileUtils.getFileName(absolutePath);
         return result;
     }
 
