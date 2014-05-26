b9d848c63bd6ec977aa86544e7c0e13b555d24e0
chous <chous@acm-sl.org>
2014-05-26 08:30:47 +0200

    201405260830

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/placeholders/DecoratedTemplateDefWrapper.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/placeholders/DecoratedTemplateDefWrapper.java
@@ -191,6 +191,9 @@ public class DecoratedTemplateDefWrapper
     }
 
     /**
+     * Retrieves the
+     */
+    /**
      * Retrieves the filename builder expression.
      * @return such expression.
      */
