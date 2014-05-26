4bf488e9792ab2315a64580b024507d2cb3ef020
chous <chous@acm-sl.org>
2014-05-26 08:41:56 +0200

    201405260841

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefImpl.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefImpl.java
@@ -442,6 +442,7 @@ public class TemplateDefImpl
 
     /**
      * Retrieves the name of the template def itself.
+     * @param file the def file.
      * @return such information.
      */
     @NotNull
