d75abd3015b8006b5bbef449f06193773cbcd76d
chous <chous@acm-sl.org>
2014-05-27 07:25:30 +0200

    201405270725

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/ParseTemplateDefsHandler.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/ParseTemplateDefsHandler.java
@@ -417,10 +417,7 @@ public class ParseTemplateDefsHandler
     throws RecognitionException,
            IOException
     {
-        return setUpParser()@NotNull final TemplateDefParser result;
-
-        @NotNull final TemplateDefLexer t_Lexer =
-            new TemplateDefLexer(new ANTLRFileStream(file.getAbsolutePath()));
+        return setUpParser(new ANTLRFileStream(file.getAbsolutePath()));
 
         @NotNull final CommonTokenStream t_Tokens = new CommonTokenStream(t_Lexer);
 
