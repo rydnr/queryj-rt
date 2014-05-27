caf2c1a4a40a0e6b3bfb73c0ecf5370a984911b8
chous <chous@acm-sl.org>
2014-05-27 07:11:03 +0200

    201405270711

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerTableTemplateContext.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerTableTemplateContext.java
@@ -51,7 +51,7 @@ import org.checkthread.annotations.ThreadSafe;
 import java.util.List;
 
 /**
- * Enhances {@link PerTableTemplateContext} with the {@link }
+ * Enhances {@link PerTableTemplateContext} with the {@link TemplateDef} reference.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/05/27 06:06
