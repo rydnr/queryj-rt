60f3349915d659aec4c2c650a1c59862f36ae029
chous <chous@acm-sl.org>
2014-05-27 22:02:07 +0200

    201405272202

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerForeignKeyTemplateContext.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerForeignKeyTemplateContext.java
@@ -27,7 +27,7 @@
  *
  * Author: Jose San Leandro Armendariz
  *
- * Description: 
+ * Description: Enhances {@link PerForeignKeyTemplateContext} with the {@link TemplateDef} reference.
  *
  * Date: 2014/05/27
  * Time: 20:59
