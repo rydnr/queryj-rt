d39fb15f099db805fa43fe8f6eea37f95925b407
chous <chous@acm-sl.org>
2014-05-27 21:45:44 +0200

    201405272145

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultTemplateContext.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultTemplateContext.java
@@ -38,6 +38,7 @@ package org.acmsl.queryj.templates.packaging;
 /*
  * Importing JetBrains annotations.
  */
+import org.acmsl.queryj.api.PerCustomResultTemplateContext;
 import org.jetbrains.annotations.NotNull;
 
 /*
@@ -53,5 +54,6 @@ import org.checkthread.annotations.ThreadSafe;
  */
 @ThreadSafe
 public class TemplateDefPerCustomResultTemplateContext
+    extends PerCustomResultTemplateContext
 {
 }
