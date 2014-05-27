25f6f0e169ad1b179bea4a85d896e215253fec5b
chous <chous@acm-sl.org>
2014-05-27 07:34:19 +0200

    201405270734

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefImpl.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefImpl.java
@@ -50,6 +50,7 @@ import org.jetbrains.annotations.NotNull;
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
+import org.jetbrains.annotations.Nullable;
 
 /*
  * Importing JDK classes.
