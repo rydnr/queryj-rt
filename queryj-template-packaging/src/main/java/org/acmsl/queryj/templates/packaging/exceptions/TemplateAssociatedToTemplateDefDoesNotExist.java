d758268efabf769b71a8ead4e098e31f2e891246
chous <chous@acm-sl.org>
2014-05-27 07:39:27 +0200

    201405270739

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/TemplateAssociatedToTemplateDefDoesNotExist.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/TemplateAssociatedToTemplateDefDoesNotExist.java
@@ -98,7 +98,7 @@ public class TemplateAssociatedToTemplateDefDoesNotExist
         @NotNull final TemplateDef<String> templateDef)
     {
         super(
-            "template.associated.to.templatedef.does.not.exist",
+            "template.associated.to.classloadtemplatedef.does.not.exist",
             new Object[]
                 {
                     templateDef.getFile().getAbsolutePath()
