93c5b2f945dff7792eb7be0990a2f1268cbd94d4
chous <chous@acm-sl.org>
2014-05-27 07:26:34 +0200

    201405270726

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/ParseTemplateDefsHandler.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/ParseTemplateDefsHandler.java
@@ -426,7 +426,7 @@ public class ParseTemplateDefsHandler
      * @return the {@link TemplateDefParser} instance.
      */
     @NotNull
-    protected TemplateDefParser setUpParser(@NotNull final File file)
+    protected TemplateDefParser setUpParser(@NotNull final InputStream file)
         throws RecognitionException,
         IOException
     {
