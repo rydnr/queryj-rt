8528845dc6c37e775a8e065844f1e5ecce451878
chous <chous@acm-sl.org>
2014-05-27 21:54:30 +0200

    201405272154

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerRepositoryTemplateContext.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerRepositoryTemplateContext.java
@@ -50,7 +50,7 @@ import org.checkthread.annotations.ThreadSafe;
 import java.util.List;
 
 /**
- * Enhances {@link org.acmsl.queryj.api.PerCustomResultTemplateContext} with the {@link TemplateDef} reference.
+ * Enhances {@link PerCustomResultTemplateContext} with the {@link TemplateDef} reference.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/05/27 20:50
