1669813ed3e9c22a25f23ad7507cddcf0c7c66c8
chous <chous@acm-sl.org>
2014-05-27 07:30:00 +0200

    201405270730

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/CannotProcessTemplateDefException.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/exceptions/CannotProcessTemplateDefException.java
@@ -86,8 +86,7 @@ public class CannotProcessTemplateDefException
      * @param file the {@link org.acmsl.queryj.templates.packaging.TemplateDef} file.
      * @param cause the cause.
      */
-    public CannotProcessTemplateDefException(
-        @NotNull final File file, @NotNull final Throwable cause)
+    public CannotProcessTemplateDefException(@NotNull final Throwable cause)
     {
         super("cannot.process.templatedef.from.stream", new Object[] { file.getAbsolutePath() }, cause);
     }
