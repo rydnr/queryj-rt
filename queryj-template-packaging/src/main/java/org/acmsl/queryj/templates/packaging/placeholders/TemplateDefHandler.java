dfe870990e57bedd0a6fe30abe0db5050a3a0d50
chous <chous@acm-sl.org>
2014-05-31 09:47:37 +0200

    201405310947

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/placeholders/TemplateDefHandler.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/placeholders/TemplateDefHandler.java
@@ -106,6 +106,6 @@ public class TemplateDefHandler
         {
             result = new DecoratedTemplateDefWrapper((DefaultTemplatePackagingContext context).getTemplateDef());
         }
-        else if ()
+        else if (context instanceof )
     }
 }
