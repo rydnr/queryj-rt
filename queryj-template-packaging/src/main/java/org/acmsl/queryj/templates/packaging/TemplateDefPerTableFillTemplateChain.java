d0673f1577abc1c457c7a0e0e666fce7f06ebee8
chous <chous@acm-sl.org>
2014-05-31 09:40:51 +0200

    201405310940

---
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerTableFillTemplateChain.java
@@ -0,0 +1,64 @@
+/*
+                        queryj
+
+    Copyright (C) 2002-today  Jose San Leandro Armendariz
+                              chous@acm-sl.org
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: jose.sanleandro@acm-sl.com
+
+ ******************************************************************************
+ *
+ * Filename: TemplateDefPerTableFillTemplateChain.java
+ *
+ * Author: Jose San Leandro Armendariz
+ *
+ * Description: 
+ *
+ * Date: 2014/05/31
+ * Time: 08:39
+ *
+ */
+package org.acmsl.queryj.templates.packaging;
+
+/*
+ * Importing JetBrains annotations.
+ */
+import org.acmsl.queryj.api.FillTemplateChain;
+import org.acmsl.queryj.api.PerTableTemplateContext;
+import org.acmsl.queryj.api.TemplateContext;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * Importing checkthread.org annotations.
+ */
+import org.checkthread.annotations.ThreadSafe;
+
+/**
+ * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
+ * @since 3.0
+ *        Created: 2014/05/31 08:39
+ */
+@ThreadSafe
+public class TemplateDefPerTableFillTemplateChain
+    implements FillTemplateChain<TemplateDefPerTableTemplateContext>
+{
+    public TemplateDefPerTableFillTemplateChain(final TemplateDefPerTableTemplateContext context)
+    {
+        super(context);
+    }
+}
