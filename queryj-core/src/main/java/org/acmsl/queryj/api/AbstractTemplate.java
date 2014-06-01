cc8f1f8c58c389bab9f6469870e63ea6a1dc6196
chous <chous@acm-sl.org>
2014-06-01 19:54:15 +0200

    201406011954

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplate.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplate.java
@@ -627,7 +627,7 @@ public abstract class AbstractTemplate<C extends TemplateContext>
         public String toString()
         {
             return
-                  "{ \"new\": " + m__bNew + "\"\"class\": \"" + FinalizingThread.class.getName() + "\""
+                  "{ \"new\": " + m__bNew + \"\"class\": \"" + FinalizingThread.class.getName() + "\""
                 + ",  }";
         }
     }
