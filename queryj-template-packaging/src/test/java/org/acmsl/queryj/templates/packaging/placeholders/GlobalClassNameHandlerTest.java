9cbab75621096a4e3e4cf49a50b5015d3d3dd656
chous <chous@acm-sl.org>
2014-06-02 08:06:04 +0200

    201406020806

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/placeholders/GlobalClassNameHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/placeholders/GlobalClassNameHandlerTest.java
@@ -83,7 +83,7 @@ public class GlobalClassNameHandlerTest
             new GlobalClassNameHandler(context)
             {
                 @Override
-                public DecoratedString getValue(@NotNull final GlobalTemplateContext )
+                public DecoratedString getValue(@NotNull final GlobalTemplateContext ctx)
             }
         ];
 
