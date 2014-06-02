3bb1f909decdca30b6797473087555b102fd4e32
chous <chous@acm-sl.org>
2014-06-03 01:38:12 +0200

    201406030138

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/placeholders/PerTemplateDefClassNameHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/placeholders/PerTemplateDefClassNameHandlerTest.java
@@ -105,6 +105,6 @@ public class PerTemplateDefClassNameHandlerTest
         @NotNull final PerTemplateDefClassNameHandler instance =
             new PerTemplateDefClassNameHandler(context);
 
-        Assert.assertEquals();
+        Assert.assertEquals("DefNameTemplateName");
     }
 }
