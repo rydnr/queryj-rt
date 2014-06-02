4a5530254018bf7f3b87b331c1f7047b5a18557f
chous <chous@acm-sl.org>
2014-06-02 08:05:01 +0200

    201406020805

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/placeholders/GlobalClassNameHandlerTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/placeholders/GlobalClassNameHandlerTest.java
@@ -98,7 +98,7 @@ public class GlobalClassNameHandlerTest
                 false,
                 false);
 
-        Assert.assertEquals(new DecoratedString("DefFileNameTemplateName.java", instance.getValue(context));
+        Assert.assertEquals(new DecoratedString("DefFileNameTemplateName.java"), instance.getValue(context));
 
     }
 }
