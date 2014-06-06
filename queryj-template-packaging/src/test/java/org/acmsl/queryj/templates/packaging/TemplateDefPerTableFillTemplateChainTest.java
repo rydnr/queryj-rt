9df264987a546bb086b876af9a6074af237de232
chous <chous@acm-sl.org>
2014-06-06 07:05:33 +0200

    201406060705

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefPerTableFillTemplateChainTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefPerTableFillTemplateChainTest.java
@@ -58,5 +58,5 @@ import org.junit.runners.JUnit4;
 public class TemplateDefPerTableFillTemplateChainTest
 {
     @Test
-    public void
+    public void getHandlers_include_templateDef_placeholder
 }
