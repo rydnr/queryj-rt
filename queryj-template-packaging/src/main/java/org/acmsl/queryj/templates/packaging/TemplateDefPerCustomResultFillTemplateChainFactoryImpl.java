14220becbece904a4f78fcc374aa56be9b1addaa
chous <chous@acm-sl.org>
2014-06-06 21:45:20 +0200

    201406062145

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultFillTemplateChainFactoryImpl.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultFillTemplateChainFactoryImpl.java
@@ -42,7 +42,7 @@ package org.acmsl.queryj.templates.packaging;
 import org.acmsl.queryj.api.FillTemplateChain;
 
 /*
- * Importing JetBrains annotations.
+ * Importing QueryJ Placeholders.
  */
 import org.acmsl.queryj.placeholders.FillTemplateChainWrapper;
 import org.acmsl.queryj.placeholders.PerCustomResultFillTemplateChainFactoryImpl;
