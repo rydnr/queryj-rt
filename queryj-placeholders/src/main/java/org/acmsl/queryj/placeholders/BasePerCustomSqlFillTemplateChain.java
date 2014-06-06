fc766500133eafad20ed9859d93fa3c014c7927b
chous <chous@acm-sl.org>
2014-06-06 19:08:52 +0200

    201406061908

--- queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/BasePerCustomSqlFillTemplateChain.java
+++ queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/BasePerCustomSqlFillTemplateChain.java
@@ -64,6 +64,8 @@ import java.util.List;
 /**
  * Sets up the chain required to provide placeholder replacements for
  * {@link org.acmsl.queryj.api.PerCustomSqlTemplate per-custom-sql templates}.
+ * @param <C
+ *
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2012/07/07
