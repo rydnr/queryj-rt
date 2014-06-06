209fc06800e00da848172e0644f1b16a9aaba4c9
chous <chous@acm-sl.org>
2014-06-06 18:47:32 +0200

    201406061847

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefPerRepositoryFillTemplateChainTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefPerRepositoryFillTemplateChainTest.java
@@ -57,7 +57,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class TemplateDefPerRepositoryFillTemplateChainTest
     extends AbstractTemplateDefFillTemplateChainTest<
-    TemplateDefPerRepositoryTemplateContext, TemplateDefPerRepositoryFillTemplateChain>
+                TemplateDefPerRepositoryTemplateContext, TemplateDefPerRepositoryFillTemplateChain>
 {
     /**
      * Checks getHandlers() include the handler to resolve
