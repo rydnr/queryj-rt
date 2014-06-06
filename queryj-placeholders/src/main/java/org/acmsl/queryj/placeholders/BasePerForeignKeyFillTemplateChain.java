8a38110e4b7a2e990d97fb788a52a26cd15b9897
chous <chous@acm-sl.org>
2014-06-06 19:14:53 +0200

    201406061914

--- queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/BasePerForeignKeyFillTemplateChain.java
+++ queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/BasePerForeignKeyFillTemplateChain.java
@@ -64,7 +64,7 @@ import org.checkthread.annotations.ThreadSafe;
 
 /**
  * Sets up the chain required to provide placeholder replacements for
- * {@link org.acmsl.queryj.api.PerForeignKeyTemplate per-foreign-key templates}.
+ * {@link PerForeignKeyTemplate per-foreign-key templates}.
  * @param <C> the template context.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
