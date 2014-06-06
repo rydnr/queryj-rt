f4a91bdf2a974b441c17c4640d4aa2130e7a22f3
chous <chous@acm-sl.org>
2014-06-06 19:25:23 +0200

    201406061925

--- queryj-core/src/main/java/org/acmsl/queryj/api/placeholders/PerRepositoryFillTemplateChainFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/placeholders/PerRepositoryFillTemplateChainFactory.java
@@ -44,6 +44,7 @@ import org.acmsl.queryj.api.PerRepositoryTemplateContext;
 /**
  * JDK 6 Services'-compatible per-repository {@link org.acmsl.queryj.api.placeholders.FillTemplateChainFactory}
  * implementation.
+ * @param <C> the template context.
  * @author <a href="mailto:chous@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2013/06/08
