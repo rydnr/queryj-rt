9d5f12d7d819d5b7a695557393caeadc3373442c
chous <chous@acm-sl.org>
2014-06-06 08:08:15 +0200

    201406060808

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultFillTemplateChainTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultFillTemplateChainTest.java
@@ -62,23 +62,23 @@ import java.util.List;
  */
 @RunWith(JUnit4.class)
 public abstract class TemplateDefPerCustomResultFillTemplateChainTest<C, CH>
-{
     extends AbstractTemplateDefFillTemplateChainTest<
     TemplateDefPerTableTemplateContext, TemplateDefPerTableFillTemplateChain>
+{
+    /**
+     * Checks getHandlers() include the handler to resolve
+     * "templateDef" placeholder.
+     */
+    @Test
+    public void getHandlers_include_templateDef_placeholder()
     {
-        /**
-         * Checks getHandlers() include the handler to resolve
-         * "templateDef" placeholder.
-         */
-        @Test
-        public void getHandlers_include_templateDef_placeholder()
-        {
-            @NotNull final TemplateDefPerTableTemplateContext context =
-                EasyMock.createNiceMock(TemplateDefPerTableTemplateContext.class);
+        @NotNull final TemplateDefPerTableTemplateContext context =
+            EasyMock.createNiceMock(TemplateDefPerTableTemplateContext.class);
 
-            @NotNull final TemplateDefPerTableFillTemplateChain instance =
-                new TemplateDefPerTableFillTemplateChain(context);
+        @NotNull final TemplateDefPerTableFillTemplateChain instance =
+            new TemplateDefPerTableFillTemplateChain(context);
 
-            testGetHandlers(instance);
-        }
+        testGetHandlers(instance);
+    }
+{
 }
