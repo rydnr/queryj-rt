87f3860225c6b07c98495bf43a0b0b991334a3dd
chous <chous@acm-sl.org>
2014-06-06 18:50:02 +0200

    201406061850

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerRepositoryFillTemplateChain.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerRepositoryFillTemplateChain.java
@@ -66,6 +66,9 @@ import org.jetbrains.annotations.NotNull;
  */
 import org.checkthread.annotations.ThreadSafe;
 
+/*
+ * Importing JDK classes.
+ */
 import java.util.ArrayList;
 import java.util.List;
 
