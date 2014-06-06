9433fdccc2c875819c54d6dd3c9be07074308afa
chous <chous@acm-sl.org>
2014-06-06 18:32:52 +0200

    201406061832

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultFillTemplateChain.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultFillTemplateChain.java
@@ -1,5 +1,5 @@
 /*
-                        QueryJ
+                        QueryJ Template Packaging
 
     Copyright (C) 2002-today  Jose San Leandro Armendariz
                               chous@acm-sl.org
