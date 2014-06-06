5933ed639fd56d11a8155fb498c25a398a3b5477
chous <chous@acm-sl.org>
2014-06-06 19:07:59 +0200

    201406061907

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomSqlFillTemplateChain.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomSqlFillTemplateChain.java
@@ -67,7 +67,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /*
- * Importing JetBrains annotations.
+ * Importing JDK classes.
  */
 import java.util.ArrayList;
 import java.util.List;
