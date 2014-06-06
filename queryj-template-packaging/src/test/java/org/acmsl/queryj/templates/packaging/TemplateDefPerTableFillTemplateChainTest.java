53d3bdc4921c347e45327360e737162f4828e417
chous <chous@acm-sl.org>
2014-06-06 07:12:52 +0200

    201406060712

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefPerTableFillTemplateChainTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefPerTableFillTemplateChainTest.java
@@ -36,7 +36,7 @@
 package org.acmsl.queryj.templates.packaging;
 
 /*
- * Importing JetBrains annotations.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.api.handlers.TemplateContextFillAdapterHandler;
 
