20f2b09bad507f22036d5d1dac761446347c4f8f
chous <chous@acm-sl.org>
2014-06-06 08:11:41 +0200

    201406060811

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultFillTemplateChain.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultFillTemplateChain.java
@@ -59,7 +59,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Builds the list of fill handlers to extend {@link BasePerTableFillTemplateChain}'s
+ * Builds the list of fill handlers to extend {@link BasePerCustomResultFillTemplateChain}'s
  * with {@link TemplateDef}-related ones.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
