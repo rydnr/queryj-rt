6385c8350f82dbd6cecaac40c96af7593925b198
chous <chous@acm-sl.org>
2014-06-06 19:04:44 +0200

    201406061904

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomSqlFillTemplateChainTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomSqlFillTemplateChainTest.java
@@ -50,7 +50,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- * Tests for {@link TemplateDefPerCustomResultFillTemplateChain}
+ * Tests for {@link TemplateDefPerCustomResultFillTemplateChain}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/06/06 18:03
