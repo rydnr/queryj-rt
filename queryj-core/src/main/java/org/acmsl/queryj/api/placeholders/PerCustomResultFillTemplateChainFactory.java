05af1dc61ccf896bb8a3630e6e7c513a74786004
chous <chous@acm-sl.org>
2014-06-06 21:19:45 +0200

    201406062119

--- queryj-core/src/main/java/org/acmsl/queryj/api/placeholders/PerCustomResultFillTemplateChainFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/placeholders/PerCustomResultFillTemplateChainFactory.java
@@ -50,7 +50,7 @@ import org.checkthread.annotations.ThreadSafe;
  * JDK 6 Services'-compatible per-custom-result {@link FillTemplateChainFactory}
  * implementation.
  * @param <C> the template context.
- * @author <a href="mailto:chous@acm-sl.org">Jose San Leandro</a>
+ * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2013/06/08
  */
