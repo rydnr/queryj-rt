0edc2a0f4383b30250b6e8332e98c8ee4aeaba55
chous <chous@acm-sl.org>
2014-06-06 21:47:15 +0200

    201406062147

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomSqlFillTemplateChainFactoryImpl.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomSqlFillTemplateChainFactoryImpl.java
@@ -27,7 +27,8 @@
  *
  * Author: Jose San Leandro Armendariz
  *
- * Description: 
+ * Description: Creates the chain to provide all per-custom-result
+ *              placeholders, with access to the TemplateDef information.
  *
  * Date: 2014/06/06
  * Time: 20:45
