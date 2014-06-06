7d0ad068a937b1d301f1e5e69f5982dbc2bc6cd0
chous <chous@acm-sl.org>
2014-06-06 08:05:18 +0200

    201406060805

--- queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/AbstractTemplateDefFillTemplateChainTest.java
+++ queryj-template-packaging/src/test/java/org/acmsl/queryj/templates/packaging/AbstractTemplateDefFillTemplateChainTest.java
@@ -54,7 +54,7 @@ import org.junit.Test;
 import java.util.List;
 
 /**
- *
+ * Common logic for
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/06/06 06:59
