1d1e7cdb94c10ed12001dbc696132f649be12f16
chous <chous@acm-sl.org>
2014-06-06 21:43:35 +0200

    201406062143

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultFillTemplateChainFactoryImpl.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/TemplateDefPerCustomResultFillTemplateChainFactoryImpl.java
@@ -71,7 +71,7 @@ public class TemplateDefPerCustomResultFillTemplateChainFactoryImpl
     {
         return
             new FillTemplateChainWrapper<>(
-                new TemplateDefPerRepositoryFillTemplateChain(context));
+                new TemplateDefPerCustomResultFillTemplateChain(context));
     }
 }
 {
