0dc20dcb1aabf752f51fc8a1b25764eabf965d17
chous <chous@acm-sl.org>
2014-06-08 21:11:47 +0200

    201406082111

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableAttributesListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableAttributesListDecorator.java
@@ -290,7 +290,7 @@ public class TableAttributesListDecorator
     @Override
     public List<Attribute<DecoratedString>> getNullableAttributes()
     {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        throw new RuntimeException(INVALID_OPERATION);
     }
 
     /**
