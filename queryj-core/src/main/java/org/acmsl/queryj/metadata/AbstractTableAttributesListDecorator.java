b497a114bfc61b302c5d8bfa1194174e6b172e46
chous <chous@acm-sl.org>
2014-06-08 21:43:03 +0200

    201406082143

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecorator.java
@@ -507,7 +507,7 @@ public abstract class AbstractTableAttributesListDecorator
     @Override
     public Result<DecoratedString> getCustomResult()
     {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return getTable().getCustomResult();
     }
 
     /**
