1c9ddf0b87ba78357fe8876ff5b5e752bce4ded1
chous <chous@acm-sl.org>
2014-06-08 21:11:16 +0200

    201406082111

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableAttributesListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableAttributesListDecorator.java
@@ -194,7 +194,7 @@ public class TableAttributesListDecorator
     @Override
     public boolean getContainsNotNullAttributes()
     {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
+        throw new RuntimeException(INVALID_OPERATION);
     }
 
     /**
