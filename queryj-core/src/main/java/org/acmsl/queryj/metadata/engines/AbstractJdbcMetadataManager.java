c373fe6c692a60d876ae5f900eb63495c04cea53
chous <chous@acm-sl.org>
2014-06-13 20:51:33 +0200

    201406132051

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/AbstractJdbcMetadataManager.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/AbstractJdbcMetadataManager.java
@@ -1835,7 +1835,9 @@ public abstract class AbstractJdbcMetadataManager
     @NotNull
     public Attribute<String> cloneAttribute(@NotNull final Attribute<String> attribute)
     {
-        @Nullable
+        @Nullable final String t_strComment = attribute.getComment();
+
+
     }
 
     /**
