2dfeead653b3a7151a809e7f9e9762243fdfbc4d
chous <chous@acm-sl.org>
2014-06-13 20:47:28 +0200

    201406132047

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/AbstractJdbcMetadataManagerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/AbstractJdbcMetadataManagerTest.java
@@ -102,6 +102,7 @@ public class AbstractJdbcMetadataManagerTest
                 "The pk of the table. @oraseq MYSEQ",
                 1,
                 10,
+                false,
             )
     }
 }
