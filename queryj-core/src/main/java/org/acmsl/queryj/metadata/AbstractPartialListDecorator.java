340840c4e4969b6257d719cffa5b1b82becb82d5
chous <chous@acm-sl.org>
2014-06-14 18:55:33 +0200

    201406141855

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractPartialListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractPartialListDecorator.java
@@ -824,7 +824,7 @@ public class AbstractPartialListDecorator
     public String toString()
     {
         return
-            "{  \"listDecorator\": " + m__ListDecorator
+            "{ \"listDecorator\": " + m__ListDecorator
             + ", \"operation\": " + m__Operation
             + " \"class\": \"" + AbstractPartialListDecorator.class.getSimpleName() + '"'
             + ", \"package\": \"org.acmsl.queryj.metadata\" }";
