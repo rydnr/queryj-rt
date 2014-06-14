8868050e7a2c2e9722d251365d792948f141b838
chous <chous@acm-sl.org>
2014-06-14 19:54:21 +0200

    201406141954

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
@@ -1914,7 +1914,7 @@ public abstract class AbstractTableDecorator
 
         Collections.sort(result);
 
-        return new TableCustomResultsLisresult;
+        return new TableCustomResultsListDecorator(result;
     }
 
     /**
