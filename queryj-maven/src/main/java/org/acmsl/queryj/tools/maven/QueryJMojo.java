36804e754d1d523d51d21f85f3e7159b3fa382cc
chous <chous@acm-sl.org>
2014-06-14 09:11:23 +0200

    201406140911

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
@@ -1502,7 +1502,7 @@ public class QueryJMojo
                                         @Nullable final String t_strPk = field.getPk();
 
                                         fieldElement.setDynamicAttribute(
-                                            "pk", t_strPk
+                                            "pk", t_strPk != null ?
                                     }
                                 }
                             }
