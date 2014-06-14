f54775f984653223e531c8d04578714d9a42ba18
chous <chous@acm-sl.org>
2014-06-14 19:57:53 +0200

    201406141957

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableCustomResultsListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableCustomResultsListDecorator.java
@@ -38,12 +38,19 @@ package org.acmsl.queryj.metadata;
 /*
  * Importing JetBrains annotations.
  */
+import org.acmsl.queryj.customsql.CustomSqlProvider;
+import org.acmsl.queryj.metadata.vo.Attribute;
+import org.acmsl.queryj.metadata.vo.ForeignKey;
+import org.acmsl.queryj.metadata.vo.Table;
 import org.jetbrains.annotations.NotNull;
 
 /*
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 /**
  *
@@ -55,5 +62,37 @@ import org.checkthread.annotations.ThreadSafe;
 public class TableCustomResultsListDecorator
     extends AbstractTableDecorator
 {
-
+    /**
+     * Creates an {@code AbstractTableDecorator} with the following
+     * information.
+     *
+     * @param table             the {@link org.acmsl.queryj.metadata.vo.Table}.
+     * @param name              the name.
+     * @param primaryKey        the primary key.
+     * @param attributes        the attributes.
+     * @param foreignKeys       the foreign keys.
+     * @param parentTable       the parent table.
+     * @param staticAttribute   the attribute used to label static contents.
+     * @param voDecorated       whether the value-object should be decorated.
+     * @param isRelationship    whether the table identifies a relationship.
+     * @param metadataManager   the {@link org.acmsl.queryj.metadata.MetadataManager metadata manager}.
+     * @param decoratorFactory  the {@link org.acmsl.queryj.metadata.DecoratorFactory decorator factory}.
+     * @param customSqlProvider the {@link org.acmsl.queryj.customsql.CustomSqlProvider custom-sql provider}.
+     */
+    public TableCustomResultsListDecorator(@NotNull final Table<String, Attribute<String>, List<Attribute<String>>>
+                                               table, @NotNull final String name, @NotNull final List<Attribute<String>> primaryKey, @NotNull final List<Attribute<String>> attributes, @NotNull final List<ForeignKey<String>> foreignKeys, @Nullable final Table<String, Attribute<String>, List<Attribute<String>>> parentTable, @Nullable final Attribute<String> staticAttribute, final boolean voDecorated, final boolean isRelationship, @NotNull final MetadataManager metadataManager, @NotNull final DecoratorFactory decoratorFactory, @NotNull final CustomSqlProvider customSqlProvider)
+    {
+        super(table,
+              name,
+              primaryKey,
+              attributes,
+              foreignKeys,
+              parentTable,
+              staticAttribute,
+              voDecorated,
+              isRelationship,
+              metadataManager,
+              decoratorFactory,
+              customSqlProvider);
+    }
 }
