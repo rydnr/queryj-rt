71b40759737877327ee8768ed47f4b2b05b599f3
chous <chous@acm-sl.org>
2014-06-14 18:57:38 +0200

    201406141857

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractPartialListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractPartialListDecorator.java
@@ -62,7 +62,7 @@ import java.util.List;
  * Created: 2014/06/14 17:54
  */
 @ThreadSafe
-public abstract class AbstractPartialListDecorator
+public abstract class AbstractPartialListDecorator<>
     implements PartialListDecorator
 {
     /**
