2c3e944ff3b9e26540763e8bfccf6b7929760b9d
chous <chous@acm-sl.org>
2014-06-14 19:53:25 +0200

    201406141953

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableDecorator.java
@@ -108,7 +108,7 @@ public interface TableDecorator
      * @return such list.
      */
     @NotNull
-    List<Result<DecoratedString>> getDifferentCustomResults();
+    List<Result<DecoratedString>> getCustomResults();
 
     /**
      * Retrieves the ordered list of the fully-qualified attribute types.
