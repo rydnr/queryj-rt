bd4562b039f1776c6561ce276b29521ac0ee1ce9
chous <chous@acm-sl.org>
2014-06-14 20:19:23 +0200

    201406142019

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/TablePartialListDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/TablePartialListDecoratorTest.java
@@ -83,6 +83,8 @@ public class TablePartialListDecoratorTest
                 1,
                 null,
                 null,
+                null,
+                false,
             )
         list.add()
         @NotNull final ListDecorator<Attribute<DecoratedString>> listDecorator =
