353c182a9c71963900eece749656e385f9aec961
chous <chous@acm-sl.org>
2014-06-14 19:49:53 +0200

    201406141949

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableAttributesListDecorator.java
@@ -41,7 +41,6 @@ package org.acmsl.queryj.metadata;
 import org.acmsl.queryj.customsql.Result;
 import org.acmsl.queryj.customsql.Sql;
 import org.acmsl.queryj.metadata.AbstractPartialListDecorator.Operation;
-import org.acmsl.queryj.metadata.TableAttributesPartialListDecorator.Operation;
 import org.acmsl.queryj.metadata.vo.Attribute;
 import org.acmsl.queryj.metadata.vo.ForeignKey;
 import org.acmsl.queryj.metadata.vo.Row;
