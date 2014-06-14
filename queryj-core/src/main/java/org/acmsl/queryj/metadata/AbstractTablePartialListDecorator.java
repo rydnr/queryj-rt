b289bfbe1c0a9919e06545d9b98aeeb6a42c75a3
chous <chous@acm-sl.org>
2014-06-14 19:46:31 +0200

    201406141946

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTablePartialListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTablePartialListDecorator.java
@@ -48,7 +48,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- *
+ * Base class for partial list decorators,
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/06/14 18:42
