f709cd3df06f498f42b92a055c1d14f1795cf873
chous <chous@acm-sl.org>
2014-06-15 20:35:03 +0200

    201406152035

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImpl.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImpl.java
@@ -160,6 +160,7 @@ public class TableResultDecoratorImpl
             result = false;
         }
         else
+        {
         TableResultDecoratorImpl that = (TableResultDecoratorImpl) o;
 
         if (!m__Result.equals(that.m__Result))
