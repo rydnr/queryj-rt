620aa6600e880a2def9697e66ec78672544eec9f
chous <chous@acm-sl.org>
2014-06-15 10:53:48 +0200

    201406151053

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractListDecorator.java
@@ -332,66 +332,6 @@ public abstract class AbstractListDecorator<T>
     }
 
     /**
-     * Checks whether given list is composed of {@link org.acmsl.queryj.metadata.vo.Attribute attributes} or not.
-     * @param list such list.
-     * @return {@code true} in such case.
-     */
-    protected boolean isListOfAttributes(@NotNull final List<T> list)
-    {
-        final boolean result;
-
-        if (list.size() > 0)
-        {
-            @Nullable final T item = list.get(0);
-
-            if (item instanceof Attribute)
-            {
-                result = true;
-            }
-            else
-            {
-                result = false;
-            }
-        }
-        else
-        {
-            result = true;
-        }
-
-        return result;
-    }
-
-    /**
-     * Checks whether given list is composed of {@link Attribute attributes} or not.
-     * @param list such list.
-     * @return {@code true} in such case.
-     */
-    protected boolean isListOfCustomResults(@NotNull final List<T> list)
-    {
-        final boolean result;
-
-        if (list.size() > 0)
-        {
-            @Nullable final T item = list.get(0);
-
-            if (item instanceof Result)
-            {
-                result = true;
-            }
-            else
-            {
-                result = false;
-            }
-        }
-        else
-        {
-            result = true;
-        }
-
-        return result;
-    }
-
-    /**
      * Writes a textual representation of the instance.
      * @return such text.
      */
