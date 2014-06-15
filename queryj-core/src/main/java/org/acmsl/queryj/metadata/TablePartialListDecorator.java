23a478de1c676b5dee56580a14afc1a07cc7a8ce
chous <chous@acm-sl.org>
2014-06-15 18:26:16 +0200

    201406151826

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TablePartialListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TablePartialListDecorator.java
@@ -761,6 +761,10 @@ public class TablePartialListDecorator<V>
         {
             result = (ListDecorator<V>) new TableCustomResultsListDecorator((List<Result<DecoratedString>>) aux, table);
         }
+        else
+        {
+            throw new RuntimeException()
+        }
         return
     }
 }
