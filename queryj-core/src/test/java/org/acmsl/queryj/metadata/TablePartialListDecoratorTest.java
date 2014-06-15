9121147611c3cabf4458450de33d3925e54a90cd
chous <chous@acm-sl.org>
2014-06-15 16:03:52 +0200

    201406151603

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/TablePartialListDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/TablePartialListDecoratorTest.java
@@ -36,7 +36,7 @@
 package org.acmsl.queryj.metadata;
 
 /*
- * Importing QueryJ Core classes
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.metadata.AbstractPartialListDecorator.Operation;
 import org.acmsl.queryj.metadata.vo.AbstractAttribute;
