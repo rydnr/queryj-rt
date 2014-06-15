d6c1dc009e6599360f4c32ffd0f3a3b8b7d18225
chous <chous@acm-sl.org>
2014-06-15 20:27:27 +0200

    201406152027

---
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImpl.java
@@ -0,0 +1,63 @@
+/*
+                        queryj
+
+    Copyright (C) 2002-today  Jose San Leandro Armendariz
+                              chous@acm-sl.org
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: jose.sanleandro@acm-sl.com
+
+ ******************************************************************************
+ *
+ * Filename: TableResultDecoratorImpl.java
+ *
+ * Author: Jose San Leandro Armendariz
+ *
+ * Description: 
+ *
+ * Date: 2014/06/15
+ * Time: 19:27
+ *
+ */
+package org.acmsl.queryj.metadata.impl;
+
+/*
+ * Importing JetBrains annotations.
+ */
+import org.acmsl.queryj.customsql.Result;
+import org.acmsl.queryj.metadata.DecoratedString;
+import org.acmsl.queryj.metadata.TableDecorator;
+import org.acmsl.queryj.metadata.TableResultDecorator;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * Importing checkthread.org annotations.
+ */
+import org.checkthread.annotations.ThreadSafe;
+
+/**
+ * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
+ * @since 3.0
+ *        Created: 2014/06/15 19:27
+ */
+@ThreadSafe
+public class TableResultDecoratorImpl implements TableResultDecorator
+{
+    public TableResultDecoratorImpl(final Result<DecoratedString> item, final TableDecorator table)
+    {
+    }
+}
