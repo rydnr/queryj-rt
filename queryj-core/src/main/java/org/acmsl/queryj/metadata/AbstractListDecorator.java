526cda4c7b2944ef017dca117cc5ec0ff783bea8
chous <chous@acm-sl.org>
2014-06-15 12:04:40 +0200

    201406151204

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractListDecorator.java
@@ -388,7 +388,7 @@ public abstract class AbstractListDecorator<T>
      */
     @SuppressWarnings("unchecked")
     @NotNull
-    protected List<Result<DecoratedString>> retrieveAttributes(
+    protected List<Result<DecoratedString>> retrieveCustomResults(
         @NotNull final List<T> items, @NotNull final TableDecoratorHelper tableDecoratorHelper)
     {
         @NotNull final List<Attribute<DecoratedString>> result;
