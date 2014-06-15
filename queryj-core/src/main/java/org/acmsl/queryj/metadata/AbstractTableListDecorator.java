4937bacd39731381f452bc9eb32d25dfa2a489d2
chous <chous@acm-sl.org>
2014-06-15 10:58:42 +0200

    201406151058

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableListDecorator.java
@@ -285,7 +285,7 @@ public abstract class AbstractTableListDecorator<V>
 
         if (tableDecoratorHelper.isListOfAttributes(items))
         {
-            result = tableDecoratorHelper.getAttributeTypes(items, metadataTypeManager);
+            result = tableDecoratorHelper.getAttributeTypes((items, metadataTypeManager);
         }
         else
         {
