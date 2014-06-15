cc93e4f2f94ce1bc2b1ff12dd2e669b3c118e6dc
chous <chous@acm-sl.org>
2014-06-15 18:19:31 +0200

    201406151819

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TablePartialListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TablePartialListDecorator.java
@@ -724,6 +724,6 @@ public class TablePartialListDecorator<V>
     @NotNull
     public ListDecorator<V> getDifferent()
     {
-        @NotNull final ListDecorator<Attribute<DecoratedString>> items = getItems();
+        @NotNull final ListDecorator<V> items = getItems();
     }
 }
