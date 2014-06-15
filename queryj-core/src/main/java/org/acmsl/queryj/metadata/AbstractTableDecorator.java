92afcbb7ff76bd8b6bbd3d0cff80c1f8ae71ac2e
chous <chous@acm-sl.org>
2014-06-15 11:09:06 +0200

    201406151109

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
@@ -1615,7 +1615,7 @@ public abstract class AbstractTableDecorator
 
     /**
      * Decorates given {@link Result results}.
-     * @param tableDecorator the {@link TableDecorator}
+     * @param tableDecorator the {@link TableDecorator table}.
      * @param results the results to decorate.
      * @param customSqlProvider the {@link CustomSqlProvider} instance.
      * @param metadataManager the {@link MetadataManager} instance.
