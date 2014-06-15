4c85372c6178f783a5cccbe699cf625d1480f9f7
chous <chous@acm-sl.org>
2014-06-15 20:24:36 +0200

    201406152024

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableCustomResultsListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableCustomResultsListDecorator.java
@@ -106,7 +106,7 @@ public class TableCustomResultsListDecorator
         {
             if (item != null)
             {
-                result.add()
+                result.add(decorate(item))
             }
         }
         return result;
