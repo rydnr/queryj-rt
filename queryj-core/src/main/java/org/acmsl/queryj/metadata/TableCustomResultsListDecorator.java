0454fc03be9fcfe041b863f089b277536b593c44
chous <chous@acm-sl.org>
2014-06-15 12:02:10 +0200

    201406151202

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableCustomResultsListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableCustomResultsListDecorator.java
@@ -82,5 +82,5 @@ public class TableCustomResultsListDecorator
      * @return such results, or an empty list if the items are of a different kind.
      */
     @NotNull
-    protected
+    protected List<Result<DecoratedString>>
 }
