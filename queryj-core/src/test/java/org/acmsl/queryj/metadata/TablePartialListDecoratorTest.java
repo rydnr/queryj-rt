8b95a9acf01f3c1f8c64026de05bd8b548eb677f
chous <chous@acm-sl.org>
2014-06-15 15:57:33 +0200

    201406151557

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/TablePartialListDecoratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/TablePartialListDecoratorTest.java
@@ -47,6 +47,7 @@ import org.jetbrains.annotations.NotNull;
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -68,6 +69,141 @@ public class TablePartialListDecoratorTest
     public void getDifferent_removes_duplicates()
     {
         @NotNull final List<Attribute<DecoratedString>> list = new ArrayList<>(3);
+
+        @NotNull final Attribute<DecoratedString> attribute1 =
+            new Attribute<DecoratedString>()
+            {
+                @NotNull
+                @Override
+                public DecoratedString getName()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Override
+                public int getTypeId()
+                {
+                    return 0;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @NotNull
+                @Override
+                public DecoratedString getType()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @NotNull
+                @Override
+                public DecoratedString getTableName()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Nullable
+                @Override
+                public DecoratedString getComment()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Override
+                public boolean isExternallyManaged()
+                {
+                    return false;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Nullable
+                @Override
+                public DecoratedString getKeyword()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Nullable
+                @Override
+                public DecoratedString getRetrievalQuery()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Override
+                public boolean isNullable()
+                {
+                    return false;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Nullable
+                @Override
+                public DecoratedString getValue()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Override
+                public boolean isReadOnly()
+                {
+                    return false;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Override
+                public boolean isBoolean()
+                {
+                    return false;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Nullable
+                @Override
+                public DecoratedString getBooleanTrue()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Nullable
+                @Override
+                public DecoratedString getBooleanFalse()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Nullable
+                @Override
+                public DecoratedString getBooleanNull()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Override
+                public int getOrdinalPosition()
+                {
+                    return 0;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Override
+                public int getLength()
+                {
+                    return 0;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Override
+                public int getPrecision()
+                {
+                    return 0;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Nullable
+                @Override
+                public DecoratedString getSequence()
+                {
+                    return null;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                @Override
+                public int compareTo(final Attribute<DecoratedString> o)
+                {
+                    return 0;  //To change body of implemented methods use File | Settings | File Templates.
+                }
+            }
         @NotNull final TableDecorator tableDecorator = EasyMock.createNiceMock(TableDecorator.class);
 
         @NotNull final ListDecorator<Attribute<DecoratedString>> listDecorator =
