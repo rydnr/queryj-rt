210e4f96b028efb3237dada2772ae6c8db992803
chous <chous@acm-sl.org>
2014-06-15 20:18:33 +0200

    201406152018

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableResultDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableResultDecorator.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Per-table {@link ResultDecorator}.
+ * Per-table {@link ResultDecorator}s.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/06/15 19:17
