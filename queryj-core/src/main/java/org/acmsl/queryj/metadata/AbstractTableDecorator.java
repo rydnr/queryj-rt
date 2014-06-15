64d33a8e703b9f4cd6ea6c55dcbbd8e0645c57fa
chous <chous@acm-sl.org>
2014-06-15 11:17:39 +0200

    201406151117

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
@@ -1867,7 +1867,7 @@ public abstract class AbstractTableDecorator
         @NotNull final DecoratorFactory decoratorFactory,
         @NotNull final CustomSqlProvider customSqlProvider)
     {
-        @NotNull final TableDecorator result =
+        @Nullable final TableDecorator result =
             decoratorFactory.createTableDecorator(table.getName(), metadataManager, customSqlProvider);
 
         if (result == null)
