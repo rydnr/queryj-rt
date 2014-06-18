bb25f5c75d864528e420edc522937576b1650340
chous <chous@acm-sl.org>
2014-06-18 20:09:00 +0200

    201406182008

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/TableCustomResultsListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/TableCustomResultsListDecorator.java
@@ -228,7 +228,8 @@ public class TableCustomResultsListDecorator
             + super.toString()
             + ", \"customSqlProvider\": " + m__CustomSqlProvider
             + ", \"decoratorFactory\": " + m__DecoratorFactory
-            + ", \"class\": \"TableCustomResultsListDecorator "
+            + ", \"class\": \"TableCustomResultsListDecorator\"" +
+            + ", \"package\": \"org.acmsl.queryj.metadata "
             + "}";
     }
 }
