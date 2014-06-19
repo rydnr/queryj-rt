404180b79c79764f017d0683c4c83a70ae0db14d
chous <chous@acm-sl.org>
2014-06-19 12:40:57 +0200

    201406191240

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
@@ -90,7 +90,7 @@ public class BindQueryParametersHandlerTest
 {
     /**
      * Tests whether the handler binds the parameters to the query correctly.
-     * @throws SQLException if the test fails.
+     * @throws QueryJBuildException if the test fails.
      * @throws SQLException if the test fails.
      */
     @SuppressWarnings("unchecked")
