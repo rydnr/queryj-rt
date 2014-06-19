e325b05e494fe9868c93149a3f2a6565df8e914e
chous <chous@acm-sl.org>
2014-06-19 10:32:02 +0200

    201406191032

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingDecoratorFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingDecoratorFactory.java
@@ -168,7 +168,7 @@ public class CachingDecoratorFactory
         @NotNull final MetadataManager metadataManager)
     {
         return
-            new CachingResultDecorator<V>(
+            new CachingResultDecorator(
                 result, customSqlProvider, metadataManager, this);
     }
 
