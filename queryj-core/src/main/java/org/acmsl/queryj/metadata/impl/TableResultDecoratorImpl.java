47d0f285b4b8210f165aae23f50263f723dbe1cc
chous <chous@acm-sl.org>
2014-06-19 16:25:06 +0200

    201406191625

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImpl.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImpl.java
@@ -136,7 +136,7 @@ public class TableResultDecoratorImpl<V>
         {
             @NotNull final TableResultDecoratorImpl that = (TableResultDecoratorImpl) o;
 
-            result = (getId().equals(that.getId());
+            result = getId().equals(that.getId());
             m__Table.equals(that.m__Table);
         }
         else
