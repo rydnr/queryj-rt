b204ff2444f095756be82950df4d398ab5724df5
chous <chous@acm-sl.org>
2014-06-19 10:32:09 +0200

    201406191032

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingDecoratorFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingDecoratorFactory.java
@@ -162,7 +162,7 @@ public class CachingDecoratorFactory
      */
     @NotNull
     @Override
-    public <V> ResultDecorator<V> createDecorator(
+    public <V> ResultDecorator<DecoratedString> createDecorator(
         @NotNull final Result<String> result,
         @NotNull final CustomSqlProvider customSqlProvider,
         @NotNull final MetadataManager metadataManager)
