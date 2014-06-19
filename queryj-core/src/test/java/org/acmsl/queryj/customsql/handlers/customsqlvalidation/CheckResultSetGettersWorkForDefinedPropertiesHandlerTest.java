fdcbff3719b788e85f7aacff8d3f1ed96543282b
chous <chous@acm-sl.org>
2014-06-19 13:05:48 +0200

    201406191305

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CheckResultSetGettersWorkForDefinedPropertiesHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CheckResultSetGettersWorkForDefinedPropertiesHandlerTest.java
@@ -99,7 +99,7 @@ import java.util.List;
 public class CheckResultSetGettersWorkForDefinedPropertiesHandlerTest
 {
     /**
-     * Checks whether it executes
+     * Checks whether it executes the resultSet getters for defined properies.
      * @throws QueryJBuildException
      * @throws SQLException
      */
