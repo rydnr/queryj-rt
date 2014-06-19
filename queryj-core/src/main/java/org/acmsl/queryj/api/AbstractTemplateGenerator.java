500f412cd3fa0f8fc6580e577e600d85728e835d
chous <chous@acm-sl.org>
2014-06-19 10:35:51 +0200

    201406191035

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
@@ -206,7 +206,7 @@ public abstract class AbstractTemplateGenerator<N extends Template<C>, C extends
      * @param fileUtils the {@link FileUtils} instance.
      * @param log the {@link Log} instance.
      * @return whether it gets written to disk.
-     *
+     * throw
      */
     protected boolean generate(
         @NotNull final N template,
