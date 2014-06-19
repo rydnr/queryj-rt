ee2bc1adc56847f7cca25aa26ba90197c12959ab
chous <chous@acm-sl.org>
2014-06-19 10:28:21 +0200

    201406191028

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableListDecorator.java
@@ -709,11 +709,11 @@ public abstract class AbstractTableListDecorator<V>
     {
         return
               "{ \"super\": " + super.toString()
+            + ", \"table\": \"" + m__Table.getName()
             + ", \"customSqlProvider\": " + this.m__CustomSqlProvider
             + ", \"decoratorFactory\": " + this.m__DecoratorFactory
             + ", \"class\": " + AbstractTableListDecorator.class.getSimpleName() + '"'
             + ", \"package\": \"org.acmsl.queryj.metadata\""
-            + ", \"table\": \"" + m__Table.getName()
             + "\" }";
     }
 }
