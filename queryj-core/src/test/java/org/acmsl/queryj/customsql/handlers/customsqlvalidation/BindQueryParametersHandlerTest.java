1b3033431f614a9b50c87690a78255fdbfd8b61f
chous <chous@acm-sl.org>
2014-06-19 12:38:19 +0200

    201406191238

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
@@ -196,6 +196,10 @@ public class BindQueryParametersHandlerTest
         testPrimitiveBody(Long.class);
     }
 
+    /**
+     * Tests whether retrieveStatementSetterMethod() works for boolean parameters.
+     * @throws Exception if the test fails.
+     */
     @Test
     public void retrieve_statement_setter_method_works_for_double_parameter()
         throws Exception
