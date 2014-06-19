fed1e6fa2a67b70eedc1eec18e36e01c8bef21ef
chous <chous@acm-sl.org>
2014-06-19 12:51:14 +0200

    201406191251

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/vo/AbstractForeignKey.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/vo/AbstractForeignKey.java
@@ -50,6 +50,7 @@ import java.util.List;
 
 /**
  * Provides the common logic for foreign key implementations.
+ * @param <V> the type of the foreign key.
  * @author <a href="mailto:chous@acm-sl.org">Jose San Leandro Armendariz</a>
  */
 public abstract class AbstractForeignKey<V>
