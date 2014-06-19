7305cb6ec70d5f797ba0641376ec5bde5dba4586
chous <chous@acm-sl.org>
2014-06-19 10:36:58 +0200

    201406191036

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerForeignKeyTemplateBuildHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/BasePerForeignKeyTemplateBuildHandler.java
@@ -118,7 +118,7 @@ public abstract class BasePerForeignKeyTemplateBuildHandler
      * @param parameters the parameters.
      * @param metadataManager the database metadata manager.
      * @param templateFactory the template factory.
-     * throws QueryJBuildException if the hash cannot be computed.
+     * throws QueryJBuildException if the templates cannot be
      */
     protected void buildTemplates(
         @NotNull final QueryJCommand parameters,
