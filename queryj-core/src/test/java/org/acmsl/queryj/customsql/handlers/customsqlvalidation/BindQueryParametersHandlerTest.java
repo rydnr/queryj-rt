e5981cdd9ce1cfc3b8db1b79155432cd702b2c11
chous <chous@acm-sl.org>
2014-06-19 12:58:53 +0200

    201406191258

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
@@ -257,7 +257,7 @@ public class BindQueryParametersHandlerTest
 
     /**
      * Tests whether retrieveStatementSetterMethod() works for Float parameters.
-     * @throws Exception if the test fails.
+     * throws Exception if the test fails.
      */
     @Test
     public void retrieve_statement_setter_method_works_for_Float_parameter()
