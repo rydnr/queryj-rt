9f4ef8b831c6b3a2353fa6bcd186728527372783
chous <chous@acm-sl.org>
2014-06-19 10:40:15 +0200

    201406191040

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableDecorator.java
@@ -228,7 +228,7 @@ public abstract class AbstractTableDecorator
         immutableSetReadOnlyAttributes(
             new TableAttributesListDecorator(
                 filterReadOnlyAttributes(
-                    decorateAttributes(attributes, metadataManager, decoratorFactory)), this));
+                    decorateAttributes(attributes, metadataManager, decoratorFactory)),this));
         immutableSetExternallyManagedAttributes(
             new TableAttributesListDecorator(
                 filterExternallyManagedAttributes(decorateAttributes(attributes, metadataManager, decoratorFactory)), this));
