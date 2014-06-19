f019a1dbfcddde43eb8a712af58c1e87af8047b5
chous <chous@acm-sl.org>
2014-06-19 12:58:48 +0200

    201406191258

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/BindQueryParametersHandlerTest.java
@@ -235,7 +235,7 @@ public class BindQueryParametersHandlerTest
 
     /**
      * Tests whether retrieveStatementSetterMethod() works for Double parameters.
-     * @throws Exception if the test fails.
+     * throws Exception if the test fails.
      */
     @Test
     public void retrieve_statement_setter_method_works_for_Double_parameter()
