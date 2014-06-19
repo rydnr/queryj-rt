4d55f89db7c774893d25e8ef4775ce8b564f1cfa
chous <chous@acm-sl.org>
2014-06-19 10:25:28 +0200

    201406191025

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableListDecorator.java
@@ -133,7 +133,7 @@ public abstract class AbstractTableListDecorator<V>
     {
         return
             new TablePartialListDecorator<>(
-                this, getTable(), Operation.PLUS);
+                this, getTable(), Operation.PLUS, getCustomSql);
     }
 
     /**
