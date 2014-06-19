d1a28b97ff50d07b74c4be4b2384088418553a59
chous <chous@acm-sl.org>
2014-06-19 10:31:08 +0200

    201406191031

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingDecoratorFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingDecoratorFactory.java
@@ -460,6 +460,7 @@ public class CachingDecoratorFactory
      * @param object the object to compare to.
      * @return the result of such comparison.
      * object prevents it from being compared to this Object.
+     *
      */
     public int compareTo(@Nullable final Object object)
         throws  ClassCastException
