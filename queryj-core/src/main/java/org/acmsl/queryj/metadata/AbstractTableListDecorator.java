e66d12b3989cf457349516c6d0371b4723989660
chous <chous@acm-sl.org>
2014-06-19 16:12:31 +0200

    201406191612

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableListDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractTableListDecorator.java
@@ -718,7 +718,7 @@ public abstract class AbstractTableListDecorator<V>
             }
         }
 
-        result = createListDecorator()new TableCustomResultsListDecorator(list, table, customSqlProvider, decoratorFactory);
+        result = createListDecorator(list, table, customSqlProvider, decoratorFactory);
 
         return result;
     }
