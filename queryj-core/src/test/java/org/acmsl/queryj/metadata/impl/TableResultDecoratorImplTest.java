13ebb98499238005bf0bc93ab74e4c08a700da81
chous <chous@acm-sl.org>
2014-06-20 08:07:15 +0200

    201406200807

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
@@ -192,7 +192,7 @@ public class TableResultDecoratorImplTest
                 false,
                 "description");
 
-        sql.
+
         @NotNull final List<Sql<String>> queries = Arrays.asList(sql);
 
         @NotNull final SqlDAO sqlDAO = EasyMock.createNiceMock(SqlDAO.class);
