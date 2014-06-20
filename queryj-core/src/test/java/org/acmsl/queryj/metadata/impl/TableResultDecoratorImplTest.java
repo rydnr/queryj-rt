c803797e04b5c94f9790a183ba91cd83bd693572
chous <chous@acm-sl.org>
2014-06-20 06:46:57 +0200

    201406200646

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
@@ -81,7 +81,8 @@ public class TableResultDecoratorImplTest
         protected Table<DecoratedString, Attribute<DecoratedString>, ListDecorator<Attribute<DecoratedString>>> createTableDecorator(
             @Nullable final String parentTable,
             @NotNull final ListDecorator<Attribute<String>> primaryKey,
-            @NotNull final ListDecorator<Attribute<String>> attributes, final boolean isStatic, final boolean voDecorated, @NotNull final MetadataManager metadataManager, @NotNull final DecoratorFactory decoratorFactory, @NotNull final CustomSqlProvider customSqlProvider)
+            @NotNull final ListDecorator<Attribute<String>> attributes,
+            final boolean isStatic,final boolean voDecorated, @NotNull final MetadataManager metadataManager, @NotNull final DecoratorFactory decoratorFactory, @NotNull final CustomSqlProvider customSqlProvider)
         {
             return new MyTableDecorator()
         }
