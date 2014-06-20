34000e6fafed695b5028edd848273d846010e93d
chous <chous@acm-sl.org>
2014-06-20 07:15:28 +0200

    201406200715

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
@@ -155,7 +155,7 @@ public class TableResultDecoratorImplTest
         @NotNull final DecoratorFactory decoratorFactory = EasyMock.createNiceMock(DecoratorFactory.class);
 
         @NotNull final Table<String, Attribute<String>, List<Attribute<String>>> wrappedTable =
-            new TableValueObject("name")
+            new TableValueObject("table", "comment", pk)
         @NotNull final TableDecorator table =
             new MyTableDecorator(
                 new TableIncompleteValueObject("table", "comment"),
