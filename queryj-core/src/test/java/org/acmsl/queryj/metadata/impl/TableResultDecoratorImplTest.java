7ac5bea69073fef89c0945ee01e182036494fa7e
chous <chous@acm-sl.org>
2014-06-20 06:40:04 +0200

    201406200640

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
@@ -48,7 +48,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- *
+ * Tests for {@link TableResultDecoratorImpl}.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2014/06/20 05:37
