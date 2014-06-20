02c20c8891192c7ccd448448613b6a0e8b2bbf8f
chous <chous@acm-sl.org>
2014-06-20 07:13:52 +0200

    201406200713

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
@@ -161,7 +161,7 @@ public class TableResultDecoratorImplTest
                 customSqlProvider);
 
         @NotNull final Attribute<String> attribute1 =
-            new AttributeIncompleteValueObject("name", Types.BIGINT)
+            new AttributeIncompleteValueObject("name", Types.BIGINT, "long")
         @NotNull final Sql<String> sql =
             new SqlElement<>(
                 "sqlId",
