0ea43be7089ba4bc2bcc3814a3d4ce919fd15b54
chous <chous@acm-sl.org>
2014-06-20 06:52:14 +0200

    201406200652

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/impl/TableResultDecoratorImplTest.java
@@ -57,6 +57,7 @@ import org.jetbrains.annotations.NotNull;
  * Importing JUnit/EasyMock classes.
  */
 import org.jetbrains.annotations.Nullable;
+import org.easymock.EasyMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
