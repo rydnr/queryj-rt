b64d673303e0a9d2ad86b452a10e6ea1de241c7d
chous <chous@acm-sl.org>
2014-06-21 11:51:30 +0200

    201406211151

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultSetColumnsHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultSetColumnsHandlerTest.java
@@ -49,7 +49,7 @@ import org.acmsl.queryj.customsql.PropertyRefElement;
 import org.acmsl.queryj.customsql.Result;
 import org.acmsl.queryj.customsql.ResultElement;
 import org.acmsl.queryj.customsql.Sql;
-import org.acmsl.queryj.customsql.Sql.Cardinality;
+import org.acmsl.queryj.customsql.SqlCardinality;
 import org.acmsl.queryj.customsql.SqlElement;
 
 /*
