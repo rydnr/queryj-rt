eb004a05944c5e25573ce9c53d1bd9608a0260a2
chous <chous@acm-sl.org>
2014-06-21 11:47:05 +0200

    201406211147

--- queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/SetupPreparedStatementHandlerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/SetupPreparedStatementHandlerTest.java
@@ -44,7 +44,7 @@ import org.acmsl.queryj.QueryJCommandWrapper;
 import org.acmsl.queryj.SerializablePropertiesConfiguration;
 import org.acmsl.queryj.api.exceptions.QueryJBuildException;
 import org.acmsl.queryj.customsql.Sql;
-import org.acmsl.queryj.customsql.Sql.Cardinality;
+import org.acmsl.queryj.customsql.SqlCardinality;
 import org.acmsl.queryj.customsql.SqlElement;
 import org.acmsl.queryj.tools.handlers.JdbcConnectionOpeningHandler;
 
