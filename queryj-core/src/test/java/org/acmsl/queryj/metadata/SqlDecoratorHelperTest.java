3c482d8332152bcac9ca8249deb75bf0dbbb4888
chous <chous@acm-sl.org>
2014-06-21 11:48:19 +0200

    201406211148

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/SqlDecoratorHelperTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/SqlDecoratorHelperTest.java
@@ -52,7 +52,6 @@ import org.jetbrains.annotations.NotNull;
 /*
  * Importing checkthread.org annotations.
  */
-import org.checkthread.annotations.ThreadSafe;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
