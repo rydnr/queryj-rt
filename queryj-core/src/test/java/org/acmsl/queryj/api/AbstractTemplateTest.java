884068a4c76129ff723875657143f70d73d0f304
chous <chous@acm-sl.org>
2014-06-25 16:38:19 +0200

    201406251638

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateTest.java
@@ -49,7 +49,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /*
- * Importing checkthread.org annotations.
+ * Importing JUnit/EasyMock classes.
  */
 import org.easymock.EasyMock;
 import org.junit.Assert;
