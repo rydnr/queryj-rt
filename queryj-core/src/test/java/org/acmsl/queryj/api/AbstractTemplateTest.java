91be68ef2fbd55dbfb47d8ea407a5074647ca6b9
chous <chous@acm-sl.org>
2014-06-25 16:31:12 +0200

    201406251631

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateTest.java
@@ -58,5 +58,8 @@ import org.junit.runners.JUnit4;
 public class AbstractTemplateTest
 {
     @Test
-    public void generateOutput_calls_TemplateDebuggingService_if_c
+    public void generateOutput_calls_TemplateDebuggingService_when_debugging()
+    {
+
+    }
 }
