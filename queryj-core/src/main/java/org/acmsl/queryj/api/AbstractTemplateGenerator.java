530baede777d62d4aa7d6449f704fc9db80d3a14
chous <chous@acm-sl.org>
2014-06-25 16:00:40 +0200

    201406251600

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
@@ -533,6 +533,7 @@ public abstract class AbstractTemplateGenerator<N extends Template<C>, C extends
      * @return such instance, or {@code null} if none is found.
      */
     @Nullable
+    @SuppressWarnings("unchecked")
     public TemplateDebuggingService<C> resolveTemplateDebuggingService()
     {
         @NotNull final TemplateDebuggingService<? extends TemplateContext> result;
