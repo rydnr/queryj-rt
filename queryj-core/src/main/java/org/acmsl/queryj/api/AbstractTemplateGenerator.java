b1f70ac04bdfdf46e213d40b5e8fd98981bc1089
chous <chous@acm-sl.org>
2014-06-25 19:24:45 +0200

    201406251924

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
@@ -353,7 +353,7 @@ public abstract class AbstractTemplateGenerator<N extends Template<C>, C extends
                             charset);
 
                         debugging(t_strFileContents, stTemplate, template.getTemplateContext(), templateDebuggingService);
-
+                    }
                     writeHash(newHash, fileName, outputDir, rootFolder, charset, fileUtils);
                 }
                 else
