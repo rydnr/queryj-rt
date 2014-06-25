834cf5014f5a88777c8c040bbdab35a0c435e2ab
chous <chous@acm-sl.org>
2014-06-25 19:01:05 +0200

    201406251901

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
@@ -355,7 +355,7 @@ public abstract class AbstractTemplateGenerator<N extends Template<C>, C extends
      * Manages the debugging session.
      * @param context the context.
      * @param debuggingService the {@link TemplateDebuggingService} instance.
-     * @return {@code true} if the template is be
+     * @return {@code true} if the template
      */
     protected boolean debugging(
         @NotNull final TemplateContext context, @Nullable final TemplateDebuggingService debuggingService)
