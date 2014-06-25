2d49dd94608058d8730badeb481978ef4b5a7eca
chous <chous@acm-sl.org>
2014-06-25 18:02:01 +0200

    201406251802

--- queryj-core/src/main/java/org/acmsl/queryj/tools/debugging/TemplateDebuggingService.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/debugging/TemplateDebuggingService.java
@@ -54,7 +54,6 @@ import org.jetbrains.annotations.NotNull;
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
-import org.stringtemplate.v4.ST;
 
 /**
  * Services able to debug templates.
