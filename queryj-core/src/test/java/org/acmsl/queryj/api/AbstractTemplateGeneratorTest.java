c661cee29e7679004c796e1ca45c10cdbbbcd469
chous <chous@acm-sl.org>
2014-06-25 16:08:06 +0200

    201406251608

--- queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateGeneratorTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/api/AbstractTemplateGeneratorTest.java
@@ -94,7 +94,7 @@ public class AbstractTemplateGeneratorTest
     }
 
     /**
-     *
+     * A
      * @param <C>
      */
     public static class DoNothingDebuggingService<C extends TemplateContext>
