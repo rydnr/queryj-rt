7e0974817d7acdb6e8337f566d1a575afa17db5d
chous <chous@acm-sl.org>
2014-06-25 16:00:02 +0200

    201406251600

--- queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/AbstractTemplateGenerator.java
@@ -549,7 +549,7 @@ public abstract class AbstractTemplateGenerator<N extends Template<C>, C extends
             {
                 for (@NotNull final TemplateDebuggingService<C> factory : loader)
                 {
-                    result =
+                    result.add(
                         (FillTemplateChain <? extends FillHandler<?>>)
                             factory.createFillChain(context));
                 }
