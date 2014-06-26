355cdf37f858a17e8e5911b2efb1fe1366bdcc4e
chous <chous@acm-sl.org>
2014-06-26 19:49:22 +0200

    201406261949

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -38,14 +38,27 @@ package org.acmsl.queryj.debugging.netty;
 /*
  * Importing JetBrains annotations.
  */
+import org.acmsl.commons.utils.io.FileUtils;
+import org.acmsl.queryj.api.AbstractTemplateGenerator;
+import org.acmsl.queryj.api.TemplateContext;
+import org.acmsl.queryj.debugging.AbstractTemplateTest;
+import org.acmsl.queryj.debugging.AbstractTemplateTest.MyTestableAbstractTemplate;
+import org.acmsl.queryj.debugging.STInspectorDebuggingService;
+import org.acmsl.queryj.metadata.DecoratorFactory;
+import org.acmsl.queryj.tools.debugging.TemplateDebuggingService;
+import org.easymock.EasyMock;
 import org.jetbrains.annotations.NotNull;
 
 /*
  * Importing JUnit classes.
  */
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.io.File;
+import java.nio.charset.Charset;
 
 /**
  * Tests for {@link NettyServerDebuggingService}.
@@ -67,6 +80,80 @@ public class NettyServerDebuggingServiceTest
             new NettyServerDebuggingService();
 
         // setup an AbstractTemplateGenerator
+        @NotNull final TemplateContext templateContext = EasyMock.createNiceMock(TemplateContext.class);
+        EasyMock.expect(templateContext.getFileName()).andReturn("foo.java").anyTimes();
+        EasyMock.expect(templateContext.isDebugEnabled()).andReturn(true);
+        EasyMock.replay(templateContext);
+
+        @NotNull final DecoratorFactory decoratorFactory = EasyMock.createNiceMock(DecoratorFactory.class);
+
+        @NotNull final STInspectorDebuggingService<TemplateContext> service =
+            new STInspectorDebuggingService<>();
+
+        @SuppressWarnings("unchecked")
+        @NotNull final AbstractTemplateGenerator<MyTestableAbstractTemplate<TemplateContext>, TemplateContext> instance =
+            new AbstractTemplateGenerator(false, 1)
+            {
+                /**
+                 * {@inheritDoc}
+                 */
+                @NotNull
+                @Override
+                public DecoratorFactory getDecoratorFactory()
+                {
+                    return decoratorFactory;
+                }
+
+                /**
+                 * {@inheritDoc}
+                 */
+                @Override
+                protected boolean isInDevMode(@NotNull final String templateFileName)
+                {
+                    return true;
+                }
+
+                /**
+                 * {@inheritDoc}
+                 */
+                @Nullable
+                @Override
+                public TemplateDebuggingService resolveTemplateDebuggingService()
+                {
+                    return service;
+                }
+
+                /**
+                 * {@inheritDoc}
+                 */
+                @Nullable
+                @Override
+                protected String retrieveHash(
+                    @NotNull final String fileName,
+                    @NotNull final File outputDir,
+                    @NotNull final File rootFolder,
+                    @NotNull final Charset charset,
+                    @NotNull final FileUtils fileUtils)
+                {
+                    return "";
+                }
+            };
+
+        @NotNull final AbstractTemplateTest.MyTestableAbstractTemplate<TemplateContext> template =
+            new AbstractTemplateTest.MyTestableAbstractTemplate<>(templateContext);
+
+        @NotNull final File outputDir = EasyMock.createNiceMock(File.class);
+        EasyMock.expect(outputDir.getAbsolutePath()).andReturn("/tmp").anyTimes();
+        EasyMock.expect(outputDir.exists()).andReturn(true).anyTimes();
+        EasyMock.replay(outputDir);
+        @NotNull final File rootFolder = EasyMock.createNiceMock(File.class);
+        EasyMock.expect(rootFolder.getAbsolutePath()).andReturn("/tmp").anyTimes();
+        EasyMock.expect(rootFolder.exists()).andReturn(true).anyTimes();
+        EasyMock.replay(rootFolder);
+
+        instance.write(template, outputDir, rootFolder, Charset.defaultCharset());
+
+        EasyMock.verify(template.template);
 
         // Simulate the server receives a reload command:
         // Create a client
