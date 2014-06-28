afe820a809f6fa7028d041e97b1797f87dea63dd
chous <chous@acm-sl.org>
2014-06-28 19:25:04 +0200

    201406281925

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -284,7 +284,7 @@ public class NettyServerDebuggingServiceTest
          * {@inheritDoc}
          */
         @Override
-        public void operationComplete(@NotNull final Future<?> future)
+        public void operationComplete(@NotNull final ChannelFuture future)
             throws Exception
         {
             Assert.assertTrue(service.m__bReloadCalled);
