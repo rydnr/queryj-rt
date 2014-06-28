524820566f9d18eb87bb67f67c6a608b00f7f22d
chous <chous@acm-sl.org>
2014-06-28 09:36:44 +0200

    201406280936

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -63,6 +63,7 @@ import org.jetbrains.annotations.NotNull;
  * Importing JUnit classes.
  */
 import org.jetbrains.annotations.Nullable;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
