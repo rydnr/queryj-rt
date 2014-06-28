3f7a2d7f4e605cd388e821e76de83a40e89cc1d2
chous <chous@acm-sl.org>
2014-06-28 19:26:06 +0200

    201406281926

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -290,7 +290,7 @@ public class NettyServerDebuggingServiceTest
         public String toString()
         {
             return
-                  "{ \"command\": \"" + m__strCommand + '"'
+                  "{ \"service\": \"" + service + '"'
                 + ", \"reloadCalled\": " + m__bReloadCalled
                 + ", \"class\": \"" + MyNettyServerDebuggingService.class.getSimpleName() + '"'
                 + ", \"package\": \"" + MyNettyServerDebuggingService.class.getPackage().getName() + "\" }";
