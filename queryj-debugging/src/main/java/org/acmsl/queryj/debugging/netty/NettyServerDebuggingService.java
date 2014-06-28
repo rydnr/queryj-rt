ca5352c8d53528d87224085cc0916b655e211a39
chous <chous@acm-sl.org>
2014-06-28 21:15:00 +0200

    201406282115

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -271,8 +271,10 @@ public class NettyServerDebuggingService<C extends TemplateContext>
             // Wait until the server socket is closed.
             // In this example, this does not happen, but you can do that to gracefully
             // shut down your server.
-            f.channel().closeFuture().sync();
-        } finally {
+            result.channel().closeFuture().sync();
+        }
+        finally
+        {
             workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
         }
