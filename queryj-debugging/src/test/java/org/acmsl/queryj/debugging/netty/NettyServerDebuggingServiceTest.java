4444471d0d62e52689b74a89f9e4e55cd0844a25
chous <chous@acm-sl.org>
2014-06-28 21:10:47 +0200

    201406282110

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -202,7 +202,7 @@ public class NettyServerDebuggingServiceTest
         out.write(buf);
         socket.close();
 
-        future.syncUninterruptibly();
+        future.channel().closeFuture();
 
         Assert.assertTrue(future.isDone());
 
