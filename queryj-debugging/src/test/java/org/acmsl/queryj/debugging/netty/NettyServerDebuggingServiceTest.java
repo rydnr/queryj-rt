8471e259db16e9eebef8fffff76ba328096e6141
chous <chous@acm-sl.org>
2014-06-28 10:07:00 +0200

    201406281007

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -246,7 +246,7 @@ public class NettyServerDebuggingServiceTest
         @Override
         public void channelRead(@NotNull final ChannelHandlerContext ctx, @NotNull final Object msg)
         {
-
+            @NotNull final ByteBuf
             this.m__strCommand = ((ByteBuf) msg).readableBytes();
             this.m__bAlive = false;
         }
