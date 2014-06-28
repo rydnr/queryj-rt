ca8f5cd4dab7fd4bf1893ee1b9cf59c970fdaef4
chous <chous@acm-sl.org>
2014-06-28 10:52:29 +0200

    201406281052

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -129,7 +129,7 @@ public class NettyServerDebuggingService<C extends TemplateContext>
         // launch the server
     }
 
-    public void launchServer()
+    public NioEventLoopGroup launchServer()
         throws InterruptedException,
                 IOException
     {
