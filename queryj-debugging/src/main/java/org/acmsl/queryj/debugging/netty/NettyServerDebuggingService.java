eee74755473c191a60897351b3d31fef99d4ff28
chous <chous@acm-sl.org>
2014-06-28 11:08:23 +0200

    201406281108

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -165,7 +165,7 @@ public class NettyServerDebuggingService<C extends TemplateContext>
     /**
      * Launches the server.
      * @param port the port.
-     * @return the {@link ServerBootstrap}.
+     * @return the {@link ChannelFuture}.
      * @throws InterruptedException if the server gets interrupted.
      * @throws IOException if the socket cannot be bound.
      */
