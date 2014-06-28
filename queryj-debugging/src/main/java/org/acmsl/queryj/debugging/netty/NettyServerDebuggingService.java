402ecb06e185f7651578933db3652869a4600733
chous <chous@acm-sl.org>
2014-06-28 11:13:57 +0200

    201406281113

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -230,7 +230,7 @@ public class NettyServerDebuggingService<C extends TemplateContext>
      */
     protected void stopServer(@NotNull final ServerBootstrap bootstrap)
     {
-        channelFuture.
+        bootstrap
     }
 
     /**
