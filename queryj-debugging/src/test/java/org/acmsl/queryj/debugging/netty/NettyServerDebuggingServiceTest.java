ebf84f77503d7c85ffd904cf42c15e58c3565da1
chous <chous@acm-sl.org>
2014-06-28 11:22:40 +0200

    201406281122

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -182,7 +182,9 @@ public class NettyServerDebuggingServiceTest
     public void reload_command_gets_received_correctly()
         throws Exception
     {
-        NettyServerDebuggingService<TemplateContext> instance = new NettyServerDebuggingService<>()
+        @NotNull final NettyServerDebuggingService<TemplateContext> instance = new NettyServerDebuggingService<>();
+
+
         final NioEventLoopGroup group = new NioEventLoopGroup(1);
 
         try
