203ea5d679bfb14c910f28b10c4637606aca7621
chous <chous@acm-sl.org>
2014-06-28 11:18:53 +0200

    201406281118

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -249,7 +249,23 @@ public class NettyServerDebuggingService<C extends TemplateContext>
         result = serverBootstrap.bind(port);
 
         Runtime.getRuntime().addShutdownHook(new Thread() {
-
+            /**
+             * If this thread was constructed using a separate
+             * <code>Runnable</code> run object, then that
+             * <code>Runnable</code> object's <code>run</code> method is called;
+             * otherwise, this method does nothing and returns.
+             * <p/>
+             * Subclasses of <code>Thread</code> should override this method.
+             *
+             * @see #start()
+             * @see #stop()
+             * @see #Thread(ThreadGroup, Runnable, String)
+             */
+            @Override
+            public void run()
+            {
+                super.run();
+            }
         });
         return result;
     }
