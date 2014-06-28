368fb75d2aea9d0b91ea6381c3e6f3ab101a2ee9
chous <chous@acm-sl.org>
2014-06-28 11:05:04 +0200

    201406281105

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -177,7 +177,7 @@ public class NettyServerDebuggingService<C extends TemplateContext>
 
         try
         {
-            result.channel()
+            result.channel().localAddress()
             @NotNull final SocketAddress address = result.bind(port).sync().channel().localAddress();
 
             result.group(group).channel(NioServerSocketChannel.class);
