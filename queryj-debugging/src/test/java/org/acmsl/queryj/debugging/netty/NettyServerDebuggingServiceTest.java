7d7e3562a32f3150887efb94f3b98d604dbafefd
chous <chous@acm-sl.org>
2014-06-28 19:27:51 +0200

    201406281927

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -200,8 +200,6 @@ public class NettyServerDebuggingServiceTest
         out.write(buf);
         socket.close();
 
-        future.addListener(new MyChannelListener(instance));
-
         future.await();
 
         instance.stopServer();
