16e674be74ecc91074f426b14d976826ef985750
chous <chous@acm-sl.org>
2014-06-28 11:01:39 +0200

    201406281101

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -166,7 +166,7 @@ public class NettyServerDebuggingService<C extends TemplateContext>
      * @throws InterruptedException if the server gets interrupted.
      * @throws IOException if the socket cannot be bound.
      */
-    public NioEventLoopGroup launchServer()
+    public NioEventLoopGroup launchServer(final int port)
         throws InterruptedException,
         IOException
     {
