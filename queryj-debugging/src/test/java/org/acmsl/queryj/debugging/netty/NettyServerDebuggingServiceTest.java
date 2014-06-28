6d9b959b323c35b259dc35971fb39f2ca1a4edd0
chous <chous@acm-sl.org>
2014-06-28 09:56:49 +0200

    201406280956

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -256,7 +256,7 @@ public class NettyServerDebuggingServiceTest
         public String toString()
         {
             return
-                "{ \"command\": " + m__strCommand + ", \"class\": \"" + MyChannelHandlerAdapter.class.getSimpleName() + "\", \"package\": \"" + MyChannelHandlerAdapter.class.getPackage().getName() + "\" }";
+                "{ \"command\": \"" + m__strCommand + ", \"class\": \"" + MyChannelHandlerAdapter.class.getSimpleName() + "\", \"package\": \"" + MyChannelHandlerAdapter.class.getPackage().getName() + "\" }";
         }
     }
 }
