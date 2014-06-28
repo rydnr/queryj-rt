8b9b63ae630b29c4b8d3835b8a42a688961d1033
chous <chous@acm-sl.org>
2014-06-28 09:54:40 +0200

    201406280954

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -240,7 +240,7 @@ public class NettyServerDebuggingServiceTest
         @Override
         public void channelRead(@NotNull final ChannelHandlerContext ctx, @NotNull final Object msg)
         {
-
+            this.command = "" + msg;
         }
     }
 }
