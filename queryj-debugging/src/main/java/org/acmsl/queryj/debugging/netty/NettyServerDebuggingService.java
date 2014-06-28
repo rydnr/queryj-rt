1994f7554fec78d781e1715ebef506c2abcedc15
chous <chous@acm-sl.org>
2014-06-28 21:40:32 +0200

    201406282140

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -337,6 +337,10 @@ public class NettyServerDebuggingService<C extends TemplateContext>
             ReferenceCountUtil.release(msg);
         }
 
+        try
+        {
+
+        }
         ctx.channel().closeFuture().sync();
     }
 
