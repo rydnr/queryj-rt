5122a6e49842458ce95830b69f4a344de42ab4de
chous <chous@acm-sl.org>
2014-06-28 11:07:36 +0200

    201406281107

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -173,6 +173,7 @@ public class NettyServerDebuggingService<C extends TemplateContext>
         throws InterruptedException,
         IOException
     {
+        @NotNull final ChannelFuture result = serverBootstrap.bind(port);
         @NotNull final ServerBootstrap serverBootstrap = new ServerBootstrap();
         @NotNull final NioEventLoopGroup group = new NioEventLoopGroup(1);
 
