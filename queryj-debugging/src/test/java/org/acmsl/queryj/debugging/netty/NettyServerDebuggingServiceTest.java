db4dfbc42d3898dce6010a851efaedb4b20e1c4d
chous <chous@acm-sl.org>
2014-06-28 09:36:17 +0200

    201406280936

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -202,7 +202,7 @@ public class NettyServerDebuggingServiceTest
             byte[] buf = new byte[3];
             in.readFully(buf);
 
-            Assert.a--(new String(buf, CharsetUtil.US_ASCII), is("abc"));
+            Assert.assertEquals(new String(buf, CharsetUtil.US_ASCII), is("abc"));
 
             s.close();
         } finally {
