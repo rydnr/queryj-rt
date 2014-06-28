c840f373cb38c0e3389098a7cf30e0a9034d70a2
chous <chous@acm-sl.org>
2014-06-28 11:05:37 +0200

    201406281105

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -168,7 +168,7 @@ public class NettyServerDebuggingService<C extends TemplateContext>
      * @throws InterruptedException if the server gets interrupted.
      * @throws IOException if the socket cannot be bound.
      */
-    public ServerBootstrap launchServer(final int port)
+    public ChannelFuture launchServer(final int port)
         throws InterruptedException,
         IOException
     {
