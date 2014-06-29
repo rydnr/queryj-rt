1d2abbac2a50662e77937241290e63ebd294801a
chous <chous@acm-sl.org>
2014-06-29 08:47:32 +0200

    201406290847

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -224,7 +224,7 @@ public class NettyServerDebuggingServiceTest
                                     }
                                     finally
                                     {
-
+                                        buffer.release();
                                     }
                                 }
                             });
