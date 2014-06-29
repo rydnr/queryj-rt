ff73eef888926a459b58f6cc371e3514996854a1
chous <chous@acm-sl.org>
2014-06-29 09:19:49 +0200

    201406290919

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -232,6 +232,7 @@ public class NettyServerDebuggingServiceTest
 
                                     buffer.writeBytes(msg.getBytes(CharsetUtil.US_ASCII));
                                     buffer.release();
+                                    ctx.writeAndFlush()
                                 }
                             });
                     }
