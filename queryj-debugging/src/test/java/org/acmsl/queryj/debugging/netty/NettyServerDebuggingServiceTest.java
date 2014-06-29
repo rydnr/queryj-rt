8632f534d47d130bb17b4099c6ddd3e6edbd3ecc
chous <chous@acm-sl.org>
2014-06-29 08:41:18 +0200

    201406290841

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -178,7 +178,7 @@ public class NettyServerDebuggingServiceTest
         EasyMock.verify(template.template);
     }
 
-    protected void send
+    protected void
     /**
      * Checks the reload command gets received successfully.
      */
