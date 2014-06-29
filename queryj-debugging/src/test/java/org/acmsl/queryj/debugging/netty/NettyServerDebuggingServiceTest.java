45d4333433d293488ab4eee3f0d4ff5ee64d6a0b
chous <chous@acm-sl.org>
2014-06-29 08:42:03 +0200

    201406290842

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -45,8 +45,12 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.CharsetUtil;
 import io.netty.util.NetUtil;
 import io.netty.util.concurrent.Future;
@@ -180,7 +184,28 @@ public class NettyServerDebuggingServiceTest
 
     protected void sendTextToServer(final int port, final String msg)
     {
+         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
+        try {
+            Bootstrap b = new Bootstrap(); // (1)
+            b.group(workerGroup); // (2)
+            b.channel(NioSocketChannel.class); // (3)
+            b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new TimeClientHandler());
+                }
+            });
+
+            // Start the client.
+            ChannelFuture f = b.connect(host, port).sync(); // (5)
+
+            // Wait until the connection is closed.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
     }
     /**
      * Checks the reload command gets received successfully.
