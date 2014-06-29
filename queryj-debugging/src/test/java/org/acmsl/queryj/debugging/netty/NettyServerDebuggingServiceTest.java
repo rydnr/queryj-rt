c6c25aeb31845704fdd3fc4f8a5dc7747ff49bf5
chous <chous@acm-sl.org>
2014-06-29 08:24:12 +0200

    201406290824

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -330,7 +330,7 @@ public class NettyServerDebuggingServiceTest
         public void operationComplete(@NotNull final ChannelFuture future)
             throws Exception
         {
-            //Assert.assertTrue(service.m__bReloadCalled);
+            Assert.assertTrue(service.m__bReloadCalled);
         }
 
         /**
