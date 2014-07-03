0e5a1537532e0c2608b747cb15142c5e3b6b23bd
chous <chous@acm-sl.org>
2014-07-03 19:32:07 +0200

    201407031932

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingService.java
@@ -205,7 +205,7 @@ public class NettyServerDebuggingService<C extends TemplateContext>
         }
         catch (@NotNull final InterruptedException interruption)
         {
-
+            throw new DevelopmentModeException()
         }
         catch (@NotNull final IOException ioException)
         {
