91fca052c38997b9d6ff80b2dd7fe1122a9a0bfe
chous <chous@acm-sl.org>
2014-07-03 18:19:18 +0200

    201407031819

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -631,6 +631,11 @@ public class NettyServerDebuggingServiceTest
             ctx.writeAndFlush(Unpooled.copiedBuffer(getMessage(), CharsetUtil.UTF_8));
         }
 
+        @Override
+        public void channelRead(final ChannelHandlerContext ctx, final Object msg) throws Exception
+        {
+            super.channelRead(ctx, msg);
+        }
 
         /**
          * {@inheritDoc}
