0103228d525d3e97e1bbe26f4cf0f1a11d2f0dfd
chous <chous@acm-sl.org>
2014-07-03 18:22:25 +0200

    201407031822

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -552,12 +552,14 @@ public class NettyServerDebuggingServiceTest
          * Connects to the server.
          */
         public void connect()
-        throws Exception {
-EventLoopGroup group = new NioEventLoopGroup();
-try {
-Bootstrap b = new Bootstrap();
-b.group(group)
-.channel(NioSocketChannel.class)
+            throws Exception
+        {
+            @NotNull final EventLoopGroup group = new NioEventLoopGroup();
+            try
+            {
+                @NotNull final Bootstrap b = new Bootstrap();
+                b.group(group)
+                    .channel(NioSocketChannel.class)
 .remoteAddress(new InetSocketAddress(host, port))
 .handler(new ChannelInitializer<SocketChannel>() {
 @Override
