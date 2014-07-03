48477b069f8950470ce2bafc326d031ffa659af7
chous <chous@acm-sl.org>
2014-07-03 18:22:27 +0200

    201407031822

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -559,7 +559,7 @@ public class NettyServerDebuggingServiceTest
             {
                 @NotNull final Bootstrap b = new Bootstrap();
                 b.group(group)
-                    .channel(NioSocketChannel.class)
+                 .channel(NioSocketChannel.class)
 .remoteAddress(new InetSocketAddress(host, port))
 .handler(new ChannelInitializer<SocketChannel>() {
 @Override
