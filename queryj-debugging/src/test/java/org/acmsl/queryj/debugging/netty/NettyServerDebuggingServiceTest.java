31307e04e5656e591a7b31f211c4ce8316431ff7
chous <chous@acm-sl.org>
2014-07-03 18:45:12 +0200

    201407031845

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -219,7 +219,7 @@ public class NettyServerDebuggingServiceTest
         // Wait until the connections are closed.
         Assert.assertTrue(server.isDone());
 
-        server.channel().closeFuture().sync();
+//        server.channel().closeFuture().sync();
 
 //        Assert.assertTrue(handler.m__bReloadCalled);
 
