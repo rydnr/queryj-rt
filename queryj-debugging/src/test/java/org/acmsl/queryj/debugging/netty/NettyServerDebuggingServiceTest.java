1f068b7a042177f5f660f7d66bd081dd9fd9a6a2
chous <chous@acm-sl.org>
2014-07-06 19:59:58 +0200

    201407061959

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/netty/NettyServerDebuggingServiceTest.java
@@ -600,7 +600,8 @@ public class NettyServerDebuggingServiceTest
         @Override
         protected void messageReceived(final ChannelHandlerContext ctx, final ByteBuf msg) throws Exception
         {
-            LogFactory.getLog(NettyServerDebuggingServiceTest.MyNettyServerChannelHandler.class).info("Client received: " + msg);
+            LogFactory.getLog(NettyServerDebuggingServiceTest.MyNettyServerChannelHandler.class).info(
+                "Client received: " + msg);
         }
 
         /**
