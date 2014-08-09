7e3fdf44a96618a23beec4a77df0e5a0d88bfef2
chous <chous@acm-sl.org>
2014-08-09 08:04:57 +0000

    201408090804

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CheckResultSetGettersWorkForDefinedPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/CheckResultSetGettersWorkForDefinedPropertiesHandler.java
@@ -177,6 +177,7 @@ public class CheckResultSetGettersWorkForDefinedPropertiesHandler
      * @param handler a {@link RetrieveResultPropertiesHandler} for dealing with reflection.
      * @param metadataManager the {@link MetadataManager} instance.
      * @throws SQLException if the properties cannot be validated.
+     * @throws SQLException if the properties cannot be validated.
      */
     protected void validateProperties(
         @NotNull final ResultSet resultSet,
