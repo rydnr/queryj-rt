8aa4413c145228ca8ef816fe4918f7a7221d9658
chous <chous@acm-sl.org>
2014-08-09 07:20:48 +0000

    201408090720

--- queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/JdbcConnectionOpeningHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/JdbcConnectionOpeningHandler.java
@@ -103,6 +103,7 @@ public class JdbcConnectionOpeningHandler
      * attribute map.
      * @param parameters the parameter map.
      * @return the JDBC connection.
+     * @throws QueryJBuildException if the connection cannot be closed.
      */
     @NotNull
     protected Connection openConnection(@NotNull final QueryJCommand parameters)
