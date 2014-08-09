b423993a810660598b55123de7815328b4745110
chous <chous@acm-sl.org>
2014-08-09 08:55:27 +0000

    201408090855

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/MetadataManagerTableDAO.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/MetadataManagerTableDAO.java
@@ -748,7 +748,7 @@ public abstract class MetadataManagerTableDAO<M extends MetadataManager>
     public String toString()
     {
         return
-              "{ \"class\": \"MetadataManagerTableDAO\""
+              "{ "
             + ", \"metadataManager\": \"" + m__MetadataManager.hashCode() + "\" }";
     }
 }
