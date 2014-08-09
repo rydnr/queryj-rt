be983758eb380b04ed89a1891f479c94269eaff3
chous <chous@acm-sl.org>
2014-08-09 07:44:20 +0000

    201408090744

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
@@ -129,7 +129,7 @@ public class QueryJMojo
      * The driver.
      * @parameter property="driver" @required
      */
-    @Parameter ()
+    @Parameter (property = "driver")
     private String m__strDriver;
 
     /**
