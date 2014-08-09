02282343847be20296be8ba5f5473b4129b84a51
chous <chous@acm-sl.org>
2014-08-09 09:05:36 +0000

    201408090905

--- queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/AbstractFillHandler.java
+++ queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/AbstractFillHandler.java
@@ -111,8 +111,6 @@ public abstract class AbstractFillHandler<P>
      */
     @SuppressWarnings("unchecked")
     public boolean handle(@NotNull final Map<String, P> map, @NotNull final String placeHolder)
-        throws InvalidTemplateException,
-               QueryJBuildException
     {
         final boolean result = true;
 
