6f94b74c8cf57ffcaf14112283553372746e9e91
chous <chous@acm-sl.org>
2014-08-09 07:25:33 +0000

    201408090725

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/MetadataTypeUtils.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/MetadataTypeUtils.java
@@ -45,6 +45,7 @@ import org.jetbrains.annotations.NotNull;
  * Importing checkthread.org annotations.
  */
 import org.checkthread.annotations.ThreadSafe;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
