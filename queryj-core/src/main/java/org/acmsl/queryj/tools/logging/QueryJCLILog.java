71cf19ad231da587ffd66137a1ea531b439aec5f
chous <chous@acm-sl.org>
2014-08-09 08:18:15 +0000

    201408090818

--- queryj-core/src/main/java/org/acmsl/queryj/tools/logging/QueryJCLILog.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/logging/QueryJCLILog.java
@@ -156,13 +156,9 @@ public class QueryJCLILog
     }
 
     /**
-     * <p> Is trace logging currently enabled? </p>
-     *
-     * <p> Call this method to prevent having to perform expensive operations
-     * (for example, <code>String</code> concatenation)
-     * when the log level is more than trace. </p>
-     * @return <code>true</code> in such case.
+     * {@inheritDoc}
      */
+    @Override
     public boolean isTraceEnabled()
     {
         return isTraceEnabled(getThreshold());
