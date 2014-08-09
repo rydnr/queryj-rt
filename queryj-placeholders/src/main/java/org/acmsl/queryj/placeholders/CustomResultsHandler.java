76b460fd0c1bb4b926982989b6b3fad0a33ade9b
chous <chous@acm-sl.org>
2014-08-09 09:07:05 +0000

    201408090907

--- queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/CustomResultsHandler.java
+++ queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/CustomResultsHandler.java
@@ -130,7 +130,7 @@ public class CustomResultsHandler
      * @param daoTemplateUtils the <code>DAOTemplateUtils</code> instance.
      * @param templateUtils the <code>TemplateUtils</code> instance.
      * @return the custom results.
-     * @throws
+     * @throws QueryJBuildException if {@code TemplateUtils#}
      */
     @NotNull
     protected List<Result<String>> retrieveCustomResults(
