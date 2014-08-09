72f4839f8ded926dfa556b0c0abbb340359a8a0d
chous <chous@acm-sl.org>
2014-08-09 08:53:44 +0000

    201408090853

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/AbstractJdbcMetadataManager.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/engines/AbstractJdbcMetadataManager.java
@@ -1247,6 +1247,8 @@ public abstract class AbstractJdbcMetadataManager
      * @param schema the schema.
      * @param caseSensitiveness whether the table names are case sensitive or not.
      * @param metadataExtractionListener the metadata extraction listener.
+     * @throws SQLException if the extraction fails.
+     * @throws QueryJException if any other error occurs.
      */
     protected abstract void extractPrimaryKeys(
         @NotNull final DatabaseMetaData metaData,
