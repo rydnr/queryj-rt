a85ccc884b01a1c6465c66b27b3b4309e51dc58e
chous <chous@acm-sl.org>
2014-08-09 07:19:48 +0000

    201408090719

--- queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/JdbcConnectionClosingHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/JdbcConnectionClosingHandler.java
@@ -33,7 +33,7 @@
 package org.acmsl.queryj.tools.handlers;
 
 /*
- * Importing some project classes.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.QueryJCommandWrapper;
