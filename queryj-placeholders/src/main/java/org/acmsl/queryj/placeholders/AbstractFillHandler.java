6ef14bbfd92cff068e1ef88140fdfb088dfb2927
chous <chous@acm-sl.org>
2014-08-09 09:03:42 +0000

    201408090903

--- queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/AbstractFillHandler.java
+++ queryj-placeholders/src/main/java/org/acmsl/queryj/placeholders/AbstractFillHandler.java
@@ -106,7 +106,7 @@ public abstract class AbstractFillHandler<P>
      * @param map the command map.
      * @param placeHolder the placeholder.
      * model.
-     * @return the
+     * @return {@code true}
      */
     @SuppressWarnings("unchecked")
     public boolean handle(@NotNull final Map<String, P> map, @NotNull final String placeHolder)
