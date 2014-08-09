d39d9c59533348644db991b1885278ac893b3560
chous <chous@acm-sl.org>
2014-08-09 08:17:08 +0000

    201408090817

--- queryj-core/src/main/java/org/acmsl/queryj/tools/logging/QueryJCLILog.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/logging/QueryJCLILog.java
@@ -32,8 +32,7 @@
  * Description: Minimal logging helper to integrate with UniqueLogFactory
  *              API. It implements *Log* only to make the developer comfortable
  *              with the API, which just means replacing *LogFactory.getLog(..)*
- *              with
- * using the *UniqueLogLogFactory.getLog(..)*.
+ *              with *UniqueLogLogFactory.getLog(..)*.
  *
  * Version: $Revision$ ($Author$ at $Date$)
  *
