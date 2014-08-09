d1a92a83dc2e2798f31bf209ef7d97ffc2c49755
chous <chous@acm-sl.org>
2014-08-09 07:43:01 +0000

    201408090743

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
@@ -96,7 +96,7 @@ import org.checkthread.annotations.ThreadSafe;
  */
 @SuppressWarnings("unused")
 @ThreadSafe
-@Mojo( name = "queryj", defaultPhase = LifecyclePhase.GENERATE_SOURCES, threadSafe = true)
+@org.apache.maven.plugins.annotations.Mojo( name = "queryj", defaultPhase = LifecyclePhase.GENERATE_SOURCES, threadSafe = true)
 public class QueryJMojo
     extends AbstractMojo
     implements Mojo
