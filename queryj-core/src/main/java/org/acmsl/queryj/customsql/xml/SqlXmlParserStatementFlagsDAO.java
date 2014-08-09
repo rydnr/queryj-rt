a2ebdc815596bd3f904d98ca4de379741e48e813
chous <chous@acm-sl.org>
2014-08-09 06:33:56 +0000

    201408090633

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/xml/SqlXmlParserStatementFlagsDAO.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/xml/SqlXmlParserStatementFlagsDAO.java
@@ -1,5 +1,5 @@
 /*
-                        QueryJ C
+                        QueryJ Core
 
     Copyright (C) 2002-today  Jose San Leandro Armendariz
                               chous@acm-sl.org
