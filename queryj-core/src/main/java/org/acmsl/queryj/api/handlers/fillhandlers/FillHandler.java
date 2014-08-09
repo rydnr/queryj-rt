bad69005ae451593bbcf643e27518663090016c7
chous <chous@acm-sl.org>
2014-08-09 10:06:43 +0000

    201408091006

--- queryj-core/src/main/java/org/acmsl/queryj/api/handlers/fillhandlers/FillHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/handlers/fillhandlers/FillHandler.java
@@ -36,7 +36,7 @@
 package org.acmsl.queryj.api.handlers.fillhandlers;
 
 /*
- * Importing project classes.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.api.handlers.TemplateHandler;
