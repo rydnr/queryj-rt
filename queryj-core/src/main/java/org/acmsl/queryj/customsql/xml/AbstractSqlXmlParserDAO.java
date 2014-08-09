4c4027ceda8a3075fa1bb4f31ed2e1b01b0a602d
chous <chous@acm-sl.org>
2014-08-09 06:22:17 +0000

    201408090622

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/xml/AbstractSqlXmlParserDAO.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/xml/AbstractSqlXmlParserDAO.java
@@ -222,6 +222,7 @@ public abstract class AbstractSqlXmlParserDAO
      * @param contents such contents.
      * @param itemClass the class to filter.
      * @param idFilter the id filter (optional).
+     @SuppressWarnings("unchecked")
      */
     @SuppressWarnings("unchecked")
     @NotNull
