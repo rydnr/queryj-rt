c8a90c251b181f27f7c44fa53ae1436eed9ba1d2
chous <chous@acm-sl.org>
2014-08-09 16:28:49 +0000

    201408091628

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractForeignKeyDecorator.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/AbstractForeignKeyDecorator.java
@@ -334,7 +334,8 @@ public abstract class AbstractForeignKeyDecorator
     @Nullable
     public Table<DecoratedString, Attribute<DecoratedString>, ListDecorator<Attribute<DecoratedString>>> getTable(
         @NotNull final String sourceTableName, @NotNull final MetadataManager metadataManager)
-    {
+    {                                            git status
+        git status
         return getTable(sourceTableName, metadataManager.getTableDAO());
     }
 
