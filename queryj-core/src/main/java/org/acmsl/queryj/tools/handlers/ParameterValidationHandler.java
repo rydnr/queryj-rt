1985cca01fa6f80541e6ce857eb4ace45b69bf02
chous <chous@acm-sl.org>
2014-08-09 07:21:43 +0000

    201408090721

--- queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/ParameterValidationHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/ParameterValidationHandler.java
@@ -29,7 +29,7 @@
 package org.acmsl.queryj.tools.handlers;
 
 /*
- * Importing some project classes.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.QueryJCommandWrapper;
