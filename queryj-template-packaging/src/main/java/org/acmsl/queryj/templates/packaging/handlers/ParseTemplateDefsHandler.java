15600c1a54af534d8b0403feaad2c23f63794c86
chous <chous@acm-sl.org>
2014-08-09 09:18:35 +0000

    201408090918

--- queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/ParseTemplateDefsHandler.java
+++ queryj-template-packaging/src/main/java/org/acmsl/queryj/templates/packaging/handlers/ParseTemplateDefsHandler.java
@@ -283,6 +283,8 @@ public class ParseTemplateDefsHandler
     /**
      * Parses a template def stream.
      * @param inputStream the stream to parse.
+     * @return the Template def.
+     * @throws TemplatePackagingCheckedException if the template def file cannot be parsed.
      */
     @NotNull
     public TemplateDef<String> parseDefStream(@NotNull final InputStream inputStream)
