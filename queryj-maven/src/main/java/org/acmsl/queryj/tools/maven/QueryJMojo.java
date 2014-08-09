a52ca8e1e67df18b88f4919f72ee29d88302d8c4
chous <chous@acm-sl.org>
2014-08-09 07:35:54 +0000

    201408090735

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
@@ -1219,6 +1219,7 @@ public class QueryJMojo
     /**
      * Executes QueryJ via Maven2.
      * @param log the Maven log.
+     * @throws MojoExecutionException
      */
     protected void execute(@NotNull final Log log)
         throws MojoExecutionException
