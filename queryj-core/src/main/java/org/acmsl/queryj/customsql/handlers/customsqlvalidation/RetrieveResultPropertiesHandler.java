7fdea8e34416b45d9b0a52ce7399c2e11fb6c3a5
chous <chous@acm-sl.org>
2014-08-09 08:08:46 +0000

    201408090808

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/RetrieveResultPropertiesHandler.java
@@ -257,7 +257,7 @@ public class RetrieveResultPropertiesHandler
      * @param sqlResult the custom sql result.
      * @param propertyDAO the {@link org.acmsl.queryj.metadata.SqlPropertyDAO} instance.
      * @return such properties.
-     * @throws QueryJBuildExceptionif the property information is unavailable.
+     * @throws QueryJBuildException if the property information is unavailable.
      */
     @NotNull
     protected List<Property<String>> retrieveExplicitProperties(
