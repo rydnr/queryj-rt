29fb519b6d9d3c484b0bf64643c1cb345d9ae137
chous <chous@acm-sl.org>
2014-08-09 10:41:35 +0000

    201408091041

--- queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingDecoratorFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/metadata/CachingDecoratorFactory.java
@@ -115,7 +115,7 @@ public class CachingDecoratorFactory
     @Override
     @NotNull
     @SuppressWarnings("unchecked")
-    public <V, K> Attribute<DecoratedString> createDecorator(
+    public <V, K> Attribute<K> createDecorator(
         @NotNull final Attribute<V> attribute, @NotNull final MetadataManager metadataManager)
     {
         @NotNull final Attribute<DecoratedString> result;
