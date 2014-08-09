89c5b64705c78d3fb68e8b71ecbfed831b986866
chous <chous@acm-sl.org>
2014-08-09 07:20:56 +0000

    201408090720

--- queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/JdbcConnectionOpeningHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/JdbcConnectionOpeningHandler.java
@@ -125,6 +125,7 @@ public class JdbcConnectionOpeningHandler
      * @param jdbcUserName the user name.
      * @param jdbcPassword the password.
      * @return the JDBC connection.
+     * @throws QueryJBuildException if the connection cannot be opened.
      */
     @NotNull
     public Connection openConnection(
