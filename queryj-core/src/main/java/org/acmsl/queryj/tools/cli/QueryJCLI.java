99ec4ccc6f1406553742c398b7bbdf6751132706
chous <chous@acm-sl.org>
2014-08-09 08:32:29 +0000

    201408090832

--- queryj-core/src/main/java/org/acmsl/queryj/tools/cli/QueryJCLI.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/cli/QueryJCLI.java
@@ -67,8 +67,8 @@ import org.jetbrains.annotations.Nullable;
 
 /**
  * Allows executing QueryJ from the command-line.
- * @since 1.0
  * @author <a href="mailto:chous@acm-sl.org">Jose San Leandro Armendariz</a>
+ * @since 1.0
  * Created: Thu Jul 13 18:21:23 2006
  */
 public final class QueryJCLI
