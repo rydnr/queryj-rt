0f9183f893382be4e904823ceb0d419f0a2676cd
chous <chous@acm-sl.org>
2014-08-09 06:39:34 +0000

    201408090639

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/SqlXmlParserSqlDAO.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/SqlXmlParserSqlDAO.java
@@ -139,6 +139,7 @@ public class SqlXmlParserSqlDAO
     /**
      * Checks whether given primary key was already retrieved or not.
      * @param id the id to check.
+     * @return
      */
     protected synchronized boolean isPrimaryKeyAlreadyRetrieved(@NotNull final String id)
     {
