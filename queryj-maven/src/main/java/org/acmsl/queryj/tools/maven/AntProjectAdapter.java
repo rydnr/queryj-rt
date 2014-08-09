7a0ea424b08b9ad706fd53d2b4b1428e5e83d75b
chous <chous@acm-sl.org>
2014-08-09 07:30:42 +0000

    201408090730

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/AntProjectAdapter.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/AntProjectAdapter.java
@@ -1453,8 +1453,7 @@ public class AntProjectAdapter
      * @param length the buffer length.
      * @param project the {@link Project} instance.
      * @return the number of bytes read.
-     * @tho
-     *
+     * @throws IOException
      */
     protected int defaultInput(
         @NotNull final byte[] buffer, final int offset, final int length, @NotNull final Project project)
