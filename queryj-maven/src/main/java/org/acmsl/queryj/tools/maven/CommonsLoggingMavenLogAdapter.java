9a8ac5786ab5f1cb6473a2a5afb85e728d767942
chous <chous@acm-sl.org>
2014-08-09 07:35:32 +0000

    201408090735

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/CommonsLoggingMavenLogAdapter.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/CommonsLoggingMavenLogAdapter.java
@@ -583,6 +583,7 @@ public class CommonsLoggingMavenLogAdapter
     /**
      * Checks whether the debug level is enabled.
      * @param mavenLog the underlying {@link Log}.
+     * @return {@code true} in such case.
      */
     protected boolean isFatalEnabled(@NotNull final Log mavenLog)
     {
