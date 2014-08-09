74cf24abd1f772d4f3f1ab812d10eb2b0135862c
chous <chous@acm-sl.org>
2014-08-09 07:19:11 +0000

    201408090719

--- queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/JdbcConnectionClosingHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/JdbcConnectionClosingHandler.java
@@ -217,6 +217,7 @@ public class JdbcConnectionClosingHandler
     /**
      * Closes the JDBC connection stored in the attribute map.
      * @param parameters the parameter map.
+     * @throws QueryJBuildException if the metadata cannot be logged.
      */
     protected void closeConnection(@NotNull final QueryJCommand parameters)
         throws  QueryJBuildException
