f262b1ffab3d449c201dd373184bdc0814653798
chous <chous@acm-sl.org>
2014-08-13 17:14:14 +0200

    201408131714

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/oracle/OracleMetadataManagerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/oracle/OracleMetadataManagerTest.java
@@ -47,7 +47,6 @@ import org.jetbrains.annotations.NotNull;
 /*
  * Importing checkthread.org annotations.
  */
-import org.checkthread.annotations.ThreadSafe;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
