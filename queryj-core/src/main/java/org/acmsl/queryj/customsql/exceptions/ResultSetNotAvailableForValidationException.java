b3ead3c0fa18188c7252488b631497d006a8bf18
chous <chous@acm-sl.org>
2014-08-13 15:16:46 +0200

    201408131516

--- queryj-core/src/main/java/org/acmsl/queryj/customsql/exceptions/ResultSetNotAvailableForValidationException.java
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/exceptions/ResultSetNotAvailableForValidationException.java
@@ -38,7 +38,7 @@
 package org.acmsl.queryj.customsql.exceptions;
 
 /*
- * Importing JetBrains annotations.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.api.exceptions.QueryJNonCheckedException;
 import org.acmsl.queryj.customsql.Sql;
