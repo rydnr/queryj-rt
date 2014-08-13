88beaffc0c1240fbce246e2297bb6e04604f06f1
chous <chous@acm-sl.org>
2014-08-13 17:14:27 +0200

    201408131714

--- queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/oracle/OracleMetadataManagerTest.java
+++ queryj-core/src/test/java/org/acmsl/queryj/metadata/engines/oracle/OracleMetadataManagerTest.java
@@ -47,6 +47,7 @@ import org.jetbrains.annotations.NotNull;
 /*
  * Importing JUnit/EasyMock classes.
  */
+import org.easymock.EasyMock;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
