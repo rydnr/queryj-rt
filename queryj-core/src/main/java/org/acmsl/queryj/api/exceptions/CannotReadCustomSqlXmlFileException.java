bf279f455ec950bac5c239f850351e82bc2b6881
chous <chous@acm-sl.org>
2014-08-13 15:49:38 +0200

    201408131549

--- queryj-core/src/main/java/org/acmsl/queryj/api/exceptions/CannotReadCustomSqlXmlFileException.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/exceptions/CannotReadCustomSqlXmlFileException.java
@@ -66,7 +66,6 @@ public class CannotReadCustomSqlXmlFileException
      */
     private static final long serialVersionUID = -8064000429461780536L;
 
-
     protected static final String CANNOT_READ_CUSTOM_SQL_XML_FILE = "cannot.read.custom-sql.xml.file";
 
     /**
