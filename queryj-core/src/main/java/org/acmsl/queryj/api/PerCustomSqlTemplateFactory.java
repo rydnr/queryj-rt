fea5d7026d9fae007edd5ee38879034a4b6c221a
chous <chous@acm-sl.org>
2014-08-13 17:12:27 +0200

    201408131712

--- queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomSqlTemplateFactory.java
+++ queryj-core/src/main/java/org/acmsl/queryj/api/PerCustomSqlTemplateFactory.java
@@ -34,7 +34,7 @@
 package org.acmsl.queryj.api;
 
 /*
- * Importing some project classes.
+ * Importing QueryJ Core classes.
  */
 import org.acmsl.queryj.QueryJCommand;
 import org.acmsl.queryj.customsql.Sql;
