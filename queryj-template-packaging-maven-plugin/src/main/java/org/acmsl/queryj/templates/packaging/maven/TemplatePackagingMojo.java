a64e0eaf223dbec4d1fb039c76e08cf407201fed
chous <chous@acm-sl.org>
2014-08-15 08:09:01 +0200

    201408150809

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/maven/TemplatePackagingMojo.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/maven/TemplatePackagingMojo.java
@@ -97,7 +97,7 @@ public class TemplatePackagingMojo
      */
     protected static final String POM_PROPERTIES_LOCATION =
         "META-INF/maven/org.acmsl.queryj/queryj-template-packaging-maven-plugin/pom.properties";
-    private static final String PREFIX = ;
+    private static final String PREFIX = "queryj.";
 
     /**
      * Additional source directories.
