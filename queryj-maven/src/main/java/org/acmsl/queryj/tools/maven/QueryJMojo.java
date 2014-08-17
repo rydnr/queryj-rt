2d17f5338480dc24093214adc0528b287879a952
chous <chous@acm-sl.org>
2014-08-17 10:56:30 +0200

    201408171056

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
@@ -1646,7 +1646,7 @@ public class QueryJMojo
                ", \"disableGenerationTimestamps\": \"" + m__bDisableGenerationTimestamps + '"' +
                ", \"disableNotNullAnnotations\": \"" + m__bDisableNotNullAnnotations + '"' +
                ", \"disableCheckthreadAnnotations\": \"" + m__bDisableCheckthreadAnnotations + '"' +
-               + """class": "" + QueryJMojo.class.getName() + '"' +
+               + ", "class": "" + QueryJMojo.class.getName() + '"' +
                ", \"session\": \"" + session + "\" }";
     }
 }
