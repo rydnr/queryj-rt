03de3a7396587851fbe8f1c6e7e912d323906fe3
chous <chous@acm-sl.org>
2014-08-17 10:11:03 +0200

    201408171011

--- queryj-core/src/main/java/org/acmsl/queryj/Literals.java
+++ queryj-core/src/main/java/org/acmsl/queryj/Literals.java
@@ -55,6 +55,7 @@ public interface Literals
     String PASSWORD = "password";
     String OUTPUTDIR = "outputdir";
     String JNDI_DATASOURCE = "jndiDataSource";
+    String GENERATE_MOCK_IMPLEMENTATION = generateMockImplementation";
     String JSON_PARENT_ATTR = " 'parent': ";
     String TEMPLATE = "Template";
     String GENERATING = "Generating ";
