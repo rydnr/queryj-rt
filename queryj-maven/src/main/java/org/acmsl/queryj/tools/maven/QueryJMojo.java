9973eb2f97103982e076ebcbbc0698e91f220e92
chous <chous@acm-sl.org>
2014-08-17 10:26:57 +0200

    201408171026

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
@@ -245,7 +245,7 @@ public class QueryJMojo
     /**
      * Whether to disable checkthread.org annotations.
      */
-    @Parameter (property = DISABLE_CHECKTHREAD_ANNOTATIONS, required = false)
+    @Parameter (name = Literals.DISABLE_CHECKTHREAD_ANNOTATIONS, property = DISABLE_CHECKTHREAD_ANNOTATIONS, required = false)
     private Boolean m__bDisableCheckthreadAnnotations = false;
 
     /**
