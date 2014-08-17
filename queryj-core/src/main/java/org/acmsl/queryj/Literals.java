71add9baec767258c589c0fbff132b66767d81bc
chous <chous@acm-sl.org>
2014-08-17 10:10:06 +0200

    201408171010

--- queryj-core/src/main/java/org/acmsl/queryj/Literals.java
+++ queryj-core/src/main/java/org/acmsl/queryj/Literals.java
@@ -53,7 +53,7 @@ public interface Literals
     String URL = "url";
     String USERNAME = "username";
     String PASSWORD = "password";
-    String
+    String OUTPUTDIR = "outputdir";
     String JSON_PARENT_ATTR = " 'parent': ";
     String TEMPLATE = "Template";
     String GENERATING = "Generating ";
