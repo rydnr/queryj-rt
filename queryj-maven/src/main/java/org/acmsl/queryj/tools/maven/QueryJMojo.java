a1e5607fe2df5a0b62b37fde7780f8a6556c5d9e
chous <chous@acm-sl.org>
2014-08-17 10:21:58 +0200

    201408171021

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
@@ -160,7 +160,7 @@ public class QueryJMojo
     /**
      * The schema.
      */
-    @Parameter (name = property = JDBC_SCHEMA, required = true)
+    @Parameter (name = Literals.SCHEMA, property = JDBC_SCHEMA, required = true)
     private String schema;
 
     /**
