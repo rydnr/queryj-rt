ffa35f63d0124523a9ad674165f56f7db7906898
chous <chous@acm-sl.org>
2014-08-17 10:56:06 +0200

    201408171056

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
@@ -178,7 +178,7 @@ public class QueryJMojo
     /**
      * The output directory.
      */
-    @Parameter (name = Literals.OUTPUT_DIR, property = OUTPUT_FOLDER, required = false)
+    @Parameter (name = Literals.OUTPUT_DIR, property = OUTPUT_DIR, required = false)
     private File m__OutputDir;
 
     /**
