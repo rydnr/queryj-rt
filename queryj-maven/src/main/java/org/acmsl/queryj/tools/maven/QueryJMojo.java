1101fcb3b1dd2eddd826f204a9232e7633ca3f02
chous <chous@acm-sl.org>
2014-08-17 10:21:41 +0200

    201408171021

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
@@ -154,7 +154,7 @@ public class QueryJMojo
     /**
      * The catalog.
      */
-    @Parameter (property = JDBC_CATALOG, required = false)
+    @Parameter (name = Literals.CATALOG, property = JDBC_CATALOG, required = false)
     private String catalog;
 
     /**
