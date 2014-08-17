9615caa4f69ae9d82c2055860ab403677f2fd0fe
chous <chous@acm-sl.org>
2014-08-17 10:07:40 +0200

    201408171007

--- queryj-core/src/main/java/org/acmsl/queryj/QueryJSettings.java
+++ queryj-core/src/main/java/org/acmsl/queryj/QueryJSettings.java
@@ -49,7 +49,7 @@ public interface QueryJSettings
     /**
      * The JDBC driver.
      */
-    public static final String JDBC_DRIVER = PREFIX + DRIVER;
+    public static final String JDBC_DRIVER = PREFIX + _DRIVER;
 
     /**
      * The JDBC url.
