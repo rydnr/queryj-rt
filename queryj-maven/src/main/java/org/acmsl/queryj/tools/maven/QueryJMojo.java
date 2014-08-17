d07c8aafd3fad8315bdf1f7040f43cf3a24d01e4
chous <chous@acm-sl.org>
2014-08-17 10:26:46 +0200

    201408171026

--- queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
+++ queryj-maven/src/main/java/org/acmsl/queryj/tools/maven/QueryJMojo.java
@@ -239,7 +239,7 @@ public class QueryJMojo
     /**
      * Whether to disable NotNull annotations.
      */
-    @Parameter (property = PREFIX + "disableNotNullAnnotations", required = false)
+    @Parameter (name = Literals.DISABLE_NOTNULL_ANNOTATIONS, property = DISABLE_NOTNULL_ANNOTATIONS, required = false)
     private Boolean m__bDisableNotNullAnnotations = false;
 
     /**
