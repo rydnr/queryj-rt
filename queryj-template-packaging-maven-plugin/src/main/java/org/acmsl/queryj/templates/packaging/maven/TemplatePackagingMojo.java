9bfc6b9652ad889b4cc046c88a61d46c10c7d26d
chous <chous@acm-sl.org>
2014-08-19 09:27:14 +0200

    201408190927

--- queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/maven/TemplatePackagingMojo.java
+++ queryj-template-packaging-maven-plugin/src/main/java/org/acmsl/queryj/templates/packaging/maven/TemplatePackagingMojo.java
@@ -477,7 +477,7 @@ public class TemplatePackagingMojo
      */
     protected String retrieveVersion(@Nullable final Properties properties)
     {
-        String result = Literals.UNKNOWN_LITERAL;
+        String result = Literals.UNKNOWNL;
 
         if (   (properties != null)
             && (properties.containsKey(QueryJMojo.VERSION_LITERAL)))
