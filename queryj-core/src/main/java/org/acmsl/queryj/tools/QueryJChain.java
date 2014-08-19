036fa9b204d26bf71c4d3242718c7ae5c13c0ee4
chous <chous@acm-sl.org>
2014-08-19 08:37:37 +0200

    201408190837

--- queryj-core/src/main/java/org/acmsl/queryj/tools/QueryJChain.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/QueryJChain.java
@@ -153,7 +153,7 @@ public class QueryJChain<CH extends QueryJCommandHandler<QueryJCommand>>
         @NotNull final ServiceLoader<TemplateChainProvider> loader =
              ServiceLoader.load(TemplateChainProvider.class);
 
-        for (@NotNull final TemplateChainProvider provider : loader.iterator().hasNext())
+        for (@NotNull final TemplateChainProvider provider : loader.iterator())
         {
             // Don't know how to fix the generics warnings
             @NotNull final TemplateChainProvider provider = loader.iterator().next();
