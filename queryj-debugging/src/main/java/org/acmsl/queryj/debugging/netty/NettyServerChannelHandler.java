a601eabf2bc56f93f0b5430658df15d483412ea0
chous <chous@acm-sl.org>
2014-08-20 14:39:40 +0200

    201408201439

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerChannelHandler.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/netty/NettyServerChannelHandler.java
@@ -175,7 +175,7 @@ public class NettyServerChannelHandler
     {
         return
             "{ \"listener\": " + m__Listener.hashCode()
-            + ","
+            + ", \"class\": \""
                '}';
     }
 }
