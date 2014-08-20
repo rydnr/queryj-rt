3cfd31d20b57487086fe65a84e103f3507d1b774
chous <chous@acm-sl.org>
2014-08-20 08:48:39 +0200

    201408200848

--- queryj-debugging-maven-plugin/src/test/java/org/acmsl/queryj/debugging/maven/QueryJDebuggingMojoTest.java
+++ queryj-debugging-maven-plugin/src/test/java/org/acmsl/queryj/debugging/maven/QueryJDebuggingMojoTest.java
@@ -90,7 +90,7 @@ public class QueryJDebuggingMojoTest
         Assert.assertEquals("QUERYJUSER", mojo.getUsername());
         Assert.assertEquals("QUERYJPASSWORD", mojo.getPassword());
         Assert.assertNull("Not-null catalog", mojo.getCatalog());
-        Assert.assertNull(mojo.getSchema());
+        Assert.assertNull("Not-null schema", mojo.getSchema());
         Assert.assertEquals("repos", mojo.getRepository());
         Assert.assertEquals("com.foo.bar", mojo.getPackageName());
         Assert.assertEquals("java:comp/env/jdbc/default", mojo.getJndiDataSource());
