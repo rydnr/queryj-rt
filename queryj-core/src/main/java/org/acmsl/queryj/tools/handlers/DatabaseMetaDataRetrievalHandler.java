a40d38a6f30dc9c6e8ef9b1b6d272c91cbdfc685
chous <chous@acm-sl.org>
2014-08-20 09:38:01 +0200

    201408200938

--- queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/DatabaseMetaDataRetrievalHandler.java
+++ queryj-core/src/main/java/org/acmsl/queryj/tools/handlers/DatabaseMetaDataRetrievalHandler.java
@@ -339,7 +339,6 @@ public abstract class DatabaseMetaDataRetrievalHandler
                     }
                 }
             }
-            */
         }
 
         storeAlreadyDoneFlag(parameters);
