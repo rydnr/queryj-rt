360a9cea6c978a7954f6b63cfadbcfa737b0da6c
chous <chous@acm-sl.org>
2014-08-21 09:01:30 +0200

    201408210901

--- queryj-debugging/src/main/java/org/acmsl/queryj/debugging/STInspectorDebuggingService.java
+++ queryj-debugging/src/main/java/org/acmsl/queryj/debugging/STInspectorDebuggingService.java
@@ -84,7 +84,9 @@ public class STInspectorDebuggingService<C extends TemplateContext>
      */
     @NotNull
     @Override
-    public TemplateDebuggingCommand debug(@NotNull final QueryJCommandHandler<QueryJCommand> handler)
+    public TemplateDebuggingCommand debug(
+        @NotNull final QueryJCommandHandler<QueryJCommand> handler,
+        @NotNull final QueryJCommand command)
     {
         return TemplateDebuggingCommand.NEXT;
     }
