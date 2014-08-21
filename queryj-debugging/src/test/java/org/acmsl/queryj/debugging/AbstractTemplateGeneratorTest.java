1a180920e95649db425bb2b27ac8afc1577ec575
chous <chous@acm-sl.org>
2014-08-21 09:02:27 +0200

    201408210902

--- queryj-debugging/src/test/java/org/acmsl/queryj/debugging/AbstractTemplateGeneratorTest.java
+++ queryj-debugging/src/test/java/org/acmsl/queryj/debugging/AbstractTemplateGeneratorTest.java
@@ -134,7 +134,9 @@ public class AbstractTemplateGeneratorTest
          */
         @NotNull
         @Override
-        public TemplateDebuggingCommand debug(@NotNull final QueryJCommandHandler<QueryJCommand> handler)
+        public TemplateDebuggingCommand debug(
+            @NotNull final QueryJCommandHandler<QueryJCommand> handler,
+            @NotNull final QueryJCommand command)
         {
             return TemplateDebuggingCommand.NEXT;
         }
